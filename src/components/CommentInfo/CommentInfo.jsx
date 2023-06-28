0db33abcea89bf6c4ce4052a0b8a2a0066939dd4
alex <cebanoleksandr@gmail.com>
2023-06-28 20:28:35 +0300

    solution2

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,22 @@
-export const CommentInfo = ({ comment }) => (
-  <div>{ comment.body }</div>
-);
+export const CommentInfo = ({ comment }) => {
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{comment.name}</strong>
+
+        {' by '}
+
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${comment.email}`}
+        >
+          {comment.email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        {comment.body}
+      </div>
+    </div>
+  );
+};
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -2,7 +2,7 @@ import { CommentInfo } from '../CommentInfo';
 import './CommentList.scss';
 
 export const CommentList = ({ comments }) => (
-  <div className="CommentInfo__body">
+  <div className="CommentList">
     {comments.map(comment => (
       <CommentInfo comment={comment} key={comment.id} />
     ))}
