34319b230b6d50e2b023c5920f1fb093c47d2597
Mariia <mariia.monakhova1@gmail.com>
2023-06-29 12:14:52 +0300

    add destructurisation

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,24 +2,31 @@ import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <>
+export const PostInfo = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
+
+  return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+        <h3 className="PostInfo__title">{title}</h3>
 
         <p>
           {' Posted by  '}
 
-          <UserInfo user={post.user} />
+          <UserInfo user={user} />
         </p>
       </div>
 
       <p className="PostInfo__body">
-        {post.body}
+        {body}
       </p>
 
-      {!post.comments.length
+      {!comments.length
 
         ? (
           <>
@@ -28,9 +35,8 @@ export const PostInfo = ({ post }) => (
           </>
         )
         : (
-          <CommentList comments={post.comments} />
+          <CommentList comments={comments} />
         )}
     </div>
-
-  </>
-);
+  );
+};
