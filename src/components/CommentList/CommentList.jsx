039a5c5073cf7bda9011327898aa43d46381a903
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-06-29 18:47:43 +0300

    feat: posts tracker provided

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,19 @@
-export const CommentList = () => (
-  <>Put the list here</>
-);
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  if (comments.length === 0) {
+    return (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    );
+  }
+
+  return (
+    <div className="CommentList">
+      {
+      comments.map(comment => (
+        <CommentInfo comment={comment} key={comment.id} />
+      ))
+    }
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,13 @@
-export const UserInfo = () => (
-  <>Put the user here</>
-);
+export const UserInfo = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <p>
+      {' Posted by  '}
+
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </p>
+  );
+};
