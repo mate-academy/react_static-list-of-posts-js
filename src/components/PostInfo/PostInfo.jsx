fc11e32af85bb72b785a400e7820746d651e6c60
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-06-29 17:04:32 +0300

    react_static-list-of-posts-js solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,21 @@
-export const PostInfo = () => (
-  <>Put the post here</>
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+import './PostInfo.scss';
+
+export const PostInfo = ({ post: { user, title, body, comments } }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{title}</h3>
+
+      {user && <UserInfo user={user} />}
+    </div>
+
+    <p className="PostInfo__body">{body}</p>
+    <hr />
+
+    { comments.length > 0
+      ? <CommentList comments={comments} />
+      : <b data-cy="NoCommentsMessage">No comments yet</b>
+    }
+  </div>
 );
