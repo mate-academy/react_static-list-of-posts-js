955616473da6281da51bedafcbe284c2e4e7997a
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-06-29 22:17:04 +0300

    Task done

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,8 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments && comments.map(comment => <CommentInfo comment={comment} />)}
+  </div>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,10 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+    {/* <b data-cy="NoCommentsMessage">No comments yet</b> */}
+  </div>
 );
