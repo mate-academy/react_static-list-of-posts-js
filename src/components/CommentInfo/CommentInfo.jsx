17fcb593bc1794248a16600038cf636432b4ba13
Andrii Dudar <andrij.dudar@gmail.com>
2023-06-30 18:46:56 +0300

    Solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,25 +1,26 @@
 import './CommentInfo.scss';
 
 export const CommentInfo = ({ comment }) => {
-  const { .. } = comment;
-  
+  const { name, email, body } = comment;
+
   return (
-  <div className="CommentInfo">
-    <div className="CommentInfo__title">
-      <strong className="CommentInfo__name">{comment.name}</strong>
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{name}</strong>
 
-      {' by '}
+        {' by '}
 
-      <a
-        className="CommentInfo__email"
-        href={`mailto:${comment.email}`}
-      >
-        {comment.email}
-      </a>
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${email}`}
+        >
+          {email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        {body}
+      </div>
     </div>
-
-    <div className="CommentInfo__body">
-      {comment.body}
-    </div>
-  </div>
-);
+  )
+};
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -8,12 +8,13 @@ export const PostInfo = ({ post }) => (
       <h3 className="PostInfo__title">
         {post.title}
       </h3>
+      {post.user && (
+        <p>
+          {' Posted by  '}
+          <UserInfo user={post.user} />
+        </p>
+      )}
 
-      <p>
-        {' Posted by  '}
-
-        <UserInfo user={post.user} />
-      </p>
     </div>
 
     <p className="PostInfo__body">
