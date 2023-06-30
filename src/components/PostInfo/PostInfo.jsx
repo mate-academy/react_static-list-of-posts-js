9f68a0b2193ba86cfdd79f3f230ad0d3d53397be
Natalia Tsiutsiura <89_22@ukr.net>
2023-06-30 11:51:31 +0300

    Solution1

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,33 @@
-export const PostInfo = () => (
-  <>Put the post here</>
+import { CommentList } from '../CommentList';
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => (
+
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <a className="UserInfo" href={`mailto:${post.email}`}>
+          {post.name}
+        </a>
+      </p>
+    </div>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    {post.comments.length === 0
+      ? (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )
+      : <CommentList comments={post.comments} />
+    }
+  </div>
 );
