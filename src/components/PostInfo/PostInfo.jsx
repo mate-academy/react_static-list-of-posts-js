1bdcc8b975b65bd3c9ff53a3b656c00fdd937375
Anatolii Lykhohodenko <anatollykh10@gmail.com>
2023-07-03 18:10:22 +0300

    fix

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,27 +2,36 @@ import { CommentList } from '../CommentList/CommentList';
 import { UserInfo } from '../UserInfo/UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <>
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo = ({ post }) => {
+  const {
+    title,
+    user,
+    body,
+    comments,
+  } = post;
 
-        <p>
-          {' Posted by  '}
+  return (
+    <>
+      <div className="PostInfo">
+        <div className="PostInfo__header">
+          <h3 className="PostInfo__title">{title}</h3>
 
-          {post.user && (<UserInfo user={post.user} />)}
-        </p>
+          <p>
+            {' Posted by  '}
+
+            {user && (<UserInfo user={user} />)}
+          </p>
+        </div>
+
+        <p className="PostInfo__body">{body}</p>
+
+        <hr />
+
+        { !comments.length
+          ? <b data-cy="NoCommentsMessage">No comments yet</b>
+          : <CommentList comments={comments} />
+      }
       </div>
-
-      <p className="PostInfo__body">{post.body}</p>
-
-      <hr />
-
-      { post.comments.length === 0
-        ? <b data-cy="NoCommentsMessage">No comments yet</b>
-        : <CommentList comments={post.comments} />
-    }
-    </div>
-  </>
-);
+    </>
+  );
+};
