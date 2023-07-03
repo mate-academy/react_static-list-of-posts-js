a0b0135f15ecfeafbcbe44a902e467e8e0e9c1c9
Yana <yanaklymenko.od@gmail.com>
2023-07-03 13:50:13 +0300

    added destructured propses

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -3,6 +3,5 @@ import { PostInfo } from '../PostInfo';
 export const PostList = ({ posts }) => (
   <div className="PostList">
     {posts.map(post => <PostInfo post={post} key={post.id} />)}
-
   </div>
 );
