21b02406b4ee63ad1960ee3b68aceedb23ffe94c
Absolution26 <zet2571428@gmail.com>
2023-07-08 12:51:41 +0300

    solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,21 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  comments.length > 0
+    ? (
+      <ul className="CommentList">
+        {comments.map(commentItem => (
+          <CommentInfo comment={commentItem} key={commentItem.id} />
+        ))}
+      </ul>
+    )
+    : (
+      <>
+        <hr />
+        <b data-cy="NoCommentsMessage">
+          not comments yet
+        </b>
+      </>
+    )
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,9 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => (
+  <ul className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    )) || 'not comments yet'}
+  </ul>
 );
