e39eb7e757205297da6f01eca98d56d397800777
Eugene Kulish <genekulish13@gmail.com>
2023-07-09 16:01:50 +0300

    change CommentList behaviour

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,18 +1,18 @@
 import './CommentList.scss';
 import { CommentInfo } from '../CommentInfo';
 
-export const CommentList = ({ comments }) => (
-  <div className="CommentList">
-    {(comments.length === 0) ? (
-      <>
-        <hr />
-
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      </>
-    ) : (
-      comments.map(comment => (
+export const CommentList = ({ comments }) => {
+  return (comments.length === 0) ? (
+    <>
+      <hr />
+      
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    </>
+  ) : (
+    <div className="CommentList">
+      {comments.map(comment => (
         <CommentInfo comment={comment} key={comment.id} />
-      ))
-    )}
-  </div>
-);
+      ))}
+    </div>
+  )
+};
