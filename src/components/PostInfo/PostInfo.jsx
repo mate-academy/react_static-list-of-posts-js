b594a6d2a5fa8f3d1f7be1da15cf00a38156ed05
Eugene Kulish <genekulish13@gmail.com>
2023-07-09 17:21:48 +0300

    remove fragments

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,7 +1,6 @@
 import './PostInfo.scss';
 import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
-import commentsFromServer from '../../api/comments.json';
 
 export const PostInfo = ({ post }) => (
   <div className="PostInfo">
@@ -19,9 +18,6 @@ export const PostInfo = ({ post }) => (
       {post.body}
     </p>
 
-    <CommentList comments={
-      commentsFromServer.filter(comment => comment.postId === post.id)
-    }
-    />
+    <CommentList comments={post.comments} />
   </div>
 );
