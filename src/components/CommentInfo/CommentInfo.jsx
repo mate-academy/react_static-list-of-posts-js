d69c3805c821aed82ddb45cf7d7667f558370698
Max <maxpusto533gmail.com>
2023-07-11 20:56:53 +0300

    Pre_result

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,52 @@
-export const CommentInfo = () => (
-  <>Put the comment here</>
-);
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => {
+  const {
+    name,
+    email,
+    body,
+  } = comment;
+
+  return (
+    <>
+      <div className="CommentInfo">
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">{name}</strong>
+
+          {' by '}
+
+          <a
+            className="CommentInfo__email"
+            href="mailto:Telly_Lynch@karl.co.uk"
+          >
+            {email}
+          </a>
+        </div>
+
+        <div className="CommentInfo__body">
+          {body}
+        </div>
+      </div>
+
+      <div className="CommentInfo">
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">
+            {name}
+          </strong>
+
+          {' by '}
+          <a
+            className="CommentInfo__email"
+            href="mailto:Nikita@garfield.biz"
+          >
+            {email}
+          </a>
+        </div>
+
+        <div className="CommentInfo__body">
+          {body}
+        </div>
+      </div>
+    </>
+  );
+};
