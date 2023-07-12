f08751c230ae656b10b7076b2ea1b33b6e48e6ea
Nazar Vorona <voron1765@gmail.com>
2023-07-12 19:29:41 +0300

    add destructuring

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,32 +2,36 @@ import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">
-        {post.title}
-      </h3>
+export const PostInfo = ({ post }) => {
+  const { title, user, body, comments } = post;
 
-      <p>
-        {' Posted by  '}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
 
-        <UserInfo user={post.user} />
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
+
+      {comments.length === 0
+        ? (
+          <>
+            <hr />
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </>
+        )
+        : (<CommentList comments={comments} />)
+      }
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-
-    {post.comments.length === 0
-      ? (
-        <>
-          <hr />
-          <b data-cy="NoCommentsMessage">No comments yet</b>
-        </>
-      )
-      : (<CommentList comments={post.comments} />)
-    }
-  </div>
-);
+  );
+};
