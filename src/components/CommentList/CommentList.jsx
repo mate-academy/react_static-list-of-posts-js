3d0d94389ba4da7627d5783d0077ee8220b16f55
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-07-13 13:24:54 +0300

    solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,30 @@
-export const CommentList = () => (
-  <>Put the list here</>
-);
+export const CommentList = ({ comments }) => {
+  const {
+    name,
+    email,
+    body,
+  } = comments;
+
+  return (
+    <div className="CommentList">
+      <div className="CommentInfo">
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">{ name }</strong>
+
+          {' by '}
+
+          <a
+            className="CommentInfo__email"
+            href={email}
+          >
+            { email }
+          </a>
+        </div>
+
+        <div className="CommentInfo__body">
+          { body }
+        </div>
+      </div>
+    </div>
+  );
+};
