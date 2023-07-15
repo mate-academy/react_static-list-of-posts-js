e8645b9f259f146a1a8aa3c37065454b16ae3ced
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-07-15 10:56:47 +0200

    almost implemented PostList JSX component but some tests are not passed

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,31 @@
-export const PostInfo = () => (
-  <>Put the post here</>
-);
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+import './PostInfo.scss';
+
+export const PostInfo = ({ post, user }) => {
+  const { title, body, comments } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+          {/* {console.log('Console log from PostInfo.jsx \nuser: ', user)} */}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      {post.comments.length !== 0
+        ? <CommentList comments={comments} />
+        : <b data-cy="NoCommentsMessage">No comments yet</b>}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,48 @@
-export const PostList = () => (
-  <>Put the list here</>
-);
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({
+  postsFromServer, commentsFromServer, usersFromServer,
+}) => {
+  const postsWithComments = ((posts, comments) => (
+    posts.map((post) => {
+      const postWithComments = { ...post };
+      const commentsOfPost = comments
+        .map(comment => comment.postId === post.id && comment)
+        .filter(comment => comment !== false && comment);
+
+      postWithComments.comments = commentsOfPost;
+
+      return postWithComments;
+    })
+  ))(postsFromServer, commentsFromServer);
+
+  const usersWithPosts = ((posts, users) => (
+    users.map((user) => {
+      const userWithPosts = { ...user };
+      const postsOfUser = posts
+        .map(post => post.userId === user.id && post)
+        .filter(post => post !== false);
+
+      userWithPosts.posts = postsOfUser;
+
+      return userWithPosts;
+    })
+  ))(postsWithComments, usersFromServer);
+
+  return (
+    <div className="PostList">
+      {usersWithPosts.map((userWithPosts) => {
+        const { name, email, username } = userWithPosts;
+        const onlyUserData = { name, email, username };
+
+        return userWithPosts.posts.map(post => (
+          <PostInfo
+            key={post.id}
+            post={post}
+            user={onlyUserData}
+          />
+        ));
+      })}
+    </div>
+  );
+};
