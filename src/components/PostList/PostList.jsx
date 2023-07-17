d4699c84e6fff9048c7cb01899797187729f9e9a
Artem Kravets <kravetsartyom@gmail.com>
2023-07-17 12:20:32 +0300

    Solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,12 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => (
+  <ul className="PostList">
+    {posts.map(post => (
+      <PostInfo
+        key={post.id}
+        post={post}
+      />
+    ))}
+  </ul>
 );
