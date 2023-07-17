f83dd752dfd5c9b9a7a55108ae09a8892cf3b648
Serhii <gashysh@protonmail.com>
2023-07-17 17:58:05 +0300

    fixed

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -6,33 +6,31 @@ export const PostInfo = ({ post }) => {
   const { title, body, user, comments } = post;
 
   return (
-    <>
-      <div className="PostInfo">
-        <div className="PostInfo__header">
-          <h3 className="PostInfo__title">{title}</h3>
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
 
-          <p>
-            {' Posted by  '}
+        <p>
+          {' Posted by  '}
 
-            <UserInfo user={user} />
-          </p>
-        </div>
-
-        <p className="PostInfo__body">
-          {body}
+          <UserInfo user={user} />
         </p>
-
-        <hr />
-
-        {comments.length
-          ? (<CommentList comments={comments} />)
-          : (
-            <>
-              <hr />
-              <b data-cy="NoCommentsMessage">No comments yet</b>
-            </>
-          )}
       </div>
-    </>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      <hr />
+
+      {comments.length
+        ? (<CommentList comments={comments} />)
+        : (
+          <>
+            <hr />
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </>
+        )}
+    </div>
   );
 };
