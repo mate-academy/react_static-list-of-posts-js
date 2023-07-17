b447b951e3b712cf52ac8cd08b277870007ae43e
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-07-17 12:19:46 +0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
+
 import { PostList } from './components/PostList';
 
 function postedBy(userId) {
