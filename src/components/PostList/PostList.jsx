e91118647c53e60e1c12ca121f9875bb92256738
Oleksandr Voronov <alexdroid33@gmail.com>
2023-07-18 21:02:06 +0300

    fixed

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,9 +1,11 @@
 import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts }) => (
-  <div className="PostList">
+  <ul className="PostList">
     {posts.map(post => (
-      <PostInfo post={post} key={post.id} />
+      <li key={post.id}>
+        <PostInfo post={post} />
+      </li>
     ))}
-  </div>
+  </ul>
 );
