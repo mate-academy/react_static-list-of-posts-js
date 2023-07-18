f1a3f17af727ae75a0ffab446f42a8ce8ee05494
Denis Kononenko <deniskononenko88@gmail.com>
2023-07-18 14:24:00 +0300

    add changes

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,7 +1,12 @@
+import './PostList.scss';
 import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts }) => (
-  <div className="PostList">
-    {posts.map(post => <PostInfo key={post.id} post={post} />)}
-  </div>
+  !posts || posts.length === 0 ? (
+    <p className="NoPostsMessage">No posts yet</p>
+  ) : (
+    <div className="PostList">
+      {posts.map(post => <PostInfo key={post.id} post={post} />)}
+    </div>
+  )
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -3,8 +3,12 @@ import './UserInfo.scss';
 export const UserInfo = ({ user }) => {
   const { name, email } = user;
 
-  return (
-    <a className="UserInfo" href={`mailto:${email}`}>
+  return user && (
+    <a
+      className="UserInfo"
+      href={`mailto:${email}`}
+      title={name}
+    >
       {name}
     </a>
   );
