e9cd1d9ffe363fe78cf76b69d1be7d6748ada9aa
Volodymyr Shostak <shostak_1@proton.me>
2023-07-22 23:19:42 +0300

    add list of posts with comments

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,11 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <div className="PostInfo">
+        <PostInfo post={post} key={posts.id} />
+      </div>
+    ))}
+  </div>
 );
