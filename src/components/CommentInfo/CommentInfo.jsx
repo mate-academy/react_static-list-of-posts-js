8afddc4ed55d8b5c11a7e56ce58de5c960701b83
purplefade <tarovykanton@gmail.com>
2023-07-28 21:59:54 +0300

    add solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,23 +1,15 @@
+import React from 'react';
+import './CommentInfo.scss';
+
 export const CommentInfo = ({ comment }) => {
-  const {
-    name,
-    email,
-    body,
-  } = comment;
+  const { name, email, body } = comment;
 
   return (
     <div className="CommentInfo">
-      <div className="CommentInfo__title">
-        <strong className="CommentInfo__name">
-          {name}
-        </strong>
-
-        {' by '}
-
-        <a
-          className="CommentInfo__email"
-          href={`mailto:${email}`}
-        >
+      <div className="CommentInfo__header">
+        <strong className="CommentInfo__name">{name}</strong>
+        <span className="CommentInfo__author">{' by '}</span>
+        <a className="CommentInfo__email" href={`mailto:${email}`}>
           {email}
         </a>
       </div>
