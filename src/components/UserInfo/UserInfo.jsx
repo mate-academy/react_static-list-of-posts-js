bb84efba0004cbdd553c52e6dac86bbfcbf9fce3
OlegRak86 <rakolegit@gmail.com>
2023-08-01 11:00:22 +0300

    react_static-list-of-posts-js

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,11 @@
-export const UserInfo = () => (
-  <>Put the user here</>
+import './UserInfo.scss';
+
+export const UserInfo = ({ users }) => (
+  <p>
+    {' Posted by  '}
+
+    <a className="UserInfo" href={`mailto:${users.email}`}>
+      {`${users.name}`}
+    </a>
+  </p>
 );
