c56cd78a1b1314fc863fe206c18a6f1809caaaf1
Alina <kozlovska.a.mail@gmail.com>
2023-08-04 18:57:06 +0200

    improve PostInfo logic

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,26 +2,35 @@ import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
+export const PostInfo = ({ post: {
+  id,
+  userId,
+  title,
+  body,
+  comments,
+  user,
+} }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+      <h3 className="PostInfo__title">{title}</h3>
 
-      <p>
-        {' Posted by  '}
+      {user && (
+        <p>
+          {' Posted by  '}
 
-        <UserInfo user={post.user} />
-      </p>
+          <UserInfo user={user} />
+        </p>
+      )}
     </div>
 
-    <p className="PostInfo__body">{post.body}</p>
+    <p className="PostInfo__body">{body}</p>
 
     <hr />
 
-    {post.comments.length === 0 ? (
+    {comments.length === 0 ? (
       <b data-cy="NoCommentsMessage">No comments yet</b>
     ) : (
-      <CommentList comments={post.comments} />
+      <CommentList comments={comments} />
     )}
   </div>
 );
