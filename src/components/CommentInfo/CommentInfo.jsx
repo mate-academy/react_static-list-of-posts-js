5c3e2bdca9e8b01264b42b514afb0942b51f74a4
Vlad Danilenko <gabiolario@gmail.com>
2023-08-04 09:32:54 +0300

    add task solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,24 @@
-export const CommentInfo = () => (
-  <>Put the comment here</>
-);
+export const CommentInfo = ({ comment }) => {
+  const { name: commentName, email: commentEmail, body: commentBody } = comment;
+
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{commentName}</strong>
+
+        {' by '}
+
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${commentEmail}`}
+        >
+          {commentEmail}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        {commentBody}
+      </div>
+    </div>
+  );
+};
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,34 @@
-export const PostInfo = () => (
-  <>Put the post here</>
-);
+import './PostInfo.scss';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+export const PostInfo = ({ post }) => {
+  const { title: postTitle, body: postBody, user, comments } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{postTitle}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {postBody}
+      </p>
+
+      <hr />
+
+      {comments.length > 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
