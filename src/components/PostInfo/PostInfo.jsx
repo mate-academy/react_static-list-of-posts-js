b782798b636a3e0a42c32d040ee5c6eb8731d948
Nataliia <koreniuknataliia@gmail.com>
2023-08-05 19:37:07 +0300

    added user validation

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -7,8 +7,9 @@ export const PostInfo = ({ post: { title, user, body, comments } }) => (
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{title}</h3>
 
+      {user && (
       <UserInfo user={user} />
-
+      )}
     </div>
 
     <p className="PostInfo__body">{body}</p>
