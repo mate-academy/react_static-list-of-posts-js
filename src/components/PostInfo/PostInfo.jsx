b2ac91d5961ff7092dee5467e172f530d30d891d
Horizon-git <apbreloaded947@gmail.com>
2023-08-06 02:13:54 +0300

    fix by suggestions

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -6,7 +6,10 @@ export const PostInfo = ({ post }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
-      <UserInfo user={post.user} />
+      <p>
+        {' Posted by  '}
+        {post.user && <UserInfo user={post.user} />}
+      </p>
     </div>
 
     <p className="PostInfo__body">{post.body}</p>
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,5 @@
 export const UserInfo = ({ user }) => (
-  <p>
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
-    </a>
-  </p>
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
 );
