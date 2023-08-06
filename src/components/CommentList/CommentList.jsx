cf051110e4b072d312c930d7cf811ba772b6c60a
RomanenkoStud <pavlo.romanenko@nure.ua>
2023-08-06 18:26:02 +0300

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,26 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import PropTypes from 'prop-types';
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    <ul className="CommentList__list">
+      {comments.map(comment => (
+        <li key={comment.id} className="CommentList__item">
+          <CommentInfo comment={comment} />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
+
+CommentList.propTypes = {
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,31 @@
-export const PostList = () => (
-  <>Put the list here</>
+import PropTypes from 'prop-types';
+import { PostInfo } from '../PostInfo';
+import './PostList.scss';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    <ul className="PostList__list">
+      {posts.map(post => (
+        <li key={post.id} className="PostList__item">
+          <PostInfo post={post} />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
+
+const userShape = PropTypes.shape({
+  email: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+});
+
+PostList.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+      user: userShape.isRequired,
+    }),
+  ).isRequired,
+};
