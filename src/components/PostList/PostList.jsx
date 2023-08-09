04bbe46c07d37edf0ea0b78bf9fd0db04317309c
Yuliia Dub <yuliia.kasaraba@gmail.com>
2023-08-09 15:30:32 +0300

    add post list

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,10 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+// import postsFromServer from './api/posts.json';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </div>
 );
