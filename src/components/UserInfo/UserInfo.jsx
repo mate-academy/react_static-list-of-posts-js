94418e606c481c44d0f955b5a563872c7e9c6748
Ksenia Skrebtsova <121960247+KseniaSkr@users.noreply.github.com>
2023-08-13 21:40:36 +0300

    changed according to the comment

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,10 +1,10 @@
 import './UserInfo.scss';
 
 export const UserInfo = ({ user: { email, name } }) => (
-  <p>
-    {' Posted by  '}
+  <div>
+    <p className="User__title">Posted by</p>
     <a className="UserInfo" href={`mailto:${email}`}>
       {name}
     </a>
-  </p>
+  </div>
 );
