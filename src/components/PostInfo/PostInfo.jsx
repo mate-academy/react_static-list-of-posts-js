b9d192f6d83d73a7fbc448afccd0b3886064d2d6
Yelisieiev Kim <rbv271@outlook.com>
2023-08-16 10:28:38 +0300

    fixes

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -23,11 +23,9 @@ export const PostInfo = ({ post }) => {
 
       <hr />
 
-      {comments.length === 0 ? (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      ) : (
-        (<CommentList comments={comments} />)
-      )}
+      {comments.length === 0
+        ? <b data-cy="NoCommentsMessage">No comments yet</b>
+        : <CommentList comments={comments} />}
     </div>
   );
 };
