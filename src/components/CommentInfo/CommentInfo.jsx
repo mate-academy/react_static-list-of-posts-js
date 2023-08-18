ff24ea61da0cb61a3687c4b6938003fadebbdfdc
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-08-18 14:44:59 +0200

    Solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,15 @@
-export const CommentInfo = () => (
-  <>Put the comment here</>
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment: { name, email, body } }) => (
+  <div className="CommentInfo">
+    <a className="CommentInfo__email" href={`mailto:${email}`}>
+      {email}
+    </a>
+
+    {' commented: '}
+
+    <strong className="CommentInfo__name">{name}</strong>
+
+    <p className="CommentInfo__body">{body}</p>
+  </div>
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,11 @@
-export const UserInfo = () => (
-  <>Put the user here</>
+import './UserInfo.scss';
+
+export const UserInfo = ({ user: { username, name, email } }) => (
+  <p>
+    {` Posted by `}
+
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  </p>
 );
