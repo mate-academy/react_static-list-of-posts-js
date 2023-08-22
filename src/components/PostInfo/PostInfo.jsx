7011bd2c1ab0dea95a46bc9a0547c38d26fba98b
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-08-22 18:27:50 +0200

    Fix 1: removed redundant fragments, desructured assignment , remoed key from userInfo , added conditional rendering for posts

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,31 +2,35 @@ import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <>
+export const PostInfo = ({ post }) => {
+  const { title, user, comments, body } = post;
+
+  return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+        <h3 className="PostInfo__title">{title}</h3>
 
-        <p>
-          {' Posted by  '}
+        {user && (
+          <p>
+            {' Posted by  '}
 
-          <UserInfo user={post.user} key={post.user.id} />
-        </p>
+            <UserInfo user={user} />
+          </p>
+        )}
+
       </div>
 
       <p className="PostInfo__body">
-        {post.body}
+        {body}
       </p>
-      {post.comments.length === 0 ? (
+      {comments.length === 0 ? (
         <>
           <hr />
           <b data-cy="NoCommentsMessage">No comments yet</b>
         </>
       ) : (
-        <CommentList comments={post.comments} />
+        <CommentList comments={comments} />
       )}
-
     </div>
-  </>
-);
+  );
+};
