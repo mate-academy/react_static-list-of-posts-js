46a41cadf58088a640bb5a87bd0328218c43f5b5
Maksym <morenko.maxim@gmail.com>
2023-08-23 08:01:43 +0300

    component separation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
+import { PostList } from './components/PostList';
 
 const getUser = userID => (
   usersFromServer.filter(user => user.id === userID)[0]);
@@ -16,98 +17,9 @@ const posts = postsFromServer.map(post => ({
   comments: getComment(post.id),
 }));
 
-const UserInfo = ({ user }) => {
-  const { name, email } = user;
-
-  return (
-    <a className="UserInfo" href={`mailto:${email}`}>
-      {name}
-    </a>
-  );
-};
-
-const CommentInfo = ({ comment }) => {
-  const { name, email, body } = comment;
-
-  return (
-    <div className="CommentInfo">
-      <div className="CommentInfo__title">
-        <strong className="CommentInfo__name">{name}</strong>
-        <span>by </span>
-        <a
-          className="CommentInfo__email"
-          href={`mailto:${email}`}
-        >
-          {email}
-        </a>
-      </div>
-
-      <div className="CommentInfo__body">
-        {body}
-      </div>
-    </div>
-  );
-};
-
-const CommentList = ({ comments }) => (
-  <div className="CommentList">
-    {comments.map(comment => (
-      <CommentInfo comment={comment} key={comment.id} />
-    ))}
-  </div>
-);
-
-const PostInfo = ({ post }) => {
-  const {
-    title,
-    body,
-    user,
-    comments,
-  } = post;
-
-  return (
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{title}</h3>
-
-        {user && (
-          <p>
-            {' Posted by  '}
-            <UserInfo user={user} />
-          </p>
-        )}
-
-      </div>
-
-      <p className="PostInfo__body">
-        {body}
-      </p>
-
-      {comments && comments.length
-        ? <CommentList comments={comments} />
-        : (
-          <>
-            <hr />
-            <b data-cy="NoCommentsMessage">No comments yet</b>
-          </>
-        )}
-    </div>
-  );
-};
-
-const PostList = () => (
-  <div className="PostList">
-    {posts.map(post => (
-      <PostInfo post={post} key={post.id} />
-    ))}
-
-  </div>
-);
-
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-
-    <PostList posts={postsFromServer} users={usersFromServer} />
+    <PostList posts={posts} />
   </section>
 );
