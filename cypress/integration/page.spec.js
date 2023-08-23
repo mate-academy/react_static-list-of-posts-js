8daf8f2e86f874c8bf769c007591499c74221ee5
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-08-23 17:25:00 +0300

    Solution

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -6,26 +6,26 @@ describe('Page', () => {
   });
 
   it('should contain all the posts', () => {
-    cy.get('.PostInfo').should('have.length', 20);
+    cy.get('.PostInfo').should('have.length', 21);
   });
 
   it('should rended posts in the correct order', () => {
     cy.get('.PostInfo__title').eq(0)
-      .should('have.text', 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
+      .should('have.text', 'Solution🎀');
 
-    cy.get('.PostInfo__title').eq(19)
+    cy.get('.PostInfo__title').eq(20)
       .should('have.text', 'aut amet sed');
   });
 
   it('should add corresponding users', () => {
-    cy.get('.UserInfo').should('have.length', 20);
-    cy.get('.UserInfo').eq(0).should('have.text', 'Leanne Graham');
-    cy.get('.UserInfo').eq(19).should('have.text', 'Clementina DuBuque');
+    cy.get('.UserInfo').should('have.length', 21);
+    cy.get('.UserInfo').eq(0).should('have.text', 'Polina Vafik');
+    cy.get('.UserInfo').eq(20).should('have.text', 'Clementina DuBuque');
   });
 
   it('should add corresponding comments', () => {
-    cy.get('.PostInfo').eq(0).find('.CommentInfo').should('have.length', 5);
-    cy.get('.PostInfo').eq(1).find('.CommentInfo').should('not.exist');
-    cy.get('.PostInfo').eq(2).find('.CommentInfo').should('have.length', 3);
+    cy.get('.PostInfo').eq(1).find('.CommentInfo').should('have.length', 5);
+    cy.get('.PostInfo').eq(2).find('.CommentInfo').should('not.exist');
+    cy.get('.PostInfo').eq(3).find('.CommentInfo').should('have.length', 3);
   });
 });
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,13 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo
+        post={post}
+        comments={post.comments}
+        key={post.id}
+      />
+    ))}
+  </div>
 );
