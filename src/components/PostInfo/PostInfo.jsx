2f7549ffc9a77df888012bc0b0620afae64ffeff
andrew gidrovich <mr.androvich55555@gmail.com>
2023-08-23 16:43:52 +0300

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,25 @@
-export const PostInfo = () => (
-  <>Put the post here</>
+import React from 'react';
+import UserInfo from '../UserInfo/UserInfo';
+import CommentList from '../CommentList/CommentList';
+import './PostInfo.scss';
+
+const PostInfo = ({ post, user, comments }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      <p>
+        {' Posted by '}
+        <UserInfo user={user} />
+      </p>
+    </div>
+    <p className="PostInfo__body">{post.body}</p>
+    <hr />
+    {comments.length > 0 ? (
+      <CommentList comments={comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
 );
+
+export default PostInfo;
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,17 @@
-export const PostList = () => (
-  <>Put the list here</>
+import React from 'react';
+import PostInfo from '../PostInfo/PostInfo';
+
+const PostList = ({ posts, comments, users }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo
+        key={post.id}
+        post={post}
+        user={users.find(user => user.id === post.userId)}
+        comments={comments.filter(comment => comment.postId === post.id)}
+      />
+    ))}
+  </div>
 );
+
+export default PostList;
