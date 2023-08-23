3a6cc6fefa5936bdcd14507f0222ca30d3930f8a
Serhii Shtokalo <Shtokalo1998@gmail.com>
2023-08-23 10:58:40 +0300

    improve code

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -4,7 +4,7 @@ import { CommentInfo } from '../CommentInfo';
 
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
-    {comments.length === 0 ? (
+    {comments.length ? (
       <b data-cy="NoCommentsMessage">No comments yet</b>
     ) : (
       comments.map(comment => <CommentInfo comment={comment} />)
