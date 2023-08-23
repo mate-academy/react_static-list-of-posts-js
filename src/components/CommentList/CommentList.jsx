894e67d03887b36ff87ab1425a808606335501ef
Roman Yatsykovych <55942779+yar0man@users.noreply.github.com>
2023-08-23 14:55:49 +0300

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,17 @@
-export const CommentList = () => (
-  <>Put the list here</>
-);
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  const { id } = comments;
+
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo
+          comment={comment}
+          key={id}
+        />
+      ))}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,16 @@
-export const PostList = () => (
-  <>Put the list here</>
-);
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => {
+  const { id } = posts;
+
+  return (
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo
+          post={post}
+          key={id}
+        />
+      ))}
+    </div>
+  );
+};
