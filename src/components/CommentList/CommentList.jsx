3c038d8231c5cb1fc98d4feac5cfd2cf0fb55dd3
Dmytro Hrynchyi <desevoker@gmail.com>
2023-08-25 19:20:46 +0300

    update condition

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -2,10 +2,8 @@ import './CommentList.scss';
 
 import { CommentInfo } from '../CommentInfo';
 
-export const CommentList = ({ comments }) => {
-  const hasComments = comments.length > 0;
-
-  return (hasComments ? (
+export const CommentList = ({ comments }) => (
+  comments.length ? (
     <div className="CommentList">
       {comments.map(comment => (
         <CommentInfo comment={comment} key={comment.id} />
@@ -16,5 +14,5 @@ export const CommentList = ({ comments }) => {
       <hr />
       <b data-cy="NoCommentsMessage">No comments yet</b>
     </>
-  ));
-};
+  )
+);
