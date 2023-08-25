a15771dc3cba91a9cfa545ed1880db9f2e5ceb51
vanom <envinenvin@gmail.com>
2023-08-25 10:06:56 +0300

    add task solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,34 @@
-export const CommentInfo = () => (
-  <>Put the comment here</>
-);
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => {
+  const {
+    title,
+    email,
+    body,
+    name,
+  } = comment;
+
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        {title}
+        <strong className="CommentInfo__name">
+          {name}
+        </strong>
+
+        {` by `}
+
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${email}`}
+        >
+          {email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        {body}
+      </div>
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,13 @@
-export const PostList = () => (
-  <>Put the list here</>
-);
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => {
+  const { id } = posts;
+
+  return (
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo post={post} key={id} />
+      ))}
+    </div>
+  );
+};
