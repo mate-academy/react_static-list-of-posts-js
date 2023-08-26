e265b303b8f513bb83f17fe9a5f7fac92934643a
Anton Bilous <toxanteloxran@gmail.com>
2023-08-26 16:14:17 +0300

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,8 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comment = [] }) => (
+  <div className="CommentList">
+    {comment === [] ? null : <CommentInfo comment={comment} key={comment.id} />}
+  </div>
 );
