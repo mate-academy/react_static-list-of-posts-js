a375f21e031c2a52f9ca4158c7991deab04b731e
Vladyslav Liakh <vladislavliakh@gmail.com>
2023-08-29 22:23:02 +0300

    list of post

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,47 @@
-export const CommentInfo = () => (
-  <>Put the comment here</>
+export const CommentInfo = ({ comment }) => (
+
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">
+        {comment.name}
+      </strong>
+
+      {' by '}
+
+      <a
+        className="CommentInfo__email"
+        href={`mailto:${comment.email}`}
+      >
+        {comment.email}
+      </a>
+    </div>
+
+    <div className="CommentInfo__body">
+      {comment.body}
+    </div>
+  </div>
 );
+
+// export const CommentInfo = ({ todo }) => (
+//   <div className="CommentInfo">
+//     <div className="CommentInfo__title">
+//       <strong className="CommentInfo__name">
+//         {/* {todo.comment && todo.comment.name} */}
+//         {todo.comment && todo.comment.map(el => el.name)}
+//       </strong>
+
+//       {' by '}
+
+//       <a
+//         className="CommentInfo__email"
+//         href={todo.comment && todo.comment.map(el => el.email)}
+//       >
+//         {todo.comment && todo.comment.map(el => el.email)}
+//       </a>
+//     </div>
+
+//     <div className="CommentInfo__body">
+//       {todo.comment && todo.comment.map(el => el.body)}
+//     </div>
+//   </div>
+// );
