005470e7e32f434082e78d5c1e812a4b0f641fea
Andriy Monko <Andriy17monko@gmail.com>
2023-08-30 16:42:28 +0300

    pt 3

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,23 +1,24 @@
-import './CommentList.scss';
+import "./CommentList.scss";
 
 export const CommentList = ({ comments }) => (
   <div>
-    {comments?.map(comment => (
-      <div className="CommentList">
-        <div className="CommentInfo">
-          <div className="CommentInfo__title">
-            <strong className="CommentInfo__name">{comment.name}</strong>
-            {' by '}
-            <a
-              className="CommentInfo__email"
-              href="mailto:Telly_Lynch@karl.co.uk"
-            >
-              {comment.email}
-            </a>
+    {!!comments.length ? (
+      comments.map((comment) => (
+        <div className="CommentList">
+          <div className="CommentInfo">
+            <div className="CommentInfo__title">
+              <strong className="CommentInfo__name">{comment.name}</strong>
+              {" by "}
+              <a className="CommentInfo__email" href={comment.email}>
+                {comment.email}
+              </a>
+            </div>
+            <div className="CommentInfo__body">{comment.body}</div>
           </div>
-          <div className="CommentInfo__body">{comment.body}</div>
         </div>
-      </div>
-    ))}
+      ))
+    ) : (
+      <strong data-cy="NoCommentsMessage" >No comments yet</strong>
+    )}
   </div>
 );
