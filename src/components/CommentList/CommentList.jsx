23e84da0ac8d0c0a0dadae4217f10cfbe8d0003e
UlianaChorna <110450537+UlianaChorna@users.noreply.github.com>
2023-08-31 14:16:52 +0300

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -4,7 +4,6 @@ export const CommentList = ({ comments }) => (
   <div className="CommentList">
     {comments.map(comment => (
       <CommentInfo comment={comment} key={comment.id} />
-    ))
-    }
+    ))}
   </div>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,23 +1,32 @@
 import { CommentList } from '../CommentList/CommentList';
 import { UserInfo } from '../UserInfo/UserInfo';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.info}</h3>
-      <p>
-        {' Posted by  '}
-        <UserInfo user={post.user} />
+export const PostInfo = ({ post }) => {
+  const { title, body, user, comments } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        {user && (
+          <p>
+            {' Posted by  '}
+
+            <UserInfo user={user} />
+          </p>
+        )}
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
+
+      {comments && comments.length
+        ? <CommentList comments={comments} />
+        : <b data-cy="NoCommentsMessage">No comments yet</b>}
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-
-    <hr />
-    {post.comments.length
-      ? <CommentList comments={post.comments} />
-      : <b data-cy="NoCommentsMessage">No comments yet</b>}
-  </div>
-);
+  );
+};
