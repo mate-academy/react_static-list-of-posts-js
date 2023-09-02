00f0483dfc490c7763de78729aaa701d2ccaba8b
Bohdan Shumylo <injobs.kharkiv@gmail.com>
2023-09-02 09:17:15 +0300

    Fix Solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,20 +1,22 @@
 export const CommentInfo = ({ comment }) => (
-  <div className="CommentInfo">
-    <div className="CommentInfo__title">
-      <strong className="CommentInfo__name">{comment.name}</strong>
+  <span>
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{comment.name}</strong>
 
-      {' by '}
+        {' by '}
 
-      <a
-        className="CommentInfo__email"
-        href={`mailto:${comment.email}`}
-      >
-        {comment.email}
-      </a>
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${comment.email}`}
+        >
+          {comment.email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        {comment.body}
+      </div>
     </div>
-
-    <div className="CommentInfo__body">
-      {comment.body}
-    </div>
-  </div>
+  </span>
 );
