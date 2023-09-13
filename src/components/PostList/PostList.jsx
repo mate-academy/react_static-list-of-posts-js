e8b65989889f972eeac7d8ace0a1da5f6c67b2fa
Vladislav Balaban <vlad.balaban2020@gmail.com>
2023-09-13 17:08:50 +0300

    Added static list of posts

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,26 @@
+import { PostInfo } from '../PostInfo';
+import postsFromServer from '../../api/posts.json';
+import commentsFromServer from '../../api/comments.json';
+import usersFromServer from '../../api/users.json';
+
+function filteredCommentsById(postId) {
+  return commentsFromServer.filter(comment => comment.postId === postId);
+}
+
+function getUserByUserId(userId) {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+const posts = postsFromServer.map(post => ({
+  ...post,
+  user: getUserByUserId(post.userId),
+  comments: filteredCommentsById(post.id),
+}));
+
 export const PostList = () => (
-  <>Put the list here</>
+  <ul className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </ul>
 );
