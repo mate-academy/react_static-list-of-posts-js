2b22c3367d1aaefd2153fc01ccb0340f11004662
FurArt <iiionerart@gmail.com>
2023-09-15 22:56:56 +0300

    solution added

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,48 @@
-export const PostInfo = () => (
-  <>Put the post here</>
-);
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+import './PostInfo.scss';
+
+function checkComments(comment) {
+  if (!comment.length) {
+    return (
+      <>
+        <hr />
+
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+
+    );
+  }
+
+  return (
+    <CommentList
+      comments={comment}
+    />
+  );
+}
+
+export const PostInfo = ({ post }) => {
+  const { body, comments, title, user } = post;
+
+  // console.log(title, body, comment, user);
+
+  return (
+    <>
+      <div className="PostInfo">
+        <div className="PostInfo__header">
+          <h3 className="PostInfo__title">{title}</h3>
+
+          <p>
+            {' Posted by  '}
+            <UserInfo user={user} />
+          </p>
+        </div>
+
+        <p className="PostInfo__body">{body}</p>
+
+        {checkComments(comments)}
+
+      </div>
+    </>
+  );
+};
