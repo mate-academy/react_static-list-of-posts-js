41a0f046e2dad06846f3f8e4d97aa72c00a4d2e5
Aleksandr <kartanovaleks@gmail.com>
2023-09-17 18:57:23 +0300

    Fixed solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -3,6 +3,11 @@ import './CommentList.scss';
 
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
-    {comments.map(comment => <CommentInfo comment={comment} />)}
+    {comments.map(comment => (
+      <CommentInfo
+        key={comment.id}
+        comment={comment}
+      />
+    ))}
   </div>
 );
