087bf974e210688b097ac5fa81cfec62c57f25b7
Astra09 <sichkarstepan09@gmail.com>
2023-09-19 17:50:27 +0300

    added deconstruct

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,20 +1,20 @@
-export const CommentInfo = ({ comment }) => (
+export const CommentInfo = ({ name, email, body }) => (
   <div className="CommentInfo">
     <div className="CommentInfo__title">
-      <strong className="CommentInfo__name">{comment.name}</strong>
+      <strong className="CommentInfo__name">{name}</strong>
 
       {' by '}
 
       <a
         className="CommentInfo__email"
-        href={`mailto:${comment.email}`}
+        href={`mailto:${email}`}
       >
-        {comment.email}
+        {email}
       </a>
     </div>
 
     <div className="CommentInfo__body">
-      {comment.body}
+      {body}
     </div>
   </div>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,24 +2,24 @@ import './PostInfo.scss';
 import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 
-export const PostInfo = ({ post }) => (
+export const PostInfo = ({ title, user, body, comments }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+      <h3 className="PostInfo__title">{title}</h3>
 
       <p>
         {' Posted by  '}
 
-        <UserInfo user={post.user} />
+        <UserInfo {...user} key={user.id} />
       </p>
     </div>
 
     <p className="PostInfo__body">
-      {post.body}
+      {body}
     </p>
 
-    {post.comments.length > 0
-      ? <CommentList comments={post.comments} />
+    {comments && comments.length > 0
+      ? <CommentList comments={comments} />
       : <b data-cy="NoCommentsMessage">No comments yet</b>}
   </div>
 );
