09c67d610d91e7acdfa0f48488cb9ddfec2a6680
Sasha Viun <sashavjun22@gmail.com>
2023-09-23 15:05:34 +0300

    Solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,9 @@
-export const UserInfo = () => (
-  <>Put the user here</>
+export const UserInfo = ({ user }) => (
+  user.name
+  && user.email
+  && (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+  )
 );
