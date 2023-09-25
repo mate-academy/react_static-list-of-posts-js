2d6cebbda41697c810e6165631eb6a8358deec04
Denis Sizemov <96919055+Denis-Sizemov@users.noreply.github.com>
2023-09-25 18:05:30 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,7 @@ function getUserById(userId) {
 }
 
 function getCommentsById(postId) {
-  return commentsFromServer.filter(comment => comment.postId === postId)
-      || [];
+  return commentsFromServer.filter(comment => comment.postId === postId);
 }
 
 export const posts = postsFromServer.map(post => ({
