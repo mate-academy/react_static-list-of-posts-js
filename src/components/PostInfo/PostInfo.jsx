36e8c633c1aeb9f4a1e66b56b69bb1516bb07ec6
Ivan Oksom <oksom.ivan82@gmail.com>
2023-09-26 19:39:50 +0200

    Solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,25 @@
-export const PostInfo = () => (
-  <>Put the post here</>
-);
+import React from 'react';
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo';
+import { CommentInfo } from '../CommentInfo';
+
+export const PostInfo = ({ post }) => {
+  const { user, comments, title, body } = post;
+
+  return (
+    <div className="PostInfo">
+      <UserInfo user={user} />
+      <div className="PostInfo__title">{title}</div>
+      <div className="PostInfo__body">{body}</div>
+      <div className="PostInfo__comments">
+        {comments.length > 0 ? (
+          comments.map(comment => (
+            <CommentInfo key={comment.id} comment={comment} />
+          ))
+        ) : (
+          <p data-cy="NoCommentsMessage">No comments available.</p>
+        )}
+      </div>
+    </div>
+  );
+};
