71e60c8b709da1b804436dfc90b5bdfa85ea14ff
Markiian Haletskyi <haletskiy.markiuan.school91@gmail.com>
2023-09-26 17:57:37 +0200

    add solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,12 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import React from 'react';
+import './CommentList.scss'; // Import the corresponding SCSS file
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {/* Map over comments and render CommentInfo components */}
+    {comments.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ))}
+  </div>
 );
