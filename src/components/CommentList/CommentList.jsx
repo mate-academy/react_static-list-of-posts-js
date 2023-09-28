cc27537ebe7212affbeab606d16c6d4f50deedc8
dmytronykorovych <dmytro.nykorovych@gmail.com>
2023-09-28 09:02:08 +0200

    fix comments

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -2,9 +2,9 @@ import { CommentInfo } from '../CommentInfo';
 import './CommentList.scss';
 
 export const CommentList = ({ comments }) => (
-  <div className="CommentList">
+  <ul className="CommentList">
     {comments.map(comment => (
       <CommentInfo comment={comment} key={comment.id} />
     ))}
-  </div>
+  </ul>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,28 +3,32 @@ import './PostInfo.scss';
 import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo = ({ post }) => {
+  const { title, user, body, comments } = post;
 
-      <p>
-        {' Posted by  '}
+  return (
+    <li className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
 
-        <UserInfo user={post.user} />
+        <p>
+          <span> Posted by  </span>
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
-    </div>
 
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
+      <hr />
 
-    <hr />
-
-    {post.comments.length === 0
-      ? <b data-cy="NoCommentsMessage">No comments yet</b>
-      : (
-        <CommentList comments={post.comments} />
-      )}
-  </div>
-);
+      {comments.length === 0
+        ? <b data-cy="NoCommentsMessage">No comments yet</b>
+        : (
+          <CommentList comments={comments} />
+        )}
+    </li>
+  );
+};
