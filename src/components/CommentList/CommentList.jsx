784eab264ff325152e20e31c0392c2c50b263d47
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-10-05 14:53:25 +0200

    static list of props

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,16 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  comments.length > 0
+    ? (
+      <div className="CommentList">
+        {comments.map(comment => (
+          <CommentInfo comment={comment} key={comment.id} />
+        ))}
+      </div>
+    )
+    : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )
 );
