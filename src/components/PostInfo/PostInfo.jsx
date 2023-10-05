e4b22d7e6b8c726f2d6c991f40b8bb2f9ffaa088
Yuliia Bakun <bakunyuliia@gmail.com>
2023-10-05 08:48:56 +0200

    task implementation

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,47 @@
-export const PostInfo = () => (
-  <>Put the post here</>
-);
+import './PostInfo.scss';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+export const PostInfo = ({ post }) => {
+  const {
+    body,
+    title,
+    user,
+    comments,
+  } = post;
+
+  const NoCommentsMessage = () => (
+    <div
+      className="NoCommentsMessage"
+      style={{ fontWeight: 'bold', borderTop: '2px solid #ccc' }}
+      data-cy="NoCommentsMessage"
+    >
+      <p>No comments yet</p>
+    </div>
+  );
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
+      </p>
+
+      {comments.length !== 0
+        ? <CommentList comments={comments} />
+        : <NoCommentsMessage />
+      }
+    </div>
+  );
+};
