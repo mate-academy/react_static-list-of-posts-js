bc57088b1a5b9a569a23915ecbd2bf6817c89532
Lubomur Chuchvara <lubomircucvara@gmail.com>
2023-10-06 18:03:56 +0300

    done2

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -7,10 +7,9 @@ export const CommentInfo = ({ comment }) => (
       <strong className="CommentInfo__name">{comment.name}</strong>
       {' by '}
       <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
-        {comment.name}
+        {comment.email}
       </a>
     </div>
-
     <div className="CommentInfo__body">{comment.body}</div>
   </div>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -6,22 +6,16 @@ import { UserInfo } from '../UserInfo';
 export const PostInfo = ({ post }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
-      <h3 className="PostInfo__title">
-        {post.title}
-      </h3>
-
+      <h3 className="PostInfo__title">{post.title}</h3>
       <p>
-        {' Posted by  '}
+        {' Posted by '}
         <UserInfo user={post.user} />
       </p>
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
+    <p className="PostInfo__body">{post.body}</p>
     <hr />
     {post.comments.length === 0 ? (
-      <b data-cy="NoCommentsMeassage">No comments yet</b>
+      <b data-cy="NoCommentsMessage">No comments yet</b>
     ) : (
       <CommentList comments={post.comments} />
     )}
