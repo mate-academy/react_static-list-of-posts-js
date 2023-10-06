1b443d71c33e280d7cfa9c7ab8aa7ba2f4483c51
Lubomur Chuchvara <lubomircucvara@gmail.com>
2023-10-06 17:54:09 +0300

    done

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,16 @@
-export const CommentInfo = () => (
-  <>Put the comment here</>
+import React from 'react';
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}</strong>
+      {' by '}
+      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+        {comment.name}
+      </a>
+    </div>
+
+    <div className="CommentInfo__body">{comment.body}</div>
+  </div>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,15 @@
-export const PostList = () => (
-  <>Put the list here</>
+import React from 'react';
+import { PostInfo } from '../PostInfo';
+import './PostList.scss';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(postWithComments => (
+      <PostInfo
+        post={postWithComments}
+        key={postWithComments.id}
+
+      />
+    ))}
+  </div>
 );
