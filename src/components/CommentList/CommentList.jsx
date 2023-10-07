36a65fd1721b66abf5c71956518e5602bcbef9c5
Ivan Baranovskyi <103584840+ivan-baranovskyi@users.noreply.github.com>
2023-10-07 16:37:10 +0200

    created static list

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,35 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ))}
+  </div>
 );
+
+// import peopleFromServer from './people.json';
+
+// export const App = () => (
+//   <div className="box">
+//     <h1 className="title">People table</h1>
+
+//     {peopleFromServer.map(({ name, sex, born, slug }) => (
+//       <tr key={slug}>
+//         <td>{name}</td>
+//         <td>{sex}</td>
+//         <td>{born}</td>
+//       </tr>
+//     ))}
+
+//     {peopleFromServer.map(person => (
+//       <tr key={person.slug}>
+//         <td>{person.name}</td>
+//         <td>{person.sex}</td>
+//         <td>{person.born}</td>
+//       </tr>
+//     ))}
+//   </div>
+// );
