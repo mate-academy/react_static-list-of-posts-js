d3b156049d08fb138631227c0b5fdd2f28c27cf6
Oleksandr <dedikad@gmail.com>
2023-10-08 09:40:04 +0200

    done

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,13 @@
-export const PostList = () => (
-  <>Put the list here</>
+import React from 'react';
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(postWithComm => (
+      <PostInfo
+        post={postWithComm}
+        key={postWithComm.id}
+      />
+    ))}
+  </div>
 );
