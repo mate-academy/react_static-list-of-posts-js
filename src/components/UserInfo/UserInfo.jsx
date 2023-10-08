8e5303cb188881e27d1d11d17f6bc9c60b543285
Yaroslav <slavik2607.lviv@gmail.com>
2023-10-08 18:00:15 +0300

    tests are fixed

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -7,7 +7,7 @@ export const UserInfo = ({ user }) => {
   } = user;
 
   return (
-    <a className="UserInfo" href={`${email}`}>
+    <a className="UserInfo" href={`mailto:${email}`}>
       {name}
     </a>
   );
