887809070c632cdbe6e8801333996d5e56efa0d6
Vika Levik <levika002@gmail.com>
2023-10-09 13:43:46 +0300

    created static list of posts

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,22 @@
-export const PostInfo = () => (
-  <>Put the post here</>
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+    </div>
+
+    <UserInfo user={post.user} key={post.user.id} />
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    <hr />
+
+    <CommentList comments={post.comments} />
+
+  </div>
 );
