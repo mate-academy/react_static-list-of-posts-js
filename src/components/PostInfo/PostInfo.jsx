eb4f3a1aaff1bcebb9ca71de9b8e555d17ba35e6
Illia Drach <illyadrach@gmail.com>
2023-10-09 15:49:19 +0300

    implement task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,52 @@
-export const PostInfo = () => (
-  <>Put the post here</>
-);
+import './PostInfo.scss';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+// import usersFromServer from '../../api/users.json';
+// import commentsFromServer from '../../api/comments.json';
+
+export const PostInfo = ({
+  users,
+  comments,
+  post: {
+    id,
+    userId,
+    title,
+    body,
+  },
+}) => {
+  const user = users.find(postUser => postUser.id === userId);
+  const commentsFiltered
+  = comments.filter(comment => comment.postId === id);
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {`${title}`}
+        </h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {`${body}`}
+      </p>
+
+      {commentsFiltered.length === 0
+        ? (
+          <>
+            <hr />
+
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </>
+        )
+        : <CommentList comments={commentsFiltered} />
+      }
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,15 @@
-export const PostList = () => (
-  <>Put the list here</>
+import './PostList.scss';
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts, comments, users }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo
+        post={post}
+        comments={comments}
+        users={users}
+        key={post.id}
+      />
+    ))}
+  </div>
 );
