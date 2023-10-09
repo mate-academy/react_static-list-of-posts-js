3a49026e998adf38058c624f85189b26238d8e96
getsi <getsize1144@gmail.com>
2023-10-09 19:38:56 +0300

    Completed the task

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,7 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  comments.map(comment => (
+    <CommentInfo comment={comment} />
+  ))
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,11 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { CommentList } from '../CommentList';
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => (
+  posts.map(post => (
+    <>
+      <PostInfo key={post.id} post={post} />
+      <CommentList comments={post.comments} />
+    </>
+  ))
 );
