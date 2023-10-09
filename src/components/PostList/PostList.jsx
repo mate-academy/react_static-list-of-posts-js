3910f1c4c82fb73e9adfb23b7929c644c3de550f
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2023-10-09 21:48:46 +0300

    Static posts -- all tests passed

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,8 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts = [] }) => (
+  <div className="PostList">
+    {posts.length > 0
+      && posts.map(post => <PostInfo post={post} key={post.id} />)}
+  </div>
 );
