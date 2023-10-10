65add8c411f23dff3fb6003edf0892bf2d60e8f2
Daniil <danil.beshlebnov@gmail.com>
2023-10-10 18:06:34 +0300

    solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,40 @@
-export const PostInfo = () => (
-  <>Put the post here</>
+import { UserInfo } from '../UserInfo';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo" key={post.id}>
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <UserInfo user={post.user} />
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
+    {post.comments.length > 0 ? (
+      post.comments.map(comment => (
+        <div className="Comment" key={comment.id}>
+          <p className="Comment__body">{comment.body}</p>
+
+          <p>
+            {' Commented by  '}
+
+            <a
+              className="UserInfo"
+              href={`mailto:${comment.email}`}
+            >
+              {comment.name}
+            </a>
+          </p>
+        </div>
+      ))
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
 );
