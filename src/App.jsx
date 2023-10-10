27b87e03a15e2bcd8ee68d0a6107ad828fc63e72
Vika Levik <levika002@gmail.com>
2023-10-10 09:55:01 +0300

    fixed pr comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import './App.scss';
-
-import postsFromServer from './api/posts.json';
-import commentsFromServer from './api/comments.json';
-import usersFromServer from './api/users.json';
+import { PreparedPosts } from './components/PreparedPosts';
 import { PostList } from './components/PostList';
-
-const preparedPosts = postsFromServer.map((post) => {
-  const user = usersFromServer.find(u => post.userId === u.id);
-  const comments = commentsFromServer.filter(comment => post.id
-  === comment.postId);
-
-  return {
-    ...post,
-    user,
-    comments,
-  };
-});
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <PostList posts={preparedPosts} />
+    <PostList posts={PreparedPosts} />
   </section>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -13,7 +13,7 @@ export const PostInfo = ({ post: {
       <h3 className="PostInfo__title">{title}</h3>
     </div>
 
-    <UserInfo user={user} key={user.id} />
+    <UserInfo user={user} />
 
     <p className="PostInfo__body">
       {body}
