ee71ba1452ce0a77aa8f733813c20ab506398b94
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-10-20 13:12:19 +0300

    revise PostInfo component

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -4,14 +4,12 @@ import './CommentList.scss';
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
 
-    {comments.length
-      ? (comments.map(comment => (
-        <CommentInfo
-          comment={comment}
-          key={comment.id}
-        />
-      )))
-      : (<b data-cy="NoCommentsMessage">No comments yet</b>)
+    {comments.map(comment => (
+      <CommentInfo
+        comment={comment}
+        key={comment.id}
+      />
+    ))
     }
   </div>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -6,9 +6,12 @@ export const PostInfo = ({ post }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
-      <UserInfo
-        user={post.user}
-      />
+      {post.user
+        && (
+        <UserInfo
+          user={post.user}
+        />
+        )}
     </div>
 
     <p className="PostInfo__body">
@@ -17,9 +20,14 @@ export const PostInfo = ({ post }) => (
 
     <hr />
 
-    <CommentList
-      comments={post.comments}
-    />
+    {post.comments.length
+      ? (
+        <CommentList
+          comments={post.comments}
+        />
+      )
+      : (<b data-cy="NoCommentsMessage">No comments yet</b>)
+    }
 
   </div>
 );
