3c9b50ef80a8837a375e426109357ec1e6b306b2
Kateryna Sydorenko <katerynnnna@gmail.com>
2023-10-22 19:53:30 +0300

    task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,19 @@
-export const CommentList = () => (
-  <>Put the list here</>
-);
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => {
+  if (comments.length === 0) {
+    return (
+      <>
+        <hr />
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+    );
+  }
+
+  return comments.map(comment => (
+    <div className="CommentList">
+      <CommentInfo comment={comment} key={comment.id} />
+    </div>
+  ));
+};
