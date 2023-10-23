a9caf2ae5810ef7149917783ccc3a7732669283b
Ivan Orinchak <124816409+IvanOrinchak@users.noreply.github.com>
2023-10-23 19:01:11 +0300

    react_static-list-of-posts-js

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,14 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ postL }) => (
+  <>
+    <div className="PostList">
+      {postL.map(post => (
+        <PostInfo
+          post={post}
+          key={post.id}
+        />
+      ))}
+    </div>
+  </>
 );
