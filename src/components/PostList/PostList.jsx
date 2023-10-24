f825ec7d3a933fe8b37395eada76aefe051d1ecd
Yaroslav Bachynskyi <yarkoo@Yaroslavs-MacBook-Pro.local>
2023-10-24 15:09:53 +0300

    add task 5

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,9 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => (
+  <section key={posts.id} className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} />
+    ))}
+  </section>
 );
