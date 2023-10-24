72caf2d1b3d1ecfbcf9b3bb22f24aef07695f60c
Olena Matsyshyna <helenmarth@Olenas-MacBook-Air.local>
2023-10-24 11:25:25 +0300

    fixed destructuring

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,25 +2,25 @@ import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
+export const PostInfo = ({ post: { title, user, body, comments } }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+      <h3 className="PostInfo__title">{title}</h3>
 
       <p>
         {' Posted by '}
-        {post.user && <UserInfo user={post.user} key={post.id} />}
+        {user && <UserInfo user={user} />}
       </p>
     </div>
 
     <p className="PostInfo__body">
-      {post.body}
+      {body}
     </p>
 
     <hr />
 
-    {post.comments.length > 0
-      ? <CommentList comments={post.comments} />
+    {comments.length > 0
+      ? <CommentList comments={comments} />
       : <b data-cy="NoCommentsMessage">No comments yet</b>}
   </div>
 );
