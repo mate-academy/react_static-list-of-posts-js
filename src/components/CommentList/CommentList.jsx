790cee37f7b8a4a29548f08ae10341042f49a229
Serhii Dranuta <sirojka8@gmail.com>
2023-10-25 20:14:26 +0300

    Add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,13 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <>
+    <div className="CommentList">
+      {comments.map(eachComment => (
+        <CommentInfo comment={eachComment} key={eachComment.id} />
+      ))}
+    </div>
+  </>
 );
