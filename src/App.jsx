fc85a252d2a52452afeee4f2e459631fed65a5a3
getsi <getsize1144@gmail.com>
2023-10-25 17:34:41 +0300

    Made Changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,6 @@ export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <div className="PostList">
-      <PostList posts={posts} />
-    </div>
+    <PostList posts={posts} />
   </section>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,11 +1,10 @@
-import { CommentList } from '../CommentList';
+import React from 'react';
 import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts }) => (
   posts.map(post => (
-    <>
-      <PostInfo key={post.id} post={post} />
-      <CommentList comments={post.comments} />
-    </>
+    <div className="PostList" key={post.id}>
+      <PostInfo post={post} />
+    </div>
   ))
 );
