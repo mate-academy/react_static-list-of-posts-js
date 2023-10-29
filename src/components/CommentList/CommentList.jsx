c28a3f6ffe3bfaa422652608ad923048ff456bc2
Denys Vasylenkoe <kapodenim@gmail.com>
2023-10-29 15:27:41 +0200

    v1

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,20 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+function checkComments(comments) {
+  if (comments.length !== 0) {
+    return (
+      comments.map(comment => (
+        <div className="CommentList">
+          <CommentInfo comment={comment} key={comment.id} />
+        </div>
+      ))
+    );
+  }
+
+  return (<b data-cy="NoCommentsMessage">No comments yet</b>);
+}
+
+export const CommentList = ({ comments }) => (
+  checkComments(comments)
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,13 @@
-export const UserInfo = () => (
-  <>Put the user here</>
+function checkUser(user) {
+  if (user !== undefined) {
+    return `${user}`;
+  }
+
+  return '';
+}
+
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${checkUser(user.email)}`}>
+    {checkUser(user.name)}
+  </a>
 );
