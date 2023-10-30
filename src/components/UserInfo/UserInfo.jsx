5f5e7a8a51dd7ef0844fc726bd3078a0bce51760
Albert Kifiuk <albertkifiuk@gmail.com>
2023-10-30 16:29:03 +0200

    add task solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,9 @@
 import './UserInfo.scss';
 
 export const UserInfo = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user.email}`}>
-    {user.name}
-  </a>
+  user && user.email ? (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  ) : null
 );
