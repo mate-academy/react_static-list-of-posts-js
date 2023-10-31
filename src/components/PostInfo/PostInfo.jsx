c93935f1568b10d6f0f35d09be91a81a9ae6744f
Andriy Yurkiv <88943977+andriy-yurkiv@users.noreply.github.com>
2023-10-31 13:03:27 +0200

    added destructuring

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,30 +2,34 @@ import './PostInfo.scss';
 import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">
-        {post.title}
-      </h3>
+export const PostInfo = ({ post }) => {
+  const { title, body, comments, user } = post;
 
-      <p>
-        {' Posted by  '}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
 
-        <UserInfo user={post.user} />
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
+      {comments.length === 0 ? (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      ) : (
+        <CommentList comments={comments} />
+      )}
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-    {post.comments.length === 0 ? (
-      <>
-        <hr />
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      </>
-    ) : (
-      <CommentList comments={post.comments} />
-    )}
-  </div>
-);
+  );
+};
