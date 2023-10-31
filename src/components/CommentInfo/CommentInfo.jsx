fa34576eeaa40c1fcc55b89c8a5a61c5869daa40
Alina Kabanets <kabanets.dolls@gmail.com>
2023-10-31 10:28:17 +0000

    Fixed

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,24 +1,20 @@
-import './CommentInfo.scss';
-
 export const CommentInfo = ({ comment }) => (
-  <>
-    <div className="CommentInfo">
-      <div className="CommentInfo__title">
-        <strong className="CommentInfo__name">{comment.name}</strong>
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}</strong>
 
-        {' by '}
+      {' by '}
 
-        <a
-          className="CommentInfo__email"
-          href={`mailto:${comment.email}`}
-        >
-          {comment.email}
-        </a>
-      </div>
-
-      <div className="CommentInfo__body">
-        {comment.body}
-      </div>
+      <a
+        className="CommentInfo__email"
+        href={`mailto:${comment.email}`}
+      >
+        {comment.email}
+      </a>
     </div>
-  </>
+
+    <div className="CommentInfo__body">
+      {comment.body}
+    </div>
+  </div>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -2,11 +2,9 @@ import './PostList.scss';
 import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts }) => (
-  <>
-    <div className="PostList">
-      {posts.map(post => (
-        <PostInfo post={post} key={post.id} />
-      ))}
-    </div>
-  </>
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </div>
 );
