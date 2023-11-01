012b47d1fd4f1ee2ddd27bd04c63723bedfa7e71
Olena Shapkun <shapkunkh@gmail.com>
2023-11-01 20:57:34 +0200

    static posts list

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,28 @@
-export const PostInfo = () => (
-  <>Put the post here</>
+import { CommentInfo } from '../CommentInfo';
+import { UserInfo } from '../UserInfo';
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      <p>
+        {' Posted by '}
+
+        <UserInfo user={post.user} />
+      </p>
+    </div>
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+    {post.comments.length > 0 ? (
+      <div className="CommentList">
+        {post.comments.map(comment => (
+          <CommentInfo key={comment.id} comment={comment} />
+        ))}
+      </div>
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
 );
