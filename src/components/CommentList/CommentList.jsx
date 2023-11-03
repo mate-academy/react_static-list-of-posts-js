401db839b838ce89e9114e06be673f7910b07570
Sasha <sashakozoriz2012@gmail.com>
2023-11-03 03:05:49 +0200

    Done

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,28 @@
-export const CommentList = () => (
-  <>Put the list here</>
-);
+import { CommentInfo } from '../CommentInfo';
+
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => {
+  if (comments.length > 0) {
+    return (
+      <>
+        <div className="CommentList">
+          {comments.map(comment => (
+            <CommentInfo
+              key={comment.id}
+              comment={comment}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <hr />
+
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    </>
+  );
+};
