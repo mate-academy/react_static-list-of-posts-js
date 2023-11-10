c0d380fa8fe0823ce08082cb48e3e01e88fee198
Rostyslav Sobchyshyn <rosta2604@icloud.com>
2023-11-10 11:09:57 +0000

    Revert "added objects destruction"
    This reverts commit d3b16070e4847f7ce95641f089e98da1ac01116f.

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,11 +1,7 @@
 import './UserInfo.scss';
 
-export const UserInfo = ({ user }) => {
-  const { name, email } = user;
-
-  return (
-    <a className="UserInfo" href={`mailto:${email || ''}`}>
-      {name}
-    </a>
-  );
-};
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+);
