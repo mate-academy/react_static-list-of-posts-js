ef76a3172a0494f0a389a94a534db738a0d981cb
Анастасия Бурковская <burkovskaya.anastasiya@gmail.com>
2023-11-11 19:44:54 +0200

    solution_listPosts

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,15 +1,13 @@
 import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
+import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => {
   const { user, comments, title, body } = post;
 
   return (
     <>
-      <div
-        className="PostInfo"
-      >
-
+      <div className="PostInfo">
         <div className="PostInfo__header">
           <h3 className="PostInfo__title">{title}</h3>
 
