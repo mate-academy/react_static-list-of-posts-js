fc7504fef4c22a53ab843026510b1200736da9a4
Svyatoslav <svyatoslavkolomoets@gmail.com>
2023-11-11 19:13:52 +0200

    add solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,10 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo comment={comment} key={comment.id} />
+    ))}
+    {!comments.length && <b data-cy="NoCommentsMessage">No comments yet</b>}
+  </div>
 );
