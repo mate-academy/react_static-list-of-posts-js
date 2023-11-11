5b3dbdb73eeb750bc76662a8166b025971bc812f
Анастасия Бурковская <burkovskaya.anastasiya@gmail.com>
2023-11-11 19:31:51 +0200

    static_list_first

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,38 @@
-export const PostInfo = () => (
-  <>Put the post here</>
-);
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+export const PostInfo = ({ post }) => {
+  const { user, comments, title, body } = post;
+
+  return (
+    <>
+      <div
+        className="PostInfo"
+      >
+
+        <div className="PostInfo__header">
+          <h3 className="PostInfo__title">{title}</h3>
+
+          <p>
+            {' Posted by  '}
+
+            <UserInfo user={user} />
+          </p>
+        </div>
+
+        <p className="PostInfo__body">
+          {body}
+        </p>
+
+        <hr />
+        {post.comments.length > 0 ? (
+          <CommentList comments={comments} />
+        ) : (
+          <b data-cy="NoCommentsMessage">
+            No comments yet
+          </b>
+        )}
+      </div>
+    </>
+  );
+};
