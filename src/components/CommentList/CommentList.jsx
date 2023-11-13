d25b819d75b88f8cbdf5a85cd9189b632060fd68
Hovera-Liubomyr <ljusuk@gmail.com>
2023-11-13 20:00:42 +0200

    react-static-list-of-posts

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,13 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import React from 'react';
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <ul className="CommentList">
+    {comments.map(comment => (
+      <li key={comment.id}>
+        <CommentInfo comment={comment} />
+      </li>
+    ))}
+  </ul>
 );
