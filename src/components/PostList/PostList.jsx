451e2a3594a9401a3e106d71b37157789626e77b
Malyholovka Ostap <maligolovkaostap@gmail.com>
2023-11-20 15:36:31 +0200

    add task solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,12 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ serverPosts }) => (
+  <div className="PostList">
+    {serverPosts.map(post => (
+      <PostInfo
+        post={post}
+        key={post.id}
+      />
+    ))}
+  </div>
 );
