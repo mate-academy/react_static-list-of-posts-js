73f20f20b7cfc39954664809c260643f7941baf7
olegnovikov10 <relaxinfh071@gmail.com>
2023-11-22 14:18:42 +0200

    task done

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,21 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />))}
+
+    {comments.length === 0 && (
+      <>
+        <hr />
+
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+
+      </>
+    )
+    }
+
+  </div>
+
 );
