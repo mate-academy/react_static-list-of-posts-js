cbdaf8dffb947fc1e75afc922a14c0116bcc6449
Egor <yehorheneralov@gmail.com>
2023-11-24 10:46:06 +0200

    removed no needed code

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -2,9 +2,6 @@ import './UserInfo.scss';
 
 export const UserInfo = ({ user }) => {
   const { email, name } = user;
-  // if (!user) {
-  //   return null;
-  // }
 
   return (
     <a className="UserInfo" href={`mailto:${email}`}>
