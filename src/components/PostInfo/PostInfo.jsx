a9a0d580dcad141e5b4366f5d5202cc720af737b
Stanislav Kohut <stako211@gmail.com>
2023-11-27 13:10:00 +0200

    added destruction for post, user, comment

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,31 +2,35 @@ import './PostInfo.scss';
 import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">
-        {post.title}
-      </h3>
+export const PostInfo = ({ post }) => {
+  const { title, user, body, comments } = post;
 
-      <p>
-        {' Posted by  '}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
 
-        <UserInfo user={post.user} />
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
+
+      {comments.length !== 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )}
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-
-    {post.comments.length !== 0 ? (
-      <CommentList comments={post.comments} />
-    ) : (
-      <>
-        <hr />
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      </>
-    )}
-  </div>
-);
+  );
+};
