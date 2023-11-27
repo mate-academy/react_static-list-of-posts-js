519bb67aca0bbe944e169e0e85f00bbc07e8f806
Adeveloper <feduniy@gmail.com>
2023-11-27 23:28:47 +0200

    delete some fragments

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,31 +3,33 @@ import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
 
 export const PostInfo = ({ post }) => (
-  <>
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">
-          {post.title}
-        </h3>
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">
+        {post.title}
+      </h3>
 
-        <UserInfo user={post.user} key={post.user.id} />
-      </div>
-
-      <p className="PostInfo__body">
-        {post.body}
-      </p>
-
-      {post.comments.length > 0
-        ? (
-          <CommentList comments={post.comments} />
-        )
-        : (
-          <>
-            <hr />
-            <b data-cy="NoCommentsMessage">No comments yet</b>
-          </>
-        )}
-
+      {post.user && (
+        <UserInfo
+          user={post.user}
+        />
+      )}
     </div>
-  </>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    {post.comments.length > 0
+      ? (
+        <CommentList comments={post.comments} />
+      )
+      : (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )}
+
+  </div>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,13 +1,11 @@
 import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts }) => (
-  <>
-    <div className="PostList">
-      {
-        posts.map(post => (
-          <PostInfo post={post} key={post.id} />
-        ))
-      }
-    </div>
-  </>
+  <div className="PostList">
+    {
+      posts.map(post => (
+        <PostInfo post={post} key={post.id} />
+      ))
+    }
+  </div>
 );
