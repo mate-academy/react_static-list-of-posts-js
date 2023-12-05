9425fb58cc63fb6b668f17cb5fcf1354b468cfca
Krot Nikita <krotnikitaasd@gmail.com>
2023-12-05 16:35:55 +0200

    react_static-posts

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,18 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  comments && comments.length > 0
+    ? (
+      <div className="CommentList">
+        {comments.map(comment => (
+          <CommentInfo key={comment.id} comment={comment} />))}
+      </div>
+    )
+    : (
+      <>
+        <hr />
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+    )
 );
