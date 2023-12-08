4fcdd371939115324acaec000e9ffc673b9813e7
boytrend0108 <boytrend@gmail.com>
2023-12-08 15:56:29 +0200

    fix err

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -2,9 +2,7 @@ import { CommentInfo } from '../CommentInfo';
 import './CommentList.scss';
 
 export const CommentList = ({ comments }) => (
-  <>
-    <div className="CommentList">
-      {comments.map(el => <CommentInfo comment={el} key={el.id} />)}
-    </div>
-  </>
+  <div className="CommentList">
+    {comments.map(el => <CommentInfo comment={el} key={el.id} />)}
+  </div>
 );
