a9312da0e5a04ffc976a2c82bc5359690f55497f
Mykhailo Loniak <loniakmykhail@gmail.com>
2023-12-09 13:04:10 +0100

    bases

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,5 @@
-export const CommentInfo = () => (
-  <>Put the comment here</>
+export const CommentInfo = ({ comment }) => (
+  <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+    {comment.email}
+  </a>
 );
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,26 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ post }) => (
+  <>
+    {
+      post.comments && post.comments.length > 0 ? (
+        <div className="CommentList">
+          {post.comments.map(comment => (
+            <div className="CommentInfo" key={comment.id}>
+              <div className="CommentInfo__title">
+                <strong className="CommentInfo__name">
+                  {comment.name}
+                </strong>
+                {' by '}
+                <CommentInfo comment={comment} />
+              </div>
+              <div className="CommentInfo__body">{comment.body}</div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )
+    }
+  </>
 );
