539d09df75620ba7331e254e1e8213c08d50c2e6
Maksym Kostetskyi <mkost@gmail.com>
2023-12-13 11:43:29 +0200

    solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,27 +1,28 @@
-import { CommentList } from "../CommentList";
+import { CommentList } from '../CommentList';
 
 export const PostInfo = ({ post }) => {
-  const comments = post.comments.length !== 0
-    ? <CommentList comments={post.comments} />
+  const { comments, title, user, body } = post;
+  const comment = comments.length !== 0
+    ? <CommentList comments={comments} />
     : <b data-cy="NoCommentsMessage">No comments yet</b>;
 
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+        <h3 className="PostInfo__title">{title}</h3>
         <p>
           {' Posted by  '}
-          <a className="UserInfo" href={`mailto:${post.user.email}`}>
-            {post.user.name}
+          <a className="UserInfo" href={`mailto:${user.email}`}>
+            {user.name}
           </a>
         </p>
       </div>
 
       <p className="PostInfo__body">
-        {post.body}
+        {body}
       </p>
 
-      {comments}
+      {comment}
     </div>
   );
 };
