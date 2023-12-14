b3ca0572a9ff71b2eb3e5e6cad649e3cf53d09fb
Kriss Kozak <misskrissretro@gmail.com>
2023-12-14 18:47:46 +0200

    react_static-list-of-posts-js

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -8,8 +8,15 @@ export const PostInfo = ({ post }) => (
       <h3 className="PostInfo__title">{post.title}</h3>
       {post.user && <UserInfo user={post.user} />}
     </div>
+
     <p className="PostInfo__body">{post.body}</p>
+
     <hr />
-    <CommentList comments={post.comments} />
+
+    {post.comments.length > 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
   </div>
 );
