fb0e21e8ac192a3fca9c0a6e0596ef03eef399ad
Leo <you@example.com>
2023-12-16 17:34:07 +0200

    create

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,11 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(coment => (
+      <li key={coment.id}>
+        <CommentInfo coment={coment} />
+      </li>
+    ))}
+  </div>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,11 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <li key={post.id}>
+        <PostInfo post={post} />
+      </li>
+    ))}
+  </div>
 );
