cb6f81646414303c9f7de73de12f82da2c5506e9
Leonid Trotsko <leonidlikew@gmail.com>
2023-12-18 21:49:40 +0200

    used destructuring

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,11 +1,11 @@
 import { PostInfo } from '../PostInfo/PostInfo';
 
 export const PostList = ({ posts }) => (
-  <div className="PostList">
+  <ul className="PostList">
     {posts.map(post => (
       <li key={post.id}>
         <PostInfo post={post} />
       </li>
     ))}
-  </div>
+  </ul>
 );
