37047e2076f01c172cd64588bc9387a7d0efaa33
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2023-12-18 15:18:31 +0200

    Done!

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,15 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {(comments.length > 0)
+      ? (
+        comments.map(comment => <CommentInfo comment={comment} />)
+      ) : (
+        <b data-cy="NoCommentsMessage">
+          No comments yet
+        </b>
+      )
+    }
+  </div>
 );
