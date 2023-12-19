67a7aa156363ded86ba8c03dacb85525c60ad4bd
Nazar Malakhovskiy <malahovskinazar007@gmail.com>
2023-12-19 22:00:09 +0200

    solution by GTXtab

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,26 @@
-export const CommentInfo = () => (
-  <>Put the comment here</>
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => (
+  <div
+    className="CommentInfo"
+    key={comment.id}
+  >
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">
+        {comment.name}
+      </strong>
+
+      {' by '}
+
+      <a
+        className="CommentInfo__email"
+        href={`mailto:${comment.email}`}
+      >
+        {comment.email}
+      </a>
+    </div>
+    <div className="CommentInfo__body">
+      {comment.body}
+    </div>
+  </div>
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,11 @@
-export const UserInfo = () => (
-  <>Put the user here</>
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => (
+  <a
+    className="UserInfo"
+    href={`mailto:${user.email}`}
+    key={user.id}
+  >
+    {user.name}
+  </a>
 );
