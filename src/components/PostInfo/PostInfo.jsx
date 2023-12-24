8ca880ddaf2b0c4ab9f17160601cb9fb98d828f9
Misha Martynov <martunmtm@gmail.com>
2023-12-24 11:22:40 +0200

    fix_react_static

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -10,7 +10,7 @@ export const PostInfo = ({ post }) => (
       <p>
         {' Posted by  '}
 
-        <UserInfo user={post.user} />
+        {post.user && <UserInfo user={post.user} />}
 
       </p>
     </div>
