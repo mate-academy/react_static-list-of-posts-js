8105378d78ca553320e448f6eb180373adcb2d8b
Dmytrosp <dmytrosp@gmail.com>
2023-12-28 20:11:11 +0200

    solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,24 @@
-export const PostInfo = () => (
-  <>Put the post here</>
-);
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => {
+  const { title, body, user, comments } = post;
+
+  return (
+    <article className={classNames(PostInfo)}>
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+        <UserInfo user={user} />
+        <p className="PostInfo__body">{body}</p>
+        <hr />
+        {
+          comments.length !== 0
+            ? <CommentList comments={comments} />
+            : <b data-cy="NoCommentsMessage">No comments yet</b>
+        }
+      </div>
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,15 @@
-export const UserInfo = () => (
-  <>Put the user here</>
-);
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <article>
+      <p>Posted by </p>
+
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </article>
+  );
+};
