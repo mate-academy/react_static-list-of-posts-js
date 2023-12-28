77aae7fa440e9d1b69c218ffbb0ebcae5ece4538
Viktoria Moroz <viktoriaandriivna25@gmail.com>
2023-12-28 14:38:55 +0200

    task for check

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,8 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ post }) => (
+  <div className="CommentList">
+    <CommentInfo comment={post.comment} />
+  </div>
 );
