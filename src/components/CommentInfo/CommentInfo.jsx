a5f412e27b545e0a6ebd2574ca98664394fdb8ed
Viktoria Moroz <viktoriaandriivna25@gmail.com>
2024-01-02 22:10:46 +0200

    fixed issues

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,20 +1,24 @@
 import './CommentInfo.scss';
 
-export const CommentInfo = ({ comment }) => (
-  <div className="CommentInfo">
-    <div className="CommentInfo__title">
-      <strong className="CommentInfo__name">{comment.name}</strong>
-      {' by '}
-      <a
-        className="CommentInfo__email"
-        href={`mailto:${comment.email}`}
-      >
-        {comment.email}
-      </a>
-    </div>
+export const CommentInfo = ({ comment }) => {
+  const { name, email, body } = comment;
 
-    <div className="CommentInfo__body">
-      {comment.body}
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{name}</strong>
+        {' by '}
+        <a
+          className="CommentInfo__email"
+          href={`mailto:${email}`}
+        >
+          {email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        {body}
+      </div>
     </div>
-  </div>
-);
+  );
+};
