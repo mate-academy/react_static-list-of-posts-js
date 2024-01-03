0c2d68a808ccb0be37248e84443dc30ce8007da2
Vasil Liakhivskyi <vazilx@gmail.com>
2024-01-03 11:20:29 +0100

    update solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,28 +3,32 @@ import { NoCommentsMessage } from '../NoCommentsMessage';
 import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">
-        {post.title}
-      </h3>
+export const PostInfo = ({ post }) => {
+  const { title, user, comments, body } = post;
 
-      <p>
-        {' Posted by  '}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">
+          {title}
+        </h3>
 
-        <UserInfo user={post.user} />
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        {body}
       </p>
+
+      {
+        comments.length === 0
+          ? <NoCommentsMessage />
+          : <CommentList comments={comments} />
+      }
     </div>
-
-    <p className="PostInfo__body">
-      {post.body}
-    </p>
-
-    {
-      post.comments.length === 0
-        ? <NoCommentsMessage />
-        : <CommentList comments={post.comments} />
-    }
-  </div>
-);
+  );
+};
