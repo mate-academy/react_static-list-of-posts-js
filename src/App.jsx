184faf722877152d97457b33e7783efed9d01110
Andrew Rich <andy.rich3a@gmail.com>
2024-01-06 11:56:59 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import './App.scss';
+import { PostList } from './components/PostList';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+
+// extract data from json files by id and data what we get
+function getObjectById(id, data) {
+  return data.find(prop => prop.id === id)
+    || null;
+}
+
+function filterObjectById(id, data) {
+  return data.filter(prop => prop.postId === id)
+    || null;
+}
+
+export const posts = postsFromServer.map(post => ({
+  ...post,
+  user: getObjectById(post.userId, usersFromServer),
+  comments: filterObjectById(post.id, commentsFromServer),
+}));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <div className="PostList">
+    <PostList posts={posts} />
+
+    {/* <div className="PostList">
       <div className="PostInfo">
         <div className="PostInfo__header">
           <h3 className="PostInfo__title">qui est esse</h3>
@@ -102,6 +122,6 @@ export const App = () => (
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
   </section>
 );
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,18 @@
-export const CommentList = () => (
-  <>Put the list here</>
-);
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => {
+  if (!comments || comments.length === 0) {
+    return (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    );
+  }
+
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
