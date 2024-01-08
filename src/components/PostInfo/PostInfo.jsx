9ab547754dc4a33fd917fe50c9027182b4729207
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-01-08 21:42:55 +0200

    fixing issues

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -9,10 +9,10 @@ export const PostInfo = ({ post }) => {
     <div className="PostInfo">
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{title}</h3>
-        <UserInfo user={user} />
+        {user && <UserInfo user={user} />}
       </div>
       <p className="PostInfo__body">{body}</p>
-      {CommentList && <CommentList comments={comments} />}
+      <CommentList comments={comments} />
     </div>
   );
 };
