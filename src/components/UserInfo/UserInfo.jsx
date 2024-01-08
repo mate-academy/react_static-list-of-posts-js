2490bdadb89d1789483e3dbf899093193d4c2c41
Dmytro Yarechuk <retrodn14@gmail.com>
2024-01-08 02:47:11 +0200

    add task solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,19 @@
-export const UserInfo = () => (
-  <>Put the user here</>
-);
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  const {
+    email,
+    id,
+    name,
+  } = user;
+
+  return (
+    <a
+      className="UserInfo"
+      href={`mailto:${email}`}
+      key={id}
+    >
+      {name}
+    </a>
+  );
+};
