67af4a5a01c4b2c31cc4d320c52b24dd62b64b49
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-01-08 21:52:21 +0200

    added key prop to PostList

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -4,7 +4,7 @@ import { PostInfo } from '../PostInfo';
 export const PostList = ({ posts }) => (
   <div className="PostList">
     {posts.map(post => (
-      <PostInfo post={post} />
+      <PostInfo key={post.id} post={post} />
     ))}
   </div>
 );
