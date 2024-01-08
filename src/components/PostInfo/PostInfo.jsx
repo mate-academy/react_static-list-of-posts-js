ca3f01978473b4615cb5eb420c5f28d845c8e2d3
avakiel <79792776+avakiel@users.noreply.github.com>
2024-01-08 18:09:16 +0200

    fix

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,20 +2,24 @@ import './PostInfo.scss';
 import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
-      <UserInfo user={post.user} />
+export const PostInfo = ({ post }) => {
+  const { title, body, user, comments } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+        <UserInfo user={user} />
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      <hr />
+      {post.comments.length > 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
     </div>
-
-    <p className="PostInfo__body">{post.body}</p>
-
-    <hr />
-    {post.comments.length > 0 ? (
-      <CommentList comments={post.comments} />
-    ) : (
-      <b data-cy="NoCommentsMessage">No comments yet</b>
-    )}
-  </div>
-);
+  );
+};
