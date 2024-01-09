4fbce30565f95d9b1980930aba31dd071079ba88
Yevhenii <polotnyak2004@gmail.com>
2024-01-09 12:08:52 +0200

    fix comentlist

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -2,15 +2,13 @@ import { CommentInfo } from '../CommentInfo';
 import './CommentList.scss';
 
 export const CommentList = ({ comments }) => (
-  <>
-    <div className="CommentList">
-      {comments.length > 0 ? (
-        comments.map(comment => (
-          <CommentInfo comment={comment} key={comment.id} />
-        ))
-      ) : (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      )}
-    </div>
-  </>
+  <div className="CommentList">
+    {comments.length ? (
+      comments.map(comment => (
+        <CommentInfo comment={comment} key={comment.id} />
+      ))
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,22 +3,19 @@ import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => (
-  <>
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">
-          {post.title}
-        </h3>
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">
+        {post.title}
+      </h3>
 
-        <UserInfo user={post.user} />
-      </div>
-
-      <p className="PostInfo__body">
-        {post.body}
-      </p>
-
-      <CommentList comments={post.comments} />
+      <UserInfo user={post.user} />
     </div>
 
-  </>
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    <CommentList comments={post.comments} />
+  </div>
 );
