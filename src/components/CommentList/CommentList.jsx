15d6dad561d09d34efc65fc7a2757139f6282adf
Yaroslav Liutyi <slavko5481@gmail.com>
2024-01-13 15:02:04 +0200

    done

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,15 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import './CommentList.scss';
+
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  comments.length > 0 ? (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  ) : (
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  )
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,13 @@
-export const PostList = () => (
-  <>Put the list here</>
+import './PostList.scss';
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo
+        key={post.id}
+        post={post}
+      />
+    ))}
+  </div>
 );
