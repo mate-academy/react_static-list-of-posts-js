83c7339a46a07ffa8c038c972cdeb064a3a9890a
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2024-01-14 23:29:42 +0200

    add functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+
+function getCommentsByIdFromPosts(idFromPosts) {
+  return commentsFromServer.filter(comm => comm.postId === idFromPosts)
+    || null;
+}
+
+function getUsersByIdFromPosts(userId) {
+  return usersFromServer.filter(user => user.id === userId)
+    || null;
+}
+
+export const combinedPostsUsersCommArr = postsFromServer.map(post => ({
+  ...post,
+  user: getUsersByIdFromPosts(post.userId),
+  comments: getCommentsByIdFromPosts(post.id),
+}));
 
 export const App = () => (
   <section className="App">
