21ff0ab7b572c624efdb2f9ef74466b96f97c87a
Iryna Lysko <goiralysko@gmail.com>
2024-01-15 18:36:06 +0200

    split App component into Comment, Post and User components

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,3 +1,16 @@
-export const CommentList = () => (
-  <>Put the list here</>
+import { CommentInfo } from '../CommentInfo';
+
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.length === 0
+      ? (<b data-cy="NoCommentsMessage">No comments yet</b>)
+      : comments.map(comment => (
+        <CommentInfo
+          comment={comment}
+        />
+      ))
+    }
+  </div>
 );
