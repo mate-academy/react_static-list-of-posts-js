a7c3904c1c602852d5331c06793172b81b213f44
kkrasnoshchok <krasnoshchok4work@gmail.com>
2024-01-19 13:20:15 +0100

    Requested changes fixed

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,23 +1,17 @@
 import { PostInfo } from '../PostInfo';
 
-export const PostList = ({ posts, comments, users }) => (
-  <div className="PostList">
-    {posts.map((post) => {
-      const postUser = users.find(user => user.id === post.userId);
-      const postComments = comments.filter(
-        comment => comment.postId === post.id,
-      );
+export const PostList = ({ posts, comments, users }) => {
+  const extendedPosts = posts.map(post => ({
+    ...post,
+    user: users.find(user => user.id === post.userId),
+    comments: comments.filter(comment => comment.postId === post.id),
+  }));
 
-      return (
-        <PostInfo
-          key={post.id}
-          post={{
-            ...post,
-            user: postUser,
-            comments: postComments,
-          }}
-        />
-      );
-    })}
-  </div>
-);
+  return (
+    <div className="PostList">
+      {extendedPosts.map(post => (
+        <PostInfo key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
