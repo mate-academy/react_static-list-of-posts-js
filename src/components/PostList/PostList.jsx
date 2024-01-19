3b657290c1c9ba5dd23544a51aef924fc500cd71
kkrasnoshchok <krasnoshchok4work@gmail.com>
2024-01-19 11:26:37 +0100

    Solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,23 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts, comments, users }) => (
+  <div className="PostList">
+    {posts.map((post) => {
+      const postUser = users.find(user => user.id === post.userId);
+      const postComments = comments.filter(
+        comment => comment.postId === post.id,
+      );
+
+      return (
+        <PostInfo
+          key={post.id}
+          post={{
+            ...post,
+            user: postUser,
+            comments: postComments,
+          }}
+        />
+      );
+    })}
+  </div>
 );
