074eeb4f0acd9d8daf634fcdf0af06423bbb5c8e
Anatolii Hulchak <anatolii.hulchak@gmail.com>
2024-01-20 17:17:46 +0200

    solved task

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+import { PostList } from './components/PostList';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId)
+      || null;
+}
+
+function getCommentsByPostId(postId) {
+  return commentsFromServer.filter(comments => comments.postId === postId);
+}
+
+export const posts = postsFromServer.map(post => ({
+  ...post,
+  user: getUserById(post.userId),
+  comments: getCommentsByPostId(post.id),
+}));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <div className="PostList">
+    <PostList posts={posts} />
+
+    {/* <div className="PostList">
       <div className="PostInfo">
         <div className="PostInfo__header">
           <h3 className="PostInfo__title">qui est esse</h3>
@@ -102,6 +120,6 @@ export const App = () => (
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
   </section>
 );
