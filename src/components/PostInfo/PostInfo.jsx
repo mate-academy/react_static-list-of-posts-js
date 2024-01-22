57df5195989b79b64ecd6553051f02ddf50e5214
White Rabbit <0983792928ag@gmail.com>
2024-01-22 20:08:00 +0100

    Added user existence check

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -9,7 +9,9 @@ export const PostInfo = ({ post }) => (
         {post.title}
       </h3>
 
-      <UserInfo user={post.user} />
+      {post.user && (
+        <UserInfo user={post.user} />
+      )}
     </div>
 
     <p className="PostInfo__body">
