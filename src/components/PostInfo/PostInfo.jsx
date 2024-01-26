ccbf084e08569092c5f10caded4b77c1e0dbd3c4
volodymyr <volodiayurovych@gmail.com>
2024-01-26 10:40:58 +0100

    edited 26.01

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -4,31 +4,30 @@ import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
 
 export const PostInfo = ({ post }) => (
-  <>
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">
-          {post.title}
-        </h3>
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">
+        {post.title}
+      </h3>
 
-        <p>
-          {' Posted by  '}
+      <p>
+        {' Posted by  '}
 
-          <UserInfo user={post.user} />
-        </p>
-      </div>
+        {post.user && <UserInfo user={post.user} />}
 
-      <p className="PostInfo__body">
-        {post.body}
       </p>
-
-      <hr />
-
-      {post.comments.length === 0 ? (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      ) : (
-        <CommentList comments={post.comments} />
-      )}
     </div>
-  </>
+
+    <p className="PostInfo__body">
+      {post.body}
+    </p>
+
+    <hr />
+
+    {post.comments.length === 0 ? (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    ) : (
+      <CommentList comments={post.comments} />
+    )}
+  </div>
 );
