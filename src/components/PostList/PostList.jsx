3a4f2c01c9a79d09248138915bcccfd0c87c068a
Denis Shyshenko <denisbmw2.84@gmail.com>
2024-02-03 13:20:35 +0200

    Solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,35 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ postsUsers }) => (
+  <div className="PostList">
+
+    {postsUsers.map(post => (
+
+      <PostInfo post={post} key={post.id} />
+    ))}
+    {/* <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">qui est esse</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+      </p>
+    </div>
+
+    <p className="PostInfo__body">
+      est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae
+      ea dolores neque fugiat blanditiis voluptate porro vel nihil
+      molestiae ut reiciendis qui aperiam non debitis possimus qui neque
+      nisi nulla
+    </p>
+
+    <hr />
+
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  </div> */}
+  </div>
 );
