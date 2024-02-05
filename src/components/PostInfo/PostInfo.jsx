77286a313402a9a4aeb7f43e40d805f85d1c708e
Denis Shyshenko <denisbmw2.84@gmail.com>
2024-02-05 09:42:01 +0200

    fix

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -13,7 +13,7 @@ export const PostInfo = ({ post }) => (
       <p>
         {' Posted by  '}
 
-        <UserInfo user={post.user} />
+        {post.user && <UserInfo user={post.user} />}
       </p>
     </div>
 
