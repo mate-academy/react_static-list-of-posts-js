20d01532a95e0f103e2e2c788abc54cca6785dee
Mari Andrushko <kap.mebel.a@gmail.com>
2024-02-07 16:39:34 +0100

    solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,24 @@
-export const PostList = () => (
-  <>Put the list here</>
+import { PostInfo } from '../PostInfo';
+import commentsFromServer from '../../api/comments.json';
+import usersFromServer from '../../api/users.json';
+
+function getCommentByPostId(postId) {
+  return commentsFromServer.filter(comment => comment.postId === postId);
+}
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map((post) => {
+      const comments = getCommentByPostId(post.id);
+      const postWithComments = {
+        ...post,
+        comments,
+        user: usersFromServer.find(user => user.id === post.userId),
+      };
+
+      return (
+        <PostInfo post={postWithComments} key={postWithComments.id} />
+      );
+    })}
+  </div>
 );
