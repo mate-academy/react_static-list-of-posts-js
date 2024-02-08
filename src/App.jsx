68c5ce6da8448b5bfe9692ba7318c5a055534dfa
Ihor Prodan <prodanigor0@gmail.com>
2024-02-08 18:25:31 +0100

    solution postId

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,17 @@ import commentsFromServer from './api/comments.json';
 import { PostList } from './components/PostList/PostList';
 
 function getUserById(userId) {
-  return usersFromServer.find(user => user.id === userId)
-    || null;
+  return usersFromServer.find(user => user.id === userId);
 }
 
 function getCommentById(postId) {
-  return commentsFromServer.filter(user => user.postId === postId);
+  return commentsFromServer.filter(comment => comment.postId === postId);
 }
 
 const posts = postsFromServer.map(post => ({
   ...post,
   user: getUserById(post.userId),
-  comments: getCommentById(post.userId),
+  comments: getCommentById(post.id),
 }));
 
 export const App = () => (
