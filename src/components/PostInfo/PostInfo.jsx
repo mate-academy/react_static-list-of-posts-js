4940c4d299c74a5b438fa698b4941f6a721224d4
alona <alionazhv@gmail.com>
2024-02-09 07:38:37 +0100

    Case when user was undefined handled

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -7,8 +7,12 @@ export const PostInfo = ({ post }) => (
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
       <p>
-        {' Posted by  '}
-        <UserInfo user={post.user} />
+        {post.user && (
+          <>
+            {' Posted by  '}
+            <UserInfo user={post.user} />
+          </>
+        )}
       </p>
     </div>
     <p className="PostInfo__body">{post.body}</p>
