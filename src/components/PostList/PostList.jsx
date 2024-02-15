6ec3fa4c6fa9be2c38be26091c28cf17a79f87aa
NataSerg <na.s@ukr.net>
2024-02-15 14:34:02 +0200

    fixed keys, used destructuring

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -3,7 +3,7 @@ import { PostInfo } from '../PostInfo';
 export const PostList = ({ posts }) => (
   <div className="PostList">
     {posts.map(post => (
-      <PostInfo post={post} id={post.id} />
+      <PostInfo key={post.id} post={post} />
     ))}
   </div>
 );
