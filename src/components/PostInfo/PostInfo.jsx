d2bf9f1950d0feb31bf200e3ff0679fd73ffef24
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-02-15 15:46:02 +0200

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,36 @@
-export const PostInfo = () => (
-  <>Put the post here</>
-);
+import './PostInfo.scss';
+import users from '../../api/users.json';
+import comments from '../../api/comments.json';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+export const PostInfo = ({ post }) => {
+  const postByUser = users.find(user => post.userId === user.id);
+  const postCommetns = comments.filter(comment => post.id === comment.postId);
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={postByUser} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      {(postCommetns.length !== 0) ? (
+        <CommentList comments={postCommetns} />
+      ) : (
+        <>
+          <hr />
+
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )}
+    </div>
+  );
+};
