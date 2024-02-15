f0c2da0f1d0ae4f5cb956f63abef648028539ddf
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-02-15 16:27:19 +0200

    fix ploblems for pass the test

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import './App.scss';
 
 import postsFromServer from './api/posts.json';
 import { PostList } from './components/PostList';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId);
+}
+
+function getCommentByPostId(postId) {
+  return commentsFromServer.filter(comment => comment.postId === postId);
+}
+
+export const posts = postsFromServer.map(post => ({
+  ...post,
+  user: getUserById(post.userId),
+  comments: getCommentByPostId(post.id),
+}));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <PostList posts={postsFromServer} />
+    <PostList posts={posts} />
   </section>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,36 +1,29 @@
 import './PostInfo.scss';
-import users from '../../api/users.json';
-import comments from '../../api/comments.json';
 import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
 
-export const PostInfo = ({ post }) => {
-  const postByUser = users.find(user => post.userId === user.id);
-  const postCommetns = comments.filter(comment => post.id === comment.postId);
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
 
-  return (
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+      <p>
+        {' Posted by  '}
 
-        <p>
-          {' Posted by  '}
-
-          {postByUser && <UserInfo user={postByUser} />}
-        </p>
-      </div>
-
-      <p className="PostInfo__body">{post.body}</p>
-
-      {(postCommetns.length !== 0) ? (
-        <CommentList comments={postCommetns} />
-      ) : (
-        <>
-          <hr />
-
-          <b data-cy="NoCommentsMessage">No comments yet</b>
-        </>
-      )}
+        {post.user && <UserInfo user={post.user} />}
+      </p>
     </div>
-  );
-};
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    {post.comments.length !== 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <>
+        <hr />
+
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+    )}
+  </div>
+);
