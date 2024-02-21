9172f2c446b92924b0ca858af696528d65c58def
zozulia.kirill <kirillzozulya95@gmail.com>
2024-02-21 11:18:35 +0200

    fix comments

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,25 +3,25 @@ import { UserInfo } from '../UserInfo/UserInfo';
 import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => (
-  <>
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
 
+      {post.user && (
         <p>
           {' Posted by  '}
 
           <UserInfo user={post.user} />
         </p>
-      </div>
-
-      <p className="PostInfo__body">{post.body}</p>
-
-      {post.comments.length !== 0 ? (
-        <CommentList comments={post.comments} />
-      ) : (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
       )}
     </div>
-  </>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    {post.comments.length !== 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
 );
