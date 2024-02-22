bccd0ac9fc07e0d7273ed1eea56d769fa3323cf4
Roman Babii <ggdiscone@gmail.com>
2024-02-22 19:10:57 +0200

    destructed objects & refactored

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList/PostList';
 
 function getUserById(userId) {
-  return usersFromServer.find(user => user.id === userId) || null;
+  return usersFromServer.find(user => user.id === userId);
 }
 
 function getCommentsById(postId) {
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,25 +3,29 @@ import './PostInfo.scss';
 import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo = ({ post }) => {
+  const { title, user, body, comments } = post;
 
-      <p>
-        {' Posted by  '}
-        <UserInfo user={post.user} />
-      </p>
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        <p>
+          <span> Posted by </span>
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      <hr />
+
+      {comments.length !== 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
     </div>
-
-    <p className="PostInfo__body">{post.body}</p>
-
-    <hr />
-
-    {post.comments.length !== 0 ? (
-      <CommentList comments={post.comments} />
-    ) : (
-      <b data-cy="NoCommentsMessage">No comments yet</b>
-    )}
-  </div>
-);
+  );
+};
