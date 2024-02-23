28bef23ce568b4a0a62443d07ee5ff914bf9d914
Anastasiia <vasilisandreeva00@gmail.com>
2024-02-23 18:37:47 +0200

    Solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -7,7 +7,6 @@ export const PostInfo = ({ post }) => (
     <div className="PostInfo">
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{post.title}</h3>
-        
 
         <p>
           {' Posted by  '}
@@ -22,7 +21,6 @@ export const PostInfo = ({ post }) => (
         <CommentList comments={post.comments} />
       ) : (
         <b data-cy="NoCommentsMessage">No comments yet</b>
-
       )}
     </div>
   </>
