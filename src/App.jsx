b163aac70cc5bcebfe55b1008cf26133b93137ae
ViktoriiaRepo <samarityankav@gmail.com>
2024-02-28 13:34:20 +0200

    Change after checking

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { PostList } from './components/PostList/PostList';
 import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
-// import { comment } from 'postcss';
 
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId);
