3e7d0693aa01e54d20507017300e28711d3dde93
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-03-03 00:08:11 +0200

    feat [react]: implement posts parsing app

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,11 @@
-export const UserInfo = () => <>Put the user here</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user: { email, name } }) => (
+  <p>
+    {' Posted by  '}
+
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  </p>
+);
