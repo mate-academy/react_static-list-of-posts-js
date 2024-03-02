9dc0b08b317321a32a81cba3abe9d02c0d008c27
Anton Kulchytskyi <antek1809@gmail.com>
2024-03-02 22:19:20 +0200

    React static list of posts js solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,34 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  const { title, body, comments, user } = post;
+
+  // eslint-disable-next-line
+  console.log(post);
+
+  return (
+    <>
+      <div className="PostInfo">
+        <div className="PostInfo__header">
+          <h3 className="PostInfo__title">{title}</h3>
+          <p>
+            {' Posted by  '}
+            {user && <UserInfo user={user} />}
+          </p>
+        </div>
+        <p className="PostInfo__body">{body}</p>
+        {comments.length === 0 ? (
+          <>
+            <hr />
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </>
+        ) : (
+          <div className="CommentList">
+            <CommentList comments={comments} />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
