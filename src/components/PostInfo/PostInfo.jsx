40d8b61562040d1b89d67596296df9c9a4e9d880
Pavlo Pukalo <misterkatlet@gmail.com>
2024-03-03 11:53:09 +0200

    add empovements

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,51 +3,25 @@ import { UserInfo } from '../UserInfo/UserInfo';
 import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => (
-  <>
-    {/* <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">qui est esse</h3>
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
 
-        <p>
-          {' Posted by  '}
+      <p>
+        {' Posted by  '}
 
-          {/* <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </p>
-      </div>
-
-      <p className="PostInfo__body">
-        est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea
-        dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut
-        reiciendis qui aperiam non debitis possimus qui neque nisi nulla
+        {post.user && <UserInfo user={post.user} />}
       </p>
-
-      <hr />
-
-      <b data-cy="NoCommentsMessage">No comments yet</b>
-    </div> */}
-
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
-
-        <p>
-          {' Posted by  '}
-
-          {post.user && <UserInfo user={post.user} />}
-        </p>
-      </div>
-
-      <p className="PostInfo__body">{post.body}</p>
-
-      <hr />
-
-      {post.comments.length !== 0 ? (
-        <CommentList comments={post.comments} />
-      ) : (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      )}
     </div>
-  </>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
+    {post.comments.length !== 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
 );
