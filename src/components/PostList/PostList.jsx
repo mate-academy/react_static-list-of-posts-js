4ef21b9a19773e6c8a91882830f2e37c9586384f
Tetiana Lisne <tplovic@gmail.com>
2024-03-06 01:07:41 +0200

    add react static list of posts

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,23 @@
-export const PostList = () => <>Put the list here</>;
+import './PostList.scss';
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts, users, comments }) => {
+  const postList = posts.map(post => {
+    const user = users.find(person => person.id === post.userId);
+    const postComments = comments.filter(comment => comment.postId === post.id);
+
+    return {
+      ...post,
+      user,
+      comments: postComments,
+    };
+  });
+
+  return (
+    <div className="PostList">
+      {postList.map(post => (
+        <PostInfo key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
