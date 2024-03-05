c7752db248f478565d9e69e1ecadc0446f209096
Denis Kotov <dionisiy2904@gmail.com>
2024-03-05 15:54:18 +0200

    static-list destructuring

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,26 +1,28 @@
-import { CommentList } from '../CommentList/CommentList';
-import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => {
+  const { title, comments, body, user } = post;
+
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+        <h3 className="PostInfo__title">{title}</h3>
 
         <p>
           {' Posted by  '}
 
-          <UserInfo user={post.user} />
+          <UserInfo user={user} />
         </p>
       </div>
 
-      <p className="PostInfo__body">{post.body}</p>
+      <p className="PostInfo__body">{body}</p>
 
       <hr />
 
-      {post.comments && post.comments.length > 0 ? (
-        <CommentList comments={post.comments} />
+      {comments?.length ? (
+        <CommentList comments={comments} />
       ) : (
         <b data-cy="NoCommentsMessage">No comments yet</b>
       )}
