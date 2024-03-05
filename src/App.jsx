0e3533605af457c9542f312214ace3d9d48ad70a
Denis Kotov <dionisiy2904@gmail.com>
2024-03-05 16:00:43 +0200

    userinfo js fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.scss';
 import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
-import { PostList } from './components/PostList/PostList';
+import { PostList } from './components/PostList';
 
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId) || null;
