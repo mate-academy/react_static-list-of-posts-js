9ee1a68dcffab0256381e188e88058be390b4eb9
Oleksandr Yepik <sepika534@gmail.com>
2024-03-05 20:53:42 +0200

    task done

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,10 @@
-export const PostList = () => <>Put the list here</>;
+
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </div>
+);
