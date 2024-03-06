050120d29889218d8c0e1375e61cf16c9bf2a239
Mariia Dzhafarova <grinchak.1997@gmail.com>
2024-03-06 16:55:50 +0200

    add task solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,15 @@
-export const UserInfo = () => <>Put the user here</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  const { name, username, email } = user;
+
+  return (
+    <p>
+      {`Posted by ${username}`}
+
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </p>
+  );
+};
