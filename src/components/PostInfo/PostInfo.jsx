dd70456c2c1f076a2f9e44eac014ec86e96a6194
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-03-06 17:00:43 +0200

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,35 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+
+import usersFromServer from '../../api/users.json';
+
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+const getUserById = userId =>
+  usersFromServer.find(user => user.id === userId) || null;
+
+export const PostInfo = ({ post }) => {
+  const user = getUserById(post.userId);
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <UserInfo user={user} />
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      {post.comments.length === 0 ? (
+        <>
+          <hr />
+
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      ) : (
+        <CommentList comments={post.comments} />
+      )}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,19 @@
-export const PostList = () => <>Put the list here</>;
+import './PostList.scss';
+
+import commentsFromServer from '../../api/comments.json';
+
+import { PostInfo } from '../PostInfo/PostInfo';
+
+const getCommentsByPostId = postId =>
+  commentsFromServer.filter(comment => comment.postId === postId);
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo
+        post={{ comments: getCommentsByPostId(post.id), ...post }}
+        key={post.id}
+      />
+    ))}
+  </div>
+);
