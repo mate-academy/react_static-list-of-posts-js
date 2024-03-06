021fc1a25bf90f4b67650622f1a907abaaf20583
Nikita Donskoy <donskoy.nikita13@gmail.com>
2024-03-06 20:25:56 +0200

    fix fix solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -18,9 +18,11 @@ export const PostInfo = ({ post }) => {
 
       <hr />
 
-      {(comments.length === 0 && (
+      {comments.length ? (
+        <CommentList comments={comments} />
+      ) : (
         <b data-cy="NoCommentsMessage">No comments yet</b>
-      )) || <CommentList comments={comments} />}
+      )}
     </div>
   );
 };
