2c9c50de2cc3fe1a6f7c788963926e5950982a65
winniepuhh <mihajlevskijbogdan@gmail.com>
2024-03-06 18:00:03 +0200

    Fixed

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,9 +1,10 @@
 import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
 
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
     {comments.map(comment => (
-      <CommentInfo comment={comment} />
+      <CommentInfo comment={comment} key={comment.id} />
     ))}
   </div>
 );
