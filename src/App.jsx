a666e935564e183ab5aaeb68fb92ffa0ccd5e9ef
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-03-07 16:07:28 +0200

    logic improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.scss';
 
+import usersFromServer from './api/users.json';
+import commentsFromServer from './api/comments.json';
 import postsFromServer from './api/posts.json';
 import { PostList } from './components/PostList/PostList';
+
+const posts = postsFromServer.map(post => ({
+  ...post,
+  user: usersFromServer.find(user => user.id === post.userId) || null,
+  comments: commentsFromServer.filter(comment => comment.postId === post.id),
+}));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <PostList posts={postsFromServer} />
+    <PostList posts={posts} />
   </section>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,19 +1,11 @@
 import './PostList.scss';
 
-import commentsFromServer from '../../api/comments.json';
-
 import { PostInfo } from '../PostInfo/PostInfo';
-
-const getCommentsByPostId = postId =>
-  commentsFromServer.filter(comment => comment.postId === postId);
 
 export const PostList = ({ posts }) => (
   <div className="PostList">
     {posts.map(post => (
-      <PostInfo
-        post={{ comments: getCommentsByPostId(post.id), ...post }}
-        key={post.id}
-      />
+      <PostInfo post={post} key={post.id} />
     ))}
   </div>
 );
