65d25f28e0f94dfcb2ab808e2105a478033abfff
Vadym <khromeicreate@gmail.com>
2024-03-07 15:30:11 +0200

    add task solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,14 @@
-export const PostList = () => <>Put the list here</>;
+import React from 'react';
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => {
+      return (
+        <React.Fragment key={post.id}>
+          <PostInfo post={post} />
+        </React.Fragment>
+      );
+    })}
+  </div>
+);
