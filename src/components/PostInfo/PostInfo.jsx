59d3b08bba316e4c24c1c3fbe3f9b4fdb3c4bd77
Ihor Bahniuk <hattorikanto@gmail.com>
2024-03-07 10:33:29 +0200

    formatted code in PostInfo.jsx

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -7,24 +7,22 @@ export const PostInfo = ({ post }) => {
   const { comments, user, title, body } = post;
 
   return (
-    <>
-      <div className="PostInfo">
-        <div className="PostInfo__header">
-          <h3 className="PostInfo__title">{title}</h3>
-        </div>
-
-        <UserInfo user={user} />
-
-        <p className="PostInfo__body">{body}</p>
-
-        <hr />
-
-        {comments.length ? (
-          <CommentList comments={comments} />
-        ) : (
-          <b data-cy="NoCommentsMessage">No comments yet</b>
-        )}
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
       </div>
-    </>
+
+      <UserInfo user={user} />
+
+      <p className="PostInfo__body">{body}</p>
+
+      <hr />
+
+      {comments.length ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
   );
 };
