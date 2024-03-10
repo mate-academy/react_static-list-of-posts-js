1e0c6def3083dce995eb4ce1fda2749b6168dfe1
Kirill <ovoo1339@gmail.com>
2024-03-10 11:01:25 +0200

    work2

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,30 +3,28 @@ import { UserInfo } from '../UserInfo/UserInfo';
 import './PostInfo.scss';
 
 export const PostInfo = ({ post: { user, comments, title, body } }) => (
-  <>
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{title}</h3>
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{title}</h3>
 
-        {user && (
-          <p>
-            {' Posted by  '}
+      {user && (
+        <p>
+          {' Posted by  '}
 
-            <UserInfo user={user} />
-          </p>
-        )}
-      </div>
-
-      <p className="PostInfo__body"> {body} </p>
-
-      {comments.length ? (
-        <CommentList comments={comments} />
-      ) : (
-        <>
-          <hr />
-          <b data-cy="NoCommentsMessage">No comments yet</b>
-        </>
+          <UserInfo user={user} />
+        </p>
       )}
     </div>
-  </>
+
+    <p className="PostInfo__body"> {body} </p>
+
+    {comments.length ? (
+      <CommentList comments={comments} />
+    ) : (
+      <>
+        <hr />
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+    )}
+  </div>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,11 +1,9 @@
 import { PostInfo } from '../PostInfo/PostInfo';
 
 export const PostList = ({ posts }) => (
-  <>
-    <div className="PostList">
-      {posts.map(post => (
-        <PostInfo key={posts.id} post={post} />
-      ))}
-    </div>
-  </>
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={posts.id} post={post} />
+    ))}
+  </div>
 );
