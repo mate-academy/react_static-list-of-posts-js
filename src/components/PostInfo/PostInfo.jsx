0769fd09c4177ff3edac72d7f454fddfcbeed9f6
DESKTOP-EHDJ2FA\yarik <yarik.zubyk@gmail.com>
2024-03-11 14:48:26 +0200

    created Comments components

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,3 +1,4 @@
+import { CommentList } from '../CommentList/CommentList';
 import { UserInfo } from '../UserInfo/UserInfo';
 
 export const PostInfo = ({ post }) => {
@@ -6,7 +7,7 @@ export const PostInfo = ({ post }) => {
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">doloremque illum aliquid sunt</h3>
+        <h3 className="PostInfo__title">{title}</h3>
 
         <p>
           {' Posted by  '}
@@ -14,55 +15,12 @@ export const PostInfo = ({ post }) => {
         </p>
       </div>
 
-      <p className="PostInfo__body">
-        deserunt eos nobis asperiores et hic est debitis repellat molestiae
-        optio nihil ratione ut eos beatae quibusdam distinctio maiores earum
-        voluptates et aut adipisci ea maiores voluptas maxime
-      </p>
-
-      <div className="CommentList">
-        <div className="CommentInfo">
-          <div className="CommentInfo__title">
-            <strong className="CommentInfo__name">pariatur omnis in</strong>
-
-            {' by '}
-
-            <a
-              className="CommentInfo__email"
-              href="mailto:Telly_Lynch@karl.co.uk"
-            >
-              Telly_Lynch@karl.co.uk
-            </a>
-          </div>
-
-          <div className="CommentInfo__body">
-            dolorum voluptas laboriosam quisquam ab totam beatae et aut aliquid
-            optio assumenda voluptas velit itaque quidem voluptatem tempore
-            cupiditate in itaque sit molestiae minus dolores magni
-          </div>
-        </div>
-
-        <div className="CommentInfo">
-          <div className="CommentInfo__title">
-            <strong className="CommentInfo__name">
-              odio adipisci rerum aut animi
-            </strong>
-
-            {' by '}
-
-            <a className="CommentInfo__email" href="mailto:Nikita@garfield.biz">
-              Nikita@garfield.biz
-            </a>
-          </div>
-
-          <div className="CommentInfo__body">
-            quia molestiae reprehenderit quasi aspernatur aut expedita occaecati
-            aliquam eveniet laudantium omnis quibusdam delectus saepe quia
-            accusamus maiores nam est cum et ducimus et vero voluptates
-            excepturi deleniti ratione
-          </div>
-        </div>
-      </div>
+      <p className="PostInfo__body">{body}</p>
+      {comments.length > 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
     </div>
   );
 };
