ea5497f25629047632e11c61513ac7629ccf023c
Oleh Obiukh <oleg.obiukh@gmail.com>
2024-03-11 15:07:16 +0000

    list of posts

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,24 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts, users, comments }) => (
+  <>
+    <div className="PostList">
+      {posts.map(post => {
+        const commentsByPost = comments.filter(
+          comment => comment.postId === post.id,
+        );
+
+        const user = users.find(item => item.id === post.userId);
+
+        return (
+          <PostInfo
+            key={post.id}
+            post={post}
+            user={user}
+            comments={commentsByPost}
+          />
+        );
+      })}
+    </div>
+  </>
+);
