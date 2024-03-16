4862365017e0332653736df52d80b5396dbe64d8
Dmytro Mytrokhin <dmytro@Dmytros-MacBook-Air.local>
2024-03-16 15:52:22 +0200

    list of posts

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,17 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment: { name, email, body } }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{name}</strong>
+
+      {' by '}
+
+      <a className="CommentInfo__email" href={`mailto:${email}`}>
+        {email}
+      </a>
+    </div>
+
+    <div className="CommentInfo__body">{body}</div>
+  </div>
+);
