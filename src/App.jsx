891030fb9dcb1aa3424739a30209ae232120c353
MaximAV <atanasovmaksim56@gmail.com>
2024-03-17 20:23:52 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable max-len */
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+import { PostList } from './components/PostList/PostList';
+
+const posts = postsFromServer.map(
+  post =>
+    (post = {
+      ...post,
+      user: {
+        name: usersFromServer.find(u => u.id === post.userId).name,
+        email: usersFromServer.find(u => u.id === post.userId).email,
+      },
+    }),
+);
+
+posts.map(
+  post =>
+    (post.comments = commentsFromServer.filter(
+      comment => comment.postId === post.id,
+    )),
+);
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <div className="PostList">
+    <PostList posts={posts} />
+
+    {/* <div className="PostList">
       <div className="PostInfo">
         <div className="PostInfo__header">
           <h3 className="PostInfo__title">qui est esse</h3>
@@ -99,6 +122,6 @@ export const App = () => (
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
   </section>
 );
