9b185d946f042caa39882394da1b77229b50d9cf
MaximAV <atanasovmaksim56@gmail.com>
2024-03-18 18:52:55 +0200

    data improve

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,11 @@ import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList/PostList';
 
-const posts = postsFromServer.map(
-  post =>
-    (post = {
-      ...post,
-      user: {
-        name: usersFromServer.find(u => u.id === post.userId).name,
-        email: usersFromServer.find(u => u.id === post.userId).email,
-      },
-    }),
-);
-
-posts.map(
-  post =>
-    (post.comments = commentsFromServer.filter(
-      comment => comment.postId === post.id,
-    )),
-);
+export const posts = postsFromServer.map(post => ({
+  ...post,
+  user: usersFromServer.find(user => user.id === post.userId),
+  comments: commentsFromServer.filter(comment => comment.postId === post.id),
+}));
 
 export const App = () => (
   <section className="App">
