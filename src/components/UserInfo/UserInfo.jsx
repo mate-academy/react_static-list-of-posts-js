b216df21aace0248230578ff2b601bc1661730fe
MaximAV <atanasovmaksim56@gmail.com>
2024-03-18 19:44:01 +0200

    user-desctruct

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export const UserInfo = ({ user }) => {
+  const { email, name } = user;
+
   return (
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
     </a>
   );
 };
