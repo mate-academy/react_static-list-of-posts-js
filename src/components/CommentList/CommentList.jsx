076509217e85df16386de147d04dbe911712ae13
hyriix <shyrii11bohdan@gmail.com>
2024-03-21 10:43:57 +0100

    farmed

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,15 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      {comments && comments.length > 0 ? (
+        comments.map(comment => {
+          return <CommentInfo comment={comment} key={comment.id} />;
+        })
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
