580b13d9e2868776f6cb1f55cc7dd4f639e22bf3
Max <gcrazy007@gmail.com>
2024-03-26 07:06:48 +0200

    add task solution

--- src/components/CommentList/CommentList.spec.js
+++ src/components/CommentList/CommentList.spec.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 /* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/no-unresolved */
 
 import React from 'react';
 import { mount } from '@cypress/react';
