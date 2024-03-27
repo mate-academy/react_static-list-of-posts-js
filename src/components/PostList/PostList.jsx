8becbea8c4d3edf82c32a4db3920be3107b02146
Iryna Liashko <ira.liashko1@gmail.com>
2024-03-27 12:46:58 +0100

    Almost done

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,10 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+import './PostList.scss';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </div>
+);
