4cf76160973928928fe9ae8f1284fa4a8541f61a
Maksym Nazarenko <71183975+maks-n@users.noreply.github.com>
2024-04-01 13:16:00 -0400

    create components and styles

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,31 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+import commentsFromServer from '../../api/comments.json';
+
+export const PostInfo = ({ post }) => {
+  const comments = commentsFromServer.filter(el => el.postId === post.id);
+  const isComments = !comments.length;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <UserInfo userId={post.userId} />
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      {isComments ? (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      ) : (
+        <CommentList comments={comments} />
+      )}
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,20 @@
-export const UserInfo = () => <>Put the user here</>;
+import './UserInfo.scss';
+import usersFromServer from '../../api/users.json';
+
+export const UserInfo = ({ userId }) => {
+  let user;
+
+  if (usersFromServer.some(el => el.id === userId)) {
+    user = usersFromServer.find(el => el.id === userId);
+  }
+
+  return (
+    <p>
+      {' Posted by  '}
+
+      <a className="UserInfo" href={`mailto:${user.email}`}>
+        {user.name}
+      </a>
+    </p>
+  );
+};
