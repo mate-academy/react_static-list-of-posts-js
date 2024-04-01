595cae6cd1cb10870310ceec58dc2385060cf716
Voloshynd <Voloshynd13@gmail.com>
2024-04-01 14:21:09 +0200

    Corrected task

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -2,9 +2,9 @@ import './CommentList.scss';
 import { CommentInfo } from '../CommentInfo/CommentInfo';
 
 export const CommentList = ({ comments }) => (
-  <div className="CommentList">
+  <ul className="CommentList">
     {comments.map(comment => (
       <CommentInfo comment={comment} key={comment.id} />
     ))}
-  </div>
+  </ul>
 );
