6e62e295c385a10d8db79875b57bdb864ecbd8c6
Illia <ilyaivasyk1@gmail.com>
2024-04-04 17:54:59 +0300

    Solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,26 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <a className="UserInfo" href={`mailto:${post.user.email}`}>
+            {post.user && <UserInfo user={post.user} />}
+          </a>
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      <CommentList comments={post.comments} />
+    </div>
+  );
+};
