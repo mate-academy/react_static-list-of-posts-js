2e2864eb11721b781b2869909ba1cd8845eae3f9
Allanpt <jatallantavares@gmail.com>
2024-04-08 13:52:54 -0300

    Solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,27 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import './CommentInfo.scss';
+
+function getNamebyEmail(email) {
+  const indexAt = email.indexOf('@');
+
+  const name = email.slice(0, indexAt);
+
+  return name;
+}
+
+export const CommentInfo = ({ comment }) => (
+  <>
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{comment.name}</strong>
+
+        {getNamebyEmail(comment.email)}
+
+        <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+          {comment.email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">{comment.body}</div>
+    </div>
+  </>
+);
