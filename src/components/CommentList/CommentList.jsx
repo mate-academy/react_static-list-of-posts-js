29f87fbc06c7a44aa6fe15eaaf5d6d8783994a92
Ivan <110199ws@gmail.com>
2024-04-08 22:39:58 +0300

    help pls

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,15 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments, post }) => {
+  const commentsPost = comments.filter(comment => comment.postId === post.id);
+  const ifComentPost =
+    commentsPost.length > 0 ? (
+      commentsPost.map(comment => (
+        <CommentInfo comment={comment} key={comment.id} />
+      ))
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    );
+
+  return <div className="CommentList">{ifComentPost}</div>;
+};
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,35 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+import postsFromServer from '../../api/posts.json';
+import commentsFromServer from '../../api/comments.json';
+
+function findPostByComment(postId) {
+  return postsFromServer.find(post => post.id === postId) || null;
+}
+
+export const commentsWithPost = commentsFromServer.map(comment => ({
+  ...comment,
+  post: findPostByComment(comment.postId),
+}));
+
+export const PostInfo = ({ post }) => {
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={post.user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      <CommentList comments={commentsWithPost} post={post} />
+    </div>
+  );
+};
