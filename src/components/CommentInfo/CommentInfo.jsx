79c4638e6a69f7dc8c5c41bb53a6dd04bf33211f
Allanpt <jatallantavares@gmail.com>
2024-04-09 18:32:59 -0300

    commentInfo adjusts

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,21 +1,17 @@
 import './CommentInfo.scss';
 
-export const CommentInfo = ({ comment }) => {
-  return (
-    <div className="CommentInfo">
-      <div className="CommentInfo__title">
-        <strong className="CommentInfo__name">{comment.name}</strong>
+export const CommentInfo = ({ comment }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}</strong>
 
-        {' by '}
+      {' by '}
 
-        <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
-          {comment.email}
-        </a>
-      </div>
-
-      <div className="CommentInfo__body">{comment.body}</div>
+      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+        {comment.email}
+      </a>
     </div>
-  );
-};
 
-export default CommentInfo;
+    <div className="CommentInfo__body">{comment.body}</div>
+  </div>
+);
