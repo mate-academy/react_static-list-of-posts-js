84146af25a8572201f1a25dfae596dea4cbadce0
RuslanSkl <rsklyarenko@gmail.com>
2024-04-09 12:49:48 +0300

    - refactor

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,12 @@ import { PostList } from './components/PostList';
 const postsWithUsers = postsFromServer.map(post => ({
   ...post,
   user: usersFromServer.find(user => user.id === post.userId),
-}));
-
-const postsWithComments = postsWithUsers.map(post => ({
-  ...post,
   comments: commentsFromServer.filter(comment => comment.postId === post.id),
 }));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <PostList posts={postsWithComments} />
+    <PostList posts={postsWithUsers} />
   </section>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -7,7 +7,7 @@ export const PostInfo = ({ post }) => (
       <h3 className="PostInfo__title">{post.title}</h3>
       <p>
         {' Posted by  '}
-        <UserInfo user={post.user} />
+        {post.user && <UserInfo user={post.user} />}
       </p>
     </div>
     <p className="PostInfo__body">{post.body}</p>
