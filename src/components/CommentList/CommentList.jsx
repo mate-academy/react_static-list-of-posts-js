3cba3b95b81c06dbc01adee2342abcf2676eed04
Oleksandr Levitskyi <oleksandr.levitskyi@gmail.com>
2024-04-15 14:11:41 +0300

    react-post-of-lists

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,15 @@
-export const CommentList = () => <>Put the list here</>;
+import React from 'react';
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.length > 0 ? (
+      comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
+);
