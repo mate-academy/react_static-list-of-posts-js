69b54ec856e4bc05cc665e808538545c843c0e02
Denys Lysiak <lisyak99.dl@gmail.com>
2024-04-16 13:56:23 +0300

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,26 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  const user = { ...post.user };
+  const comments = [...post.comments];
+  const noComments = <b data-cy="NoCommentsMessage">No comments yet</b>;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      {comments.length === 0 ? noComments : <CommentList comments={comments} />}
+    </div>
+  );
+};
