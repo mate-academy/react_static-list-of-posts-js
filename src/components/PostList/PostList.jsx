01896fe288d2583020b43a3e8254426a65a03014
Yana Momot <yanamomot2004@gmail.com>
2024-04-17 18:48:34 +0300

    add solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,33 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+
+    {/* <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">qui est esse</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <a className="UserInfo" href="mailto:Sincere@april.biz">
+            Leanne Graham
+          </a>
+        </p>
+      </div>
+
+      <p className="PostInfo__body">
+        est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea
+        dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut
+        reiciendis qui aperiam non debitis possimus qui neque nisi nulla
+      </p>
+
+      <hr />
+
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    </div> */}
+  </div>
+);
