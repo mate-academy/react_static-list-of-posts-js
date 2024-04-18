fc763c2cef093f9c7a1a194ab849e5ff2a47d632
ordila <ordila99@gmail.com>
2024-04-18 23:56:32 +0300

    add solving

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,33 @@
-export const PostInfo = () => <>Put the post here</>;
+import React from 'react';
+
+import { CommentList } from '../CommentList/CommentList';
+
+import { UserInfo } from '../UserInfo/UserInfo';
+
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => {
+  const { user, title, body, comments } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      {comments.length > 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,17 @@
-export const PostList = () => <>Put the list here</>;
+import React from 'react';
+
+import { getPosts } from '../../services';
+
+import { PostInfo } from '../PostInfo/PostInfo';
+
+const posts = getPosts();
+
+export const PostList = () => {
+  return (
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo post={post} key={post.id} />
+      ))}
+    </div>
+  );
+};
