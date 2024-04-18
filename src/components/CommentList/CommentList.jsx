e75dbdb13d866b4c4f10481a83c461773b86f153
ropanefarias <rodrigopanefarias27@gmail.com>
2024-04-17 23:40:15 -0300

    Solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,15 @@
-export const CommentList = () => <>Put the list here</>;
+import React from 'react';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    <ul>
+      {comments.map(comment => (
+        <li key={comment.id}>
+          <CommentInfo comment={comment} />
+        </li>
+      ))}
+    </ul>
+  </div>
+);
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,14 @@
-export const PostList = () => <>Put the list here</>;
+import React from 'react';
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>
+          <PostInfo post={post} />
+        </li>
+      ))}
+    </ul>
+  </div>
+);
