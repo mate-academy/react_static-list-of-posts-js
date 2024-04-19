cc28cf7da635861fef01f8eabdbff9e9d82ea420
Artem Demeniev <myfirstmillio93@gmail.com>
2024-04-19 12:50:45 +0300

    delete excessive fragment

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,16 +1,14 @@
 export const CommentInfo = ({ comment }) => (
-  <>
-    <div className="CommentInfo">
-      <div className="CommentInfo__title">
-        <strong className="CommentInfo__name">{comment.name}</strong>
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}</strong>
 
-        {' by '}
-        <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
-          {comment.email}
-        </a>
-      </div>
-
-      <div className="CommentInfo__body">{comment.body}</div>
+      {' by '}
+      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+        {comment.email}
+      </a>
     </div>
-  </>
+
+    <div className="CommentInfo__body">{comment.body}</div>
+  </div>
 );
