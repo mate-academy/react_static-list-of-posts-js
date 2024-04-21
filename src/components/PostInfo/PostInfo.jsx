06ef356f6a98556afb796694fb103b4540f055c7
Marina Semipyatnaya <epicentr50@gmail.com>
2024-04-21 20:08:49 +0300

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -8,20 +8,19 @@ export const PostInfo = ({ post }) => (
       <h3 className="PostInfo__title">{post.title}</h3>
 
       <p>
-        {' Posted by '}
-
+        {`Posted by `}
         {post.user && <UserInfo user={post.user} />}
       </p>
     </div>
 
     <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
     {post.comments.length > 0 ? (
       <CommentList comments={post.comments} />
     ) : (
-      <>
-        <hr />
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      </>
+      <b data-cy="NoCommentsMessage">No comments yet</b>
     )}
   </div>
 );
