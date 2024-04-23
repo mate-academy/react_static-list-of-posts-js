00e1b766d24b26cd5523f25f0864083f8dab507b
Marian <getmoodyup@gmail.com>
2024-04-23 04:29:42 +0300

    comments and users are also destructed

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,9 +1,11 @@
 import './UserInfo.scss';
 
 export const UserInfo = ({ user }) => {
-  const {email, name} = user;
+  const { email, name } = user;
 
-  <a className="UserInfo" href={`mailto:${email}`}>
-    {name}
-  </a>
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
 };
