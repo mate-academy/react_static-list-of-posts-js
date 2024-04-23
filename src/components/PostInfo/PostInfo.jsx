7e2540ca0d7f4d33c889fcc611bd3b3e13f8d35e
Marian <getmoodyup@gmail.com>
2024-04-23 04:15:33 +0300

    destrucuring everything

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,24 +2,28 @@ import './PostInfo.scss';
 import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo/UserInfo';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__item">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
-        <p>
-          {' Posted by '}
-          <UserInfo user={post.user} />
-        </p>
-      </div>
-      <p className="PostInfo__body">{post.body}</p>
-      {post.comments && post.comments.length > 0 ? (
-        <CommentList comments={post.comments} />
-      ) : (
-        <div>
-          <b data-cy="NoCommentsMessage">No comments yet</b>
+export const PostInfo = ({ post }) => {
+  const { title, user, body, comments } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__item">
+        <div className="PostInfo__header">
+          <h3 className="PostInfo__title">{title}</h3>
+          <p>
+            {' Posted by '}
+            <UserInfo user={user} />
+          </p>
         </div>
-      )}
+        <p className="PostInfo__body">{body}</p>
+        {comments && comments.length > 0 ? (
+          <CommentList comments={comments} />
+        ) : (
+          <div>
+            <b data-cy="NoCommentsMessage">No comments yet</b>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
