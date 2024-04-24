7972883411105b35e7a7d8049369496bbfade980
Greshch <greshchook@gmail.com>
2024-04-24 19:58:56 +0300

    2-PostInfo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.scss';
 import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
+import { PostInfo } from './components/PostInfo/PostInfo';
 
 function getUserById(users, id) {
   return users.filter(user => {
@@ -22,101 +23,6 @@ export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
     <div className="PostList">
-      {/* <div className="PostList">
-      <div className="PostInfo">
-        <div className="PostInfo__header">
-          <h3 className="PostInfo__title">qui est esse</h3>
-
-          <p>
-            {' Posted by  '}
-
-            <a className="UserInfo" href="mailto:Sincere@april.biz">
-              Leanne Graham
-            </a>
-          </p>
-        </div>
-
-        <p className="PostInfo__body">
-          est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea
-          dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut
-          reiciendis qui aperiam non debitis possimus qui neque nisi nulla
-        </p>
-
-        <hr />
-
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      </div>
-
-      <div className="PostInfo">
-        <div className="PostInfo__header">
-          <h3 className="PostInfo__title">doloremque illum aliquid sunt</h3>
-
-          <p>
-            {' Posted by  '}
-
-            <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-              Patricia Lebsack
-            </a>
-          </p>
-        </div>
-
-        <p className="PostInfo__body">
-          deserunt eos nobis asperiores et hic est debitis repellat molestiae
-          optio nihil ratione ut eos beatae quibusdam distinctio maiores earum
-          voluptates et aut adipisci ea maiores voluptas maxime
-        </p>
-
-        <div className="CommentList">
-          <div className="CommentInfo">
-            <div className="CommentInfo__title">
-              <strong className="CommentInfo__name">pariatur omnis in</strong>
-
-              {' by '}
-
-              <a
-                className="CommentInfo__email"
-                href="mailto:Telly_Lynch@karl.co.uk"
-              >
-                Telly_Lynch@karl.co.uk
-              </a>
-            </div>
-
-            <div className="CommentInfo__body">
-              dolorum voluptas laboriosam quisquam ab totam beatae et aut
-              aliquid optio assumenda voluptas velit itaque quidem voluptatem
-              tempore cupiditate in itaque sit molestiae minus dolores magni
-            </div>
-          </div>
-
-          <div className="CommentInfo">
-            <div className="CommentInfo__title">
-              <strong className="CommentInfo__name">
-                odio adipisci rerum aut animi
-              </strong>
-
-              {' by '}
-
-              <a
-                className="CommentInfo__email"
-                href="mailto:Nikita@garfield.biz"
-              >
-                Nikita@garfield.biz
-              </a>
-            </div>
-
-            <div className="CommentInfo__body">
-              quia molestiae reprehenderit quasi aspernatur aut expedita
-              occaecati aliquam eveniet laudantium omnis quibusdam delectus
-              saepe quia accusamus maiores nam est cum et ducimus et vero
-              voluptates excepturi deleniti ratione
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> */}
-
-      {/* {console.log(postsFromServer)} */}
-
       {postsFromServer.map(post => {
         const id = post.userId;
         const resp = getUserById(usersFromServer, id);
@@ -124,31 +30,13 @@ export const App = () => (
         const user = resp.length ? resp[0] : null;
         const name = user ? user.name : null;
         const email = user ? user.email : null;
-        const { body } = post;
+        // const { body } = post;
         const userId = user.id;
-        // console.log(postId);
-        // console.log(name);
-        // console.log(userId);
-        // const posts = getPostsById(postsFromServer, postId);
-        // console.log(postsFromServer[0]);
         const comentsUserById = getComentsById(commentsFromServer, userId);
 
-        // console.log(comentsUserById);
         return (
-          <div className="PostInfo" key={post.id}>
-            <div className="PostInfo__header">
-              <h3 className="PostInfo__title">{post.title}</h3>
-              <p>
-                {' Posted by  '}
-                <a className="UserInfo" href={`mailto:${email}`}>
-                  {/* {usersFromServer[post.userId].name} */}
-                  {name}
-                </a>
-              </p>
-            </div>
-
-            <p className="PostInfo__body">{body}</p>
-
+          <div key={post.id} className="PostInfo">
+            <PostInfo post={post} email={email} name={name} />
             {comentsUserById.length === 0 ? (
               <>
                 <hr />
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,17 @@
-export const PostInfo = () => <>Put the post here</>;
+export const PostInfo = ({ post, email, name }) => {
+  return (
+    <>
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+        <p>
+          {' Posted by  '}
+          <a className="UserInfo" href={`mailto:${email}`}>
+            {name}
+          </a>
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+    </>
+  );
+};
