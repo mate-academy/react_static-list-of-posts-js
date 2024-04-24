3d0655e3fb1a0d818dfdef3a89550a7c4c20bfd9
Greshch <greshchook@gmail.com>
2024-04-24 18:51:39 +0300

    view good bad archytect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+// import { comment } from 'postcss';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+
+function getUserById(users, id) {
+  return users.filter(user => {
+    // console.log(user.id, '\t', id);
+    return user.id === id;
+  });
+}
+
+function getComentsById(comments, id) {
+  return comments.filter(comment => {
+    return comment.postId === id;
+  });
+}
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-
     <div className="PostList">
+      {/* <div className="PostList">
       <div className="PostInfo">
         <div className="PostInfo__header">
           <h3 className="PostInfo__title">qui est esse</h3>
@@ -99,6 +113,79 @@ export const App = () => (
           </div>
         </div>
       </div>
+    </div> */}
+
+      {/* {console.log(postsFromServer)} */}
+
+      {postsFromServer.map(post => {
+        const id = post.userId;
+        const resp = getUserById(usersFromServer, id);
+        // console.log(resp);
+        const user = resp.length ? resp[0] : null;
+        const name = user ? user.name : null;
+        const email = user ? user.email : null;
+        const { body } = post;
+        const userId = user.id;
+        // console.log(postId);
+        // console.log(name);
+        // console.log(userId);
+        // const posts = getPostsById(postsFromServer, postId);
+        // console.log(postsFromServer[0]);
+        const comentsUserById = getComentsById(commentsFromServer, userId);
+
+        // console.log(comentsUserById);
+        return (
+          <div className="PostInfo" key={post.id}>
+            <div className="PostInfo__header">
+              <h3 className="PostInfo__title">{post.title}</h3>
+              <p>
+                {' Posted by  '}
+                <a className="UserInfo" href={`mailto:${email}`}>
+                  {/* {usersFromServer[post.userId].name} */}
+                  {name}
+                </a>
+              </p>
+            </div>
+
+            <p className="PostInfo__body">{body}</p>
+
+            {comentsUserById.length === 0 ? (
+              <>
+                <hr />
+
+                <b data-cy="NoCommentsMessage">No comments yet</b>
+              </>
+            ) : (
+              <>
+                <div className="CommentList">
+                  {comentsUserById.map(comment => {
+                    return (
+                      <div key={comment.id} className="CommentInfo">
+                        <div className="CommentInfo__title">
+                          <strong className="CommentInfo__name">
+                            {comment.name}
+                          </strong>
+
+                          {' by '}
+
+                          <a
+                            className="CommentInfo__email"
+                            href={`mailto:${comment.email}`}
+                          >
+                            {comment.email}
+                          </a>
+                        </div>
+
+                        <div className="CommentInfo__body">{comment.body}</div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </>
+            )}
+          </div>
+        );
+      })}
     </div>
   </section>
 );
