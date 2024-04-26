df341f3a1ddad62f2f66054c07fc4b94d46262f1
Yuliia <kotyuliya24@gmail.com>
2024-04-26 17:13:05 -0400

    solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,9 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts: postListProp }) => (
+  <div className="PostList">
+    {postListProp.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </div>
+);
