b912d7d27c39bff04fe5df8c066e88f748df4a22
DTMF <yarike451@gmail.com>
2024-05-09 11:16:30 +0300

    noRender

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,21 @@
-export const CommentInfo = () => <>Put the comment here</>;
+export const CommentInfo = () => {
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">pariatur omnis in</strong>
+
+        {' by '}
+
+        <a className="CommentInfo__email" href="mailto:Telly_Lynch@karl.co.uk">
+          Telly_Lynch@karl.co.uk
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">
+        dolorum voluptas laboriosam quisquam ab totam beatae et aut aliquid
+        optio assumenda voluptas velit itaque quidem voluptatem tempore
+        cupiditate in itaque sit molestiae minus dolores magni
+      </div>
+    </div>
+  );
+};
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,47 @@
-export const CommentList = () => <>Put the list here</>;
+export const CommentList = () => {
+  return (
+    <div className="CommentList">
+      <div className="CommentInfo">
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">pariatur omnis in</strong>
+
+          {' by '}
+
+          <a
+            className="CommentInfo__email"
+            href="mailto:Telly_Lynch@karl.co.uk"
+          >
+            Telly_Lynch@karl.co.uk
+          </a>
+        </div>
+
+        <div className="CommentInfo__body">
+          dolorum voluptas laboriosam quisquam ab totam beatae et aut aliquid
+          optio assumenda voluptas velit itaque quidem voluptatem tempore
+          cupiditate in itaque sit molestiae minus dolores magni
+        </div>
+      </div>
+
+      <div className="CommentInfo">
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">
+            odio adipisci rerum aut animi
+          </strong>
+
+          {' by '}
+
+          <a className="CommentInfo__email" href="mailto:Nikita@garfield.biz">
+            Nikita@garfield.biz
+          </a>
+        </div>
+
+        <div className="CommentInfo__body">
+          quia molestiae reprehenderit quasi aspernatur aut expedita occaecati
+          aliquam eveniet laudantium omnis quibusdam delectus saepe quia
+          accusamus maiores nam est cum et ducimus et vero voluptates excepturi
+          deleniti ratione
+        </div>
+      </div>
+    </div>
+  );
+};
