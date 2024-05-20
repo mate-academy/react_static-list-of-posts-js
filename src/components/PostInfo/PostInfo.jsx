9a8d513636b98268d4226fcb3797d9d1a664c1b0
Jenya Golovko <u9on1210@gmail.com>
2024-05-20 16:45:40 +0300

    task repair

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,27 +3,25 @@ import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => (
-  <>
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
 
-        <p>
-          <span>Posted by</span>
+      <p>
+        <span>Posted by</span>
 
-          <UserInfo user={post.user} key={post.user.id} />
-        </p>
-      </div>
-
-      <p className="PostInfo__body">{post.body}</p>
-
-      <hr />
-
-      {post.comments.length !== 0 ? (
-        <CommentList comments={post.comments} />
-      ) : (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      )}
+        <UserInfo user={post.user} key={post.user.id} />
+      </p>
     </div>
-  </>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
+    {post.comments.length !== 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
 );
