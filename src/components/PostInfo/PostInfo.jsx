8e70f9709e792fb0f716be2e5a58b5100c104236
Yaroslav Horych <gorichjaroslav@gmail.com>
2024-05-28 15:56:43 +0300

    using destructuring

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,16 +2,16 @@ import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
+export const PostInfo = ({ post: { title, user, body, comments } }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+      <h3 className="PostInfo__title">{title}</h3>
 
-      {post.user && <UserInfo user={post.user} />}
+      {user && <UserInfo user={user} />}
     </div>
 
-    <p className="PostInfo__body">{post.body}</p>
+    <p className="PostInfo__body">{body}</p>
 
-    <CommentList comments={post.comments} />
+    <CommentList comments={comments} />
   </div>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -3,12 +3,10 @@ import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts }) => {
   return (
-    <>
-      <div className="PostList">
-        {posts.map(post => (
-          <PostInfo post={post} key={post.id} />
-        ))}
-      </div>
-    </>
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo post={post} key={post.id} />
+      ))}
+    </div>
   );
 };
