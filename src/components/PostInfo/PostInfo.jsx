de17b6713911a9f1da6f61cb02238acd6d075a85
Hanna Svirina <26anna07@ukr.net>
2024-05-30 19:00:20 +0200

    add solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,22 +2,22 @@ import { CommentList } from '../CommentList/CommentList';
 import { UserInfo } from '../UserInfo/UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
+export const PostInfo = ({ post: { title, user, body, comments } }) => (
   <div className="PostInfo">
     <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+      <h3 className="PostInfo__title">{title}</h3>
 
       <p>
         {' Posted by  '}
 
-        <UserInfo user={post.user} />
+        <UserInfo user={user} />
       </p>
     </div>
 
-    <p className="PostInfo__body">{post.body}</p>
+    <p className="PostInfo__body">{body}</p>
 
     <hr />
 
-    <CommentList comments={post.comments} />
+    <CommentList comments={comments} />
   </div>
 );
