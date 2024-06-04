8f2b2f38b7e62bfc0825d7b2851e146d575026f4
Yaroslav Halynskyi <125606535+Yaroslav65@users.noreply.github.com>
2024-06-04 20:53:40 +0200

    static-list-of-posts

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,23 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+export const PostInfo = ({ post, comments }) => {
+  const postComments = comments
+    ? comments.filter(comment => comment.postId === post.id)
+    : [];
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+        {post.user ? <UserInfo user={post.user} /> : ''}
+      </div>
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      <CommentList comments={postComments} />
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,11 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts, comments }) => (
+  <div className="PostList">
+    {posts.length !== 0
+      ? posts.map(post => (
+          <PostInfo key={post.id} post={post} comments={comments} />
+        ))
+      : null}
+  </div>
+);
