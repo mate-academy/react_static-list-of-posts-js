6404f1c32f723fc85958a65338619fb0d1c4e20c
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-06-06 07:53:25 +0300

    fix task

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,15 @@ import usersFromServer from './api/users.json';
 
 import { PostList } from './components/PostList/PostList';
 
-// Получение пользователей
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId) || null;
 }
 
-// Получение комментов
 function getCommentById(postId) {
-  return (
-    commentsFromServer.filter(comment => comment.postId === postId) || null
-  );
+  return commentsFromServer.filter(comment => comment.postId === postId);
 }
 
-// Создаём один общий объект для использования
-export const posts = postsFromServer.map(post => ({
+export const preparedPosts = postsFromServer.map(post => ({
   ...post,
   user: getUserById(post.userId),
   comments: getCommentById(post.id),
@@ -28,6 +23,6 @@ export const posts = postsFromServer.map(post => ({
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <PostList posts={posts} />
+    <PostList posts={preparedPosts} />
   </section>
 );
