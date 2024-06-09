b5e787bb18798a7637a6820a9066c964787aef5a
Liudmyla Hnydyn <gnydynluda@gmail.com>
2024-06-08 20:14:01 -0400

    Solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,14 @@
-export const PostList = () => <>Put the list here</>;
+// eslint-disable-next-line import/no-cycle
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => (
+  <>
+    <div className="PostList">
+      <div className="posts">
+        {posts.map(post => (
+          <PostInfo post={post} key={post.id} />
+        ))}
+      </div>
+    </div>
+  </>
+);
