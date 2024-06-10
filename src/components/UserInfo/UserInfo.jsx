dc89a3a874b2db52c57174631f9cbc8326fba1af
Nataliia Honcharuk <133349158+Natallia3105@users.noreply.github.com>
2024-06-10 16:06:23 +0200

    Update src/components/UserInfo/UserInfo.jsx
    Co-authored-by: anastasiiavorobiova <135232001+anastasiiavorobiova@users.noreply.github.com>

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -4,10 +4,8 @@ export const UserInfo = ({ user }) => {
   const { email, name } = user;
 
   return (
-    <>
       <a className="UserInfo" href={`mailto:${email}`}>
         {name}
       </a>
-    </>
   );
 };
