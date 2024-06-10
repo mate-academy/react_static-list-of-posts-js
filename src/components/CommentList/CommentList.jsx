15ee8a688eaebe557857dc6b26dc80433b3c4fd2
Nataliia Honcharuk <133349158+Natallia3105@users.noreply.github.com>
2024-06-10 16:06:28 +0200

    Update src/components/CommentList/CommentList.jsx
    Co-authored-by: anastasiiavorobiova <135232001+anastasiiavorobiova@users.noreply.github.com>

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -2,10 +2,10 @@ import './CommentList.scss';
 import { CommentInfo } from '../CommentInfo/CommentInfo';
 
 export const CommentList = ({ comments }) => (
-  <>
+
     <div className="CommentList" />
     {comments.map(comment => (
       <CommentInfo key={comment.id} comment={comment} />
     ))}
-  </>
+    </div>
 );
