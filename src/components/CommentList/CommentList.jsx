fa4184acc54510e06cb7f83846b744e55490f505
Les Dyman <lesdyman@gmail.com>
2024-06-11 22:06:13 +0200

    add solution for the react static list of posts task

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,17 @@
-export const CommentList = () => <>Put the list here</>;
+import React from 'react';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => {
+  return comments.map(com => (
+    <div key={com.id} className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{com.name}</strong>
+        {' by '}
+        <a className="CommentInfo__email" href={`mailto:${com.email}`}>
+          {com.email}
+        </a>
+      </div>
+      <div className="CommentInfo__body">{com.body}</div>
+    </div>
+  ));
+};
