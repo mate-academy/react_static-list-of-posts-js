e81989d69cd8000ce21712764f8eb97d02aaa20b
Sasha Stopochkin <stopochkin23@gmail.com>
2024-06-12 14:11:53 +0300

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,40 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <UserInfo user={post.user} />
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    {post.comments && post.comments.length > 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+
+    {/* <div className="CommentList">
+      <div className="CommentInfo">
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">{comment.name}</strong>
+
+          {' by '}
+
+          <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+            {comment.email}
+          </a>
+        </div>
+
+        <div className="CommentInfo__body">{comment.body}</div>
+      </div>
+    </div> */}
+  </div>
+);
