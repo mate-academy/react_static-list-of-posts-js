1c9c62c4da8330ee46ae91cf440529d458cab9d6
Sasha Stopochkin <stopochkin23@gmail.com>
2024-06-12 14:36:35 +0300

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -20,21 +20,5 @@ export const PostInfo = ({ post }) => (
     ) : (
       <b data-cy="NoCommentsMessage">No comments yet</b>
     )}
-
-    {/* <div className="CommentList">
-      <div className="CommentInfo">
-        <div className="CommentInfo__title">
-          <strong className="CommentInfo__name">{comment.name}</strong>
-
-          {' by '}
-
-          <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
-            {comment.email}
-          </a>
-        </div>
-
-        <div className="CommentInfo__body">{comment.body}</div>
-      </div>
-    </div> */}
   </div>
 );
