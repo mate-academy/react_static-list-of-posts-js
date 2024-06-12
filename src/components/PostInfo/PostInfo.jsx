b3bca484640a75982ce9d24be9e3c115f0c9e212
Veronika Demko <demkoveronika06@gmail.com>
2024-06-12 19:01:09 +0300

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,18 +2,22 @@ import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo = ({ post }) => {
+  const { title, user, body, comments } = post;
 
-      {post.user && <UserInfo user={post.user} />}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        {user && <UserInfo user={user} />}
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      <hr />
+
+      {comments && <CommentList comments={comments} />}
     </div>
-
-    <p className="PostInfo__body">{post.body}</p>
-
-    <hr />
-
-    {post.comments && <CommentList comments={post.comments} />}
-  </div>
-);
+  );
+};
