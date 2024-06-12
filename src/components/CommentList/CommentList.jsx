096e61ae226b82bf3efed2e1cce3ad0db303d5b7
MariCodec <masha1994la@gmail.com>
2024-06-12 14:59:52 +0300

    destructured

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,11 +1,10 @@
-import './CommentList.scss';
 import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
 
 export const CommentList = ({ comments }) => (
-  <>
-    <div className="CommentList" />
+  <div className="CommentList">
     {comments.map(comment => (
       <CommentInfo key={comment.id} comment={comment} />
     ))}
-  </>
+  </div>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,25 +1,29 @@
-import { CommentList } from '../CommentList/CommentList';
-import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo = ({ post }) => {
+  const { title, user, body, comments } = post;
 
-      <p>
-        {' Posted by  '}
-        <UserInfo user={post.user} />
-      </p>
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+
+        <p>
+          {' Posted by  '}
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      <hr />
+      {comments.length === 0 ? (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      ) : (
+        <CommentList comments={comments} />
+      )}
     </div>
-
-    <p className="PostInfo__body">{post.body}</p>
-
-    <hr />
-    {post.comments.length === 0 ? (
-      <b data-cy="NoCommentsMessage">No comments yet</b>
-    ) : (
-      <CommentList comments={post.comments} />
-    )}
-  </div>
-);
+  );
+};
