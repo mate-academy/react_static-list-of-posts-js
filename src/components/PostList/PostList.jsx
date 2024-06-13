0edd02c701f4cf6a7d2eb89d277541cff4746507
Serdiuk Sergii <sergejserduk096@gmail.com>
2024-06-13 13:24:22 +0300

    add solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,7 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ post }) => (
+  <div className="PostList">
+    <PostInfo post={post} />
+  </div>
+);
