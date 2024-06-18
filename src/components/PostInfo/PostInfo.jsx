6849fc4c13b3f2206870cd931896bdeab8d8e1bd
Artur Yaroshenko <free.shok2@gmail.com>
2024-06-18 14:04:29 +0300

    Solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,23 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+export const PostInfo = ({ user, post, comments }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      {user ? <UserInfo user={user} /> : null}
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    {comments && comments.length > 2 ? (
+      <CommentList comments={comments} />
+    ) : (
+      <>
+        <hr />
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+    )}
+  </div>
+);
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,14 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ user, posts, comments }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo
+        key={post.id}
+        user={user.find(person => person.id === post.userId)}
+        post={post}
+        comments={comments.filter(comm => comm.postId === post.id)}
+      />
+    ))}
+  </div>
+);
