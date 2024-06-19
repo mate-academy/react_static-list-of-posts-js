af78925d560754c5b316d9aed01df1d3a9f3eafd
Veniamin <teslaanazuma@gmail.com>
2024-06-19 12:02:05 +0200

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,11 +1,18 @@
 import './CommentList.scss';
 import { CommentInfo } from '../CommentInfo/CommentInfo';
 
-export const CommentList = ({ comments }) => (
-  <>
-    <div className="CommentList" />
-    {comments.map(comment => (
-      <CommentInfo key={comment.id} comment={comment} />
-    ))}
-  </>
-);
+export const CommentList = ({ comments }) => {
+  if (!comments) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="CommentList">
+        {comments.map(comment => (
+          <CommentInfo key={comment.id} comment={comment} />
+        ))}
+      </div>
+    </>
+  );
+};
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,24 +2,23 @@ import { CommentList } from '../CommentList/CommentList';
 import { UserInfo } from '../UserInfo/UserInfo';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo = ({ post: { body, title, user, comments } }) => {
+  return (
+    <>
+      <div className="PostInfo">
+        <div className="PostInfo__header">
+          <h3 className="PostInfo__title">{title}</h3>
 
-      <p>
-        {' Posted by  '}
-        <UserInfo user={post.user} />
-      </p>
-    </div>
+          <UserInfo user={user} />
+        </div>
+        <p className="PostInfo__body">{body}</p>
 
-    <p className="PostInfo__body">{post.body}</p>
-
-    <hr />
-    {post.comments.length === 0 ? (
-      <b data-cy="NoCommentsMessage">No comments yet</b>
-    ) : (
-      <CommentList comments={post.comments} />
-    )}
-  </div>
-);
+        {comments.length ? (
+          <CommentList comments={comments} />
+        ) : (
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        )}
+      </div>
+    </>
+  );
+};
