39a3df20e4731e6800c84695325559a34afbd96f
akopp90 <62627921+akopp90@users.noreply.github.com>
2024-07-07 12:29:55 +0200

    done

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,27 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+import './PostInfo.scss';
+
+const NO_COMMENTS = 'No comments yet';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <UserInfo user={post.user} />
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    {!post.comments.length ? (
+      <div>
+        <hr />
+
+        <b data-cy="NoCommentsMessage">{NO_COMMENTS}</b>
+      </div>
+    ) : (
+      <CommentList comments={post.comments} />
+    )}
+  </div>
+);
