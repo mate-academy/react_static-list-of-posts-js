94c412316ab1a688fbf1c0f7cdf2e34fa1dd046d
Bohdan Smachylo <smachyloooo@gmail.com>
2024-07-12 16:49:55 +0200

    add destructuring

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,23 +3,25 @@ import { UserInfo } from '../UserInfo/UserInfo';
 import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => {
+  const { title, user, body, comments } = post;
+
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
+        <h3 className="PostInfo__title">{title}</h3>
 
         <p>
           {' Posted by  '}
-          <UserInfo user={post.user} />
+          <UserInfo user={user} />
         </p>
       </div>
 
-      <p className="PostInfo__body">{post.body}</p>
+      <p className="PostInfo__body">{body}</p>
 
       <hr />
 
-      {post.comments.length > 0 ? (
-        <CommentList comments={post.comments} />
+      {comments.length > 0 ? (
+        <CommentList comments={comments} />
       ) : (
         <b data-cy="NoCommentsMessage">No comments yet</b>
       )}
