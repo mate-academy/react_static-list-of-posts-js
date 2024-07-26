23b5b84f393362867e044965863569603a614b2e
Oleksii Kislukhin <kislukhin.lesha@gmail.com>
2024-07-26 16:32:36 +0300

    add solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,6 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) =>
+  comments.map(comment => <CommentInfo comment={comment} key={comment.id} />);
