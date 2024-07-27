545058c9c107e62c45a2f0c17d4cb00a15b43b9f
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-07-27 13:40:39 +0300

    add solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,3 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => <div>{comment.body}</div>;
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,16 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ post }) => (
+  <>
+    {post.comment.length > 0 ? (
+      <>
+        {post.comment.map(com => {
+          return <CommentInfo comment={com} key={com.id} />;
+        })}
+      </>
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </>
+);
