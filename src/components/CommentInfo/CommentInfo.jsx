7c4853949578b36df86290ebe98a06aee3fa50d1
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-07-27 14:01:32 +0300

    add style to commentInfo

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,14 @@
+// import { comment } from 'postcss';
 import './CommentInfo.scss';
 
-export const CommentInfo = ({ comment }) => <div>{comment.body}</div>;
+export const CommentInfo = ({ comment }) => (
+  <div className="CommentInfo" style={{ border: '1px solid #fff' }}>
+    <div className="CommentInfo__name" style={{ fontWeight: 'bold' }}>
+      {comment.name}
+    </div>
+    <div className="CommentInfo__email" style={{ color: 'gray' }}>
+      email:{comment.email}
+    </div>
+    <div className="CommentInfo__body">{comment.body}</div>
+  </div>
+);
