016128208d88fb66ca49830712c7ecd130f1ab6d
Melnyk Andriian <andrian_m@ukr.net>
2024-07-31 11:33:09 +0300

    solution was added

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,37 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => {
+  const comments = post.comments.length ? post.comments : [];
+
+  const displayComments = commentsList =>
+    commentsList.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ));
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={post.user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      {comments.length ? (
+        displayComments(comments)
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
