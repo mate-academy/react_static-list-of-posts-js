a44733f7d0f663bc2958737f635090941a33ccf5
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-08-01 18:35:18 +0300

    fix problems with excessive symbol

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,11 +1,9 @@
 import { PostInfo } from '../PostInfo/PostInfo';
 
 export const PostList = ({ posts }) => (
-  <>
-    <div className="PostList">
-      {posts.map(post => (
-        <PostInfo post={post} key={post.id} />
-      ))}
-    </div>
-  </>
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </div>
 );
