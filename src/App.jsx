f453eaa77fe387596d5045959d12d1914b3edbf6
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-08-01 18:29:59 +0300

    react_static-list-of-posts-js second try

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
 
-export const App = () => (
-  <section className="App">
-    <h1 className="App__title">Static list of posts</h1>
-    <PostList
-      posts={postsFromServer.map(post => ({
-        ...post,
-        user: usersFromServer.find(user => user.id === post.userId) || null,
-        comments: commentsFromServer.filter(
-          comment => comment.postId === post.id,
-        ),
-      }))}
-    />
-  </section>
-);
+export const App = () => {
+  const posts = postsFromServer.map(post => ({
+    ...post,
+    user: usersFromServer.find(user => user.id === post.userId),
+    comments: commentsFromServer.filter(comment => comment.postId === post.id),
+  }));
+
+  return (
+    <section className="App">
+      <h1 className="App__title">Static list of posts</h1>
+      <PostList posts={posts} />
+    </section>
+  );
+};
