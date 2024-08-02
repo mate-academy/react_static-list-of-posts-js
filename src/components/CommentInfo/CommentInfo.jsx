201fda9780a531dfb8ac01aea6dafd0f07e477c6
andreinik1 <125960028+andreinik1@users.noreply.github.com>
2024-08-02 10:10:37 +0300

    add task solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,25 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comm }) => {
+  if (!comm) {
+    console.error('Received undefined or null comment prop');
+
+    return null;
+  }
+
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{comm.name}</strong>
+
+        {' by '}
+
+        <a className="CommentInfo__email" href={`mailto:${comm.email}`}>
+          {comm.email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">{comm.body}</div>
+    </div>
+  );
+};
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,16 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comms }) => {
+  if (!Array.isArray(comms) || comms.length === 0) {
+    return <p>No comments available</p>;
+  }
+
+  return (
+    <div className="CommentList">
+      {comms.map(comm => (
+        <CommentInfo key={comm.id} comm={comm} />
+      ))}
+    </div>
+  );
+};
