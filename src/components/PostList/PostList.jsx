228c2a39f3c0441a5fb2ea3bf02154fa50b59609
Oleksii Shpak <willwildred@gmail.com>
2024-08-02 19:34:57 -0600

    no user = no comment

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -2,8 +2,6 @@ import { PostInfo } from '../PostInfo/PostInfo';
 
 export const PostList = ({ posts }) => (
   <div className="PostList">
-    {posts.map(post => (
-      <PostInfo post={post} key={post.id} />
-    ))}
+    {posts.map(post => post.user && <PostInfo post={post} key={post.id} />)}
   </div>
 );
