eff4beefa68b1b511609a0a88438eb76ef3e2fdd
Tanya Baletska <t.baletska@gmail.com>
2024-08-07 22:03:10 +0300

    fixed

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,9 +1,12 @@
 import { CommentInfo } from '../CommentInfo/CommentInfo';
 import './CommentList.scss';
 
-export const CommentList = ({ comments }) =>
-  comments.map(comment => (
-    <div className="CommentList" key={comment.id}>
-      <CommentInfo comment={comment} />
-    </div>
-  ));
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <div key={comment.id}>
+        <CommentInfo comment={comment} />
+      </div>
+    ))}
+  </div>
+);
