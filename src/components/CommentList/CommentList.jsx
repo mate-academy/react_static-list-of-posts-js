cb549a868210185a768e08a4a03f2e035b68653a
voidmain69 <photoreznichenko@gmail.com>
2024-08-15 21:09:53 +0300

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,11 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  if (comments.length === 0) {
+    return <b data-cy="NoCommentsMessage">No comments yet</b>;
+  }
+
+  return comments.map(comment => (
+    <CommentInfo comment={comment} key={comment.id} />
+  ));
+};
