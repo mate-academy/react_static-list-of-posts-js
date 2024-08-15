0e0c8f7084c25849703819721ea0faef72fece7e
Valentyn Radobenko <valikradobenko@gmail.com>
2024-08-15 20:38:20 +0300

    improve code style with destructuring

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,20 +2,24 @@ import { UserInfo } from '../UserInfo/UserInfo';
 import { CommentList } from '../CommentList';
 import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo = ({ post }) => {
+  const { title, user, body, comments } = post;
 
-      <p>
-        {' Posted by  '}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
 
-        <UserInfo user={post.user} />
-      </p>
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{body}</p>
+
+      <CommentList comments={comments} />
     </div>
-
-    <p className="PostInfo__body">{post.body}</p>
-
-    <CommentList comments={post.comments} />
-  </div>
-);
+  );
+};
