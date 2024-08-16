149a702ed0208c4eef9cc17a11821400264325b4
NataliaTopornitska <natalia.topornitska3@gmail.com>
2024-08-16 10:56:54 +0300

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,13 @@
-export const CommentList = () => <>Put the list here</>;
+// export const CommentList = () => <>Put the list here</>;
+
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  if (comments.length === 0) {
+    return <b data-cy="NoCommentsMessage">No comments yet</b>;
+  }
+
+  return comments.map(comment => (
+    <CommentInfo comment={comment} key={comment.id} />
+  ));
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,6 @@
-export const PostList = () => <>Put the list here</>;
+// export const PostList = () => <>Put the list here</>;
+
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) =>
+  posts.map(post => <PostInfo post={post} key={post.id} />);
