1a93bbda88893e3c7ae569f07ea7586aa8097d69
luter <newgglutt@gmail.com>
2024-08-18 22:41:30 +0300

    proba2

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -4,9 +4,18 @@ export const PostList = ({ posts, users, comments }) => (
   <div className="PostList">
     {posts.map(post => {
       const author = users.find(user => user.id === post.userId);
-      const postComments = comments.filter(comment => comment.postId === post.id);
+      const postComments = comments.filter(
+        comment => comment.postId === post.id,
+      );
 
-      return <PostInfo key={post.id} post={post} user={author} comments={postComments} />;
+      return (
+        <PostInfo
+          key={post.id}
+          post={post}
+          user={author}
+          comments={postComments}
+        />
+      );
     })}
   </div>
 );
