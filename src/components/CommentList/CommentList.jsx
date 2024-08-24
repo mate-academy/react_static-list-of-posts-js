e5d0bf1cea81e2243e0b8ceceb33b518d7f51286
Mainaiev <farid.mainaiev@proton.me>
2024-08-24 08:38:57 +0300

    first commit

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,20 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  if (!comments || comments.length === 0) {
+    return (
+      <div className="CommentList" data-cy="NoCommentsMessage">
+        No comments available.
+      </div>
+    );
+  }
+
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
