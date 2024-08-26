dd841ca6e06ce2e81f17a1ea30ab2205a1c29f9a
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-08-26 19:45:40 +0300

    task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,26 @@
 import './App.scss';
-
 import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
+import { PostList } from './components/PostList/PostList';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+function getCommentById(postId) {
+  return commentsFromServer.filter(comment => comment.postId === postId);
+}
+
+export const posts = postsFromServer.map(post => ({
+  ...post,
+  comments: getCommentById(post.id),
+  user: getUserById(post.userId),
+}));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <div className="PostList">
-      {postsFromServer.map(posts => {
-        const user = usersFromServer.find(users => posts.userId === users.id);
-        const comment = commentsFromServer.filter(
-          comments => posts.id === comments.postId,
-        );
-
-        return (
-          <div className="PostInfo">
-            <div className="PostInfo__header">
-              <h3 className="PostInfo__title">{posts.title}</h3>
-              <p>
-                {' Posted by  '}
-
-                <a className="UserInfo" href={`mailto:${user.email}`}>
-                  {user.name}
-                </a>
-              </p>
-            </div>
-            <p className="PostInfo__body">{posts.body}</p>
-            {comment.length > 0 && (
-              <div className="CommentList">
-                {comment.map(com => (
-                  <div className="CommentInfo">
-                    <div className="CommentInfo__title">
-                      <strong className="CommentInfo__name">{com.name}</strong>
-
-                      {' by '}
-
-                      <a
-                        className="CommentInfo__email"
-                        href={`mailto:${com.email}`}
-                      >
-                        {com.email}
-                      </a>
-                    </div>
-
-                    <div className="CommentInfo__body">{com.body}</div>
-                  </div>
-                ))}
-              </div>
-            )}
-            {comment.length === 0 && (
-              <>
-                <hr />
-                <b data-cy="NoCommentsMessage">No comments yet</b>
-              </>
-            )}
-          </div>
-        );
-      })}
-    </div>
+    <PostList posts={posts} />
   </section>
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,10 @@
-export const UserInfo = () => <>Put the user here</>;
+import React from 'react';
+
+export const UserInfo = ({ user }) => (
+  <p>
+    {' Posted by  '}
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  </p>
+);
