635ca49dd466ae199056981535322c20f750c6f8
Vlad Zahorovskyi <zagorovskii12@gmail.com>
2024-08-27 20:01:17 +0300

    code was improved

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -15,7 +15,7 @@ export const PostInfo = ({ post }) => {
         </p>
       </div>
       <p className="PostInfo__body">{body}</p>
-      {comments.length !== 0 ? (
+      {!!comments.length ? (
         <CommentList comments={comments} />
       ) : (
         <>
