f4b25d8b8939936d14cc4a16981f82dea49f222d
hardaira <hardaira@yahoo.com>
2024-09-07 21:16:48 +0200

    add solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,24 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import './PostInfo.scss';
+import { CommentList } from '../CommentList/CommentList';
+import commentsFromServer from '../../api/comments.json';
+
+export const PostInfo = ({ post }) => (
+  <>
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+        {post.user && <UserInfo user={post.user} />}
+      </p>
+    </div>
+    <p className="PostInfo__body">{post.body}</p>
+    <hr />
+    <CommentList
+      comments={commentsFromServer.filter(
+        comment => comment.postId === post.id,
+      )}
+    />
+  </>
+);
