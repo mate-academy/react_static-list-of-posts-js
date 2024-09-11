1706d2af8cb4323019dd893020979c8f7967189c
Daria Nastas <bycinska@gmail.com>
2024-09-11 13:13:33 +0300

    Update src/components/PostInfo/PostInfo.jsx
    Co-authored-by: Denys Semtso <70220667+etojeDenys@users.noreply.github.com>

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -8,7 +8,7 @@ export const PostInfo = ({ post: { title, user, body, comments } }) => (
       <h3 className="PostInfo__title">{title}</h3>
       <p>
         {' Posted by '}
-        <UserInfo user={user} />
+        {user && <UserInfo user={user} />}
       </p>
     </header>
     <p className="PostInfo__body">{body}</p>
