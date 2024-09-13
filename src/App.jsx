4baa27c058e031c82bfdb79d42d9cc1b1e499d8e
Bohdan Horiunov <b.horiunov@gmail.com>
2024-09-13 17:10:43 +0300

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,20 @@ import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
 
+const getUserInfo = userId => usersFromServer.find(user => user.id === userId);
+
+const getComments = postId =>
+  commentsFromServer.filter(comment => comment.postId === postId);
+
+const posts = postsFromServer.map(post => ({
+  ...post,
+  user: getUserInfo(post.userId),
+  comments: getComments(post.id),
+}));
+
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <PostList
-      posts={postsFromServer}
-      users={usersFromServer}
-      comments={commentsFromServer}
-    />
+    <PostList posts={posts} />
   </section>
 );
