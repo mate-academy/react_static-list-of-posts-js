9cc324f04557c13552bcdb2c7124d8e06b1a3b87
Maksym Mohyla <greenparksquad@gmail.com>
2024-09-14 12:47:59 +0300

    list of posts solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,10 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+import './PostList.scss';
+
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(postInlist => (
+      <PostInfo key={postInlist.id} post={postInlist} />
+    ))}
+  </div>
+);
