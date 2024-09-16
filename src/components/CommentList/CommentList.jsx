5b403a7fdaf2f40fb99dda03a1da607cb490b541
Влад Теут <vladteut@Teutss-Air.localdomain>
2024-09-16 20:18:08 +0300

    taskListOfPosts

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,20 @@
-export const CommentList = () => <>Put the list here</>;
+import React from 'react';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => {
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {comments.length === 0 ? (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      ) : (
+        <div className="CommentList">
+          {comments.map(comment => (
+            <CommentInfo comment={comment} key={comment.id} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
