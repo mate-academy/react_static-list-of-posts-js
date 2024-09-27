46c0a7d7dbacb609586d9483ed06c8735f5246f0
Kateryna <moskkat40@gmail.com>
2024-09-27 09:15:17 +0200

    add task react static post list

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,20 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const PostInfo = ({ post: { title, body, comments, user } }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{title}</h3>
+    </div>
+    <UserInfo user={user} />
+    <p className="PostInfo__body">{body}</p>
+
+    <hr />
+    {comments.length === 0 ? (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    ) : (
+      <CommentList comments={comments} />
+    )}
+  </div>
+);
