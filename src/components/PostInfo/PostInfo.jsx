b0dff019de50a63c40af105dafbcd53f053299cc
Vladimir Orlov <orelik392@gmail.com>
2024-10-01 14:50:29 +0300

    result

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,28 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  const { title, body, user } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{title}</h3>
+        <p>
+          {' Posted by '}
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo_body">{body}</p>
+      <hr />
+
+      {post.comment.lenght !== 0 ? (
+        <CommentList comments={post.comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,11 @@
-export const UserInfo = () => <>Put the user here</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <a href={`mailto:${email}`} className="UserInfo">
+      {name}
+    </a>
+  );
+};
