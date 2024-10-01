0ae98e7970fea04ec68c20af717a5409c718dedc
Vladimir Orlov <orelik392@gmail.com>
2024-10-01 14:58:28 +0300

    result

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,24 +1,24 @@
-import './PostInfo.scss';
 import { UserInfo } from '../UserInfo/UserInfo';
 import { CommentList } from '../CommentList/CommentList';
 
 export const PostInfo = ({ post }) => {
-  const { title, body, user } = post;
+  const { title, user, body } = post;
 
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{title}</h3>
+
         <p>
-          {' Posted by '}
+          {' Posted by  '}
           <UserInfo user={user} />
         </p>
       </div>
 
-      <p className="PostInfo_body">{body}</p>
-      <hr />
+      <p className="PostInfo__body">{body}</p>
 
-      {post.comment.lenght !== 0 ? (
+      <hr />
+      {post.comments.length !== 0 ? (
         <CommentList comments={post.comments} />
       ) : (
         <b data-cy="NoCommentsMessage">No comments yet</b>
