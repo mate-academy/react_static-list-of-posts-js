b3fd090ae690c4dff157d3a0273709aa33772d48
Viktoriia <vkkishko@gmail.com>
2024-10-04 12:44:18 +0300

    key added

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -5,7 +5,7 @@ import { PostInfo } from '../PostInfo/PostInfo';
 export const PostList = ({ posts }) => (
   <div className="PostList">
     {posts.map(post => (
-      <PostInfo post={post} />
+      <PostInfo post={post} key={post.id} />
     ))}
   </div>
 );
