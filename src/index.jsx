43a5897088be0b7940f552ac966a8f87a6a20787
Eduard <mr.nerkararyna99@gmail.com>
2024-10-06 16:58:58 +0300

    add task solution

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client';
-
+import { StrictMode } from 'react';
 import { App } from './App';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
