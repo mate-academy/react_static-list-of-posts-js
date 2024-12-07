3423b898c22507cf33b8d88ae828e3b7632856a9
Andrii Tkachuk <kovklad@gmail.com>
2024-12-07 21:37:06 +0100

    add problem with tests

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,13 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <>
+    {' '}
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo comment={comment} key={comment.id} />
+      ))}
+    </div>
+  </>
+);
