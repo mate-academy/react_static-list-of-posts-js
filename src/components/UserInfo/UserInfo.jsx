8bc074ddb64871d33215734f6da1c99fe7da871f
Andrii Tkachuk <kovklad@gmail.com>
2024-12-08 16:03:35 +0100

    Solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,14 @@
-export const UserInfo = () => <>Put the user here</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => (
+  <>
+    {' '}
+    {user ? (
+      <a className="UserInfo" href={`mailto:${user.email}`}>
+        {user.name}
+      </a>
+    ) : (
+      ''
+    )}
+  </>
+);
