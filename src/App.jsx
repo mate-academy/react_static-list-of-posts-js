b5ed55817efe1b5e4a0a6aea3a8cd1389b193814
jarkkokilpe <jarkko.kilpelainen@protonmail.com>
2025-01-04 11:19:22 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+
+import { PostList } from './components/PostList';
+
+function getCommentsByPostId(postId) {
+  return commentsFromServer.filter(comment => comment.postId === postId);
+}
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+export const posts = postsFromServer.map(post => ({
+  ...post,
+  user: getUserById(post.userId),
+  comments: getCommentsByPostId(post.id),
+}));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-
+    <PostList posts={posts} />
+    {/**
     <div className="PostList">
       <div className="PostInfo">
         <div className="PostInfo__header">
@@ -15,7 +32,6 @@ export const App = () => (
 
           <p>
             {' Posted by  '}
-
             <a className="UserInfo" href="mailto:Sincere@april.biz">
               Leanne Graham
             </a>
@@ -100,5 +116,6 @@ export const App = () => (
         </div>
       </div>
     </div>
+    */}
   </section>
 );
