c672706491a11f2235671ddf32f6bf5a9cf3ee95
Vika Ruda <rudaviktoria0@gmail.com>
2025-01-05 10:08:01 +0200

    Solution0501

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,18 @@
-export const CommentInfo = () => <>Put the comment here</>;
+export const CommentInfo = ({ comment }) => (
+  <div>
+    {comment && comment.length > 0 ? (
+      <div className="CommentInfo">
+        <div className="CommentInfo__title">
+          <strong className="CommentInfo__name">{comment.name}</strong>
+          {' by '}
+          <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+            {comment.email}
+          </a>
+        </div>
+        <div className="CommentInfo__body">{comment.body}</div>
+      </div>
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
+);
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,20 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+        {/* <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
+            Patricia Lebsack
+          </a> */}
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+  </div>
+);
