b24800519adb215541c8376d0f2889f21f85b9f8
prutasveta <svetapruta@gmail.com>
2025-01-22 15:58:54 +0100

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,24 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+import usersFromServer from '../../api/users.json';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+        {usersFromServer
+          .filter(user => user.id === post.userId)
+          .map(user => (
+            <UserInfo user={user} key={user.id} />
+          ))}
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <CommentList comments={post.comments} />
+  </div>
+);
