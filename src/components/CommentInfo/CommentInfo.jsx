d296b0683465d974837833a72c98f9c9c515e6c1
Valentyna <adm@adms-MacBook-Pro.local>
2025-01-26 16:18:47 +0200

    add react static list of posts js

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,11 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import React from 'react';
+
+export const CommentInfo = ({ comment }) => (
+  <div className="CommentInfo comment-info">
+    <h4 className="CommentInfo__name">{comment.name}</h4>
+    <p className="CommentInfo__body">{comment.body}</p>
+    <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+      {comment.email}
+    </a>
+  </div>
+);
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,16 @@
-export const PostInfo = () => <>Put the post here</>;
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+export const PostInfo = ({ post }) => (
+  <article className="post-info">
+    <h2 className="PostInfo__title">{post.title}</h2>
+    <p>{post.body}</p>
+    <UserInfo user={post.user} />
+    {post.comments.length === 0 ? (
+      <div data-cy="NoCommentsMessage">No comments yet.</div>
+    ) : (
+      <CommentList comments={post.comments} />
+    )}
+  </article>
+);
