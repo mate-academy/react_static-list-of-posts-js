0c4e60aa4b40c8ca648cd34d601163fb55ac1476
Marius R <36150783+Marsol0@users.noreply.github.com>
2025-02-03 21:12:52 +0200

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,32 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  if (!post.user) {
+    return <div>User not found</div>;
+  }
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+        <p>
+          {`Posted by `}
+          <a className="UserInfo" href={`mailto:${post.user.email}`}>
+            <UserInfo user={post.user} />
+          </a>
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      {post.comments.length === 0 ? (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      ) : (
+        <CommentList comments={post.comments} />
+      )}
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,8 @@
-export const UserInfo = () => <>Put the user here</>;
+export const UserInfo = ({ user }) => (
+  <div className="UserInfo">
+    <p className="UserInfo__name">{user.name}</p>
+    <a className="UserInfo__email" href={`mailto:${user.email}`}>
+      {user.email}
+    </a>
+  </div>
+);
