f2db875bc00efeb7c125d897f7e62fa0347593e8
Yehor Heiko <yehorheiko.developer@gmail.com>
2025-02-09 20:08:18 +0200

    Add Tasks

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,10 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(e => (
+      <CommentInfo comment={e} key={e.id} />
+    ))}
+  </div>
+);
