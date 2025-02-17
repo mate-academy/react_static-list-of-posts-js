5c0e0b93998b0d86539aef5c1caae1ae6465bba1
Oleksii Petrychenko <apetr@MacBook-Air.local>
2025-02-17 17:10:06 +0200

    add task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,15 @@
-export const PostInfo = () => <>Put the post here</>;
+import React from 'react';
+import { CommentList } from '../CommentList';
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <h2 className="PostInfo__title">{post.title}</h2>
+    <p className="PostInfo__body">{post.body}</p>
+    <p className="PostInfo__user">Author: {post.user.name}</p>
+    <div className="UserInfo">
+      <p className="UserInfo__name">{post.user.name}</p>
+    </div>
+    <CommentList comments={post.comments} />
+  </div>
+);
