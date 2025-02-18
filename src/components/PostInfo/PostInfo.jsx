23259430b176e285bc05416b8d9751189f080925
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-02-18 12:44:05 +0200

    add solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,41 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+// import usersFromServer from '../../api/users.json';
+// import commentsFromServer from '../../api/comments.json';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  // const getUserById = userId => {
+  //   return usersFromServer.find(user => user.id === userId);
+  // };
+
+  // const getCommentsByPostId = postId => {
+  //   return commentsFromServer.filter(comment => comment.postId === postId);
+  // };
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        {post.user && (
+          <p>
+            {' Posted by  '}
+
+            <UserInfo user={post.user} />
+          </p>
+        )}
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      {post.comments.length ? (
+        <CommentList comments={post.comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,38 @@
-export const PostList = () => <>Put the list here</>;
+import './PostList.scss';
+import { PostInfo } from '../PostInfo/PostInfo';
+import usersFromServer from '../../api/users.json';
+import commentsFromServer from '../../api/comments.json';
+
+export const PostList = ({ posts }) => {
+  // const getUserById = userId => {
+  //   return usersFromServer.find(user => user.id === userId);
+  // };
+
+  // const getCommentsByPostId = postId => {
+  //   return commentsFromServer.filter(comment => comment.postId === postId);
+  // };
+
+  const getPostWithComments = post => {
+    const user = usersFromServer.find(u => u.id === post.userId);
+    const comments = commentsFromServer.filter(
+      comment => comment.postId === post.id,
+    );
+
+    return {
+      userId: user.id,
+      id: post.id,
+      title: post.title,
+      body: post.body,
+      user,
+      comments,
+    };
+  };
+
+  return (
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo key={post.id} post={getPostWithComments(post)} />
+      ))}
+    </div>
+  );
+};
