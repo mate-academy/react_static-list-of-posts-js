940769f5e5a769c5ae5940d9d97836d2c1ef5d7a
Andrew <andrijvasilik252@gmail.com>
2025-02-19 00:22:47 +0200

    first commit

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,11 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => {
+  return (
+    <section className="TodoList">
+      {posts.map(post => (
+        <PostInfo key={post.id} post={post} />
+      ))}
+    </section>
+  );
+};
