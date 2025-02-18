2f3d70d32a622c08d35bd3c8a3154c5b5e2ae4ae
Albina <yakunina.albina@gmail.com>
2025-02-18 10:13:50 +0100

    Add changes new-develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+
+export const users = () => (
+  
+);
 
 export const App = () => (
   <section className="App">
