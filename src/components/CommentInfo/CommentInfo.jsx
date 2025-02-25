d996a4c2f493ef07cc46bdc3836e4799e5a2b909
mmromanova <emy2004.bo@gmail.com>
2025-02-25 23:04:52 +0100

    solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,10 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import React from 'react';
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => (
+  <div className="CommentInfo">
+    <p className="CommentInfo__name">{comment.name}</p>
+    <p className="CommentInfo__body">{comment.body}</p>
+    <p className="CommentInfo__email" href={`mailto:${comment.email}`}>{comment.email}</p>
+  </div>
+);
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,10 @@
-export const CommentList = () => <>Put the list here</>;
+import React from 'react';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <section className="Comment__List">
+    {comments.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ))}
+  </section>
+);
