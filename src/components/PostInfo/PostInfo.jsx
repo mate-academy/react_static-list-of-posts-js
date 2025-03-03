3c961d06d9c724984be134b539897a70567ec580
Vitor MF <vitorfreire419@gmail.com>
2025-03-03 15:39:01 -0300

    done

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,35 @@
-export const PostInfo = () => <>Put the post here</>;
+import usersFromServer from '../../api/users.json';
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+import './PostInfo.scss';
+
+const findUser = userId => {
+  return usersFromServer.find(user => user.id === userId);
+};
+
+export const PostInfo = ({ post }) => {
+  const user = findUser(post.userId);
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+        <p>
+          {' Posted by '}
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      {post.comments && post.comments.length > 0 ? (
+        <CommentList comments={post.comments} />
+      ) : (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,9 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ postsFromServer }) => (
+  <div className="PostList">
+    {postsFromServer.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </div>
+);
