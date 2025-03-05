86003ca543fcf0d99e6f0146f68465b10fda229f
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-03-05 18:30:32 +0200

    task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,15 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import React from 'react';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.length === 0 ? (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    ) : (
+      comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))
+    )}
+  </div>
+);
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,37 @@
-export const PostList = () => <>Put the list here</>;
+import React, { useState, useEffect } from 'react';
+import { PostInfo } from '../PostInfo/PostInfo';
+
+import postsFromServer from '../../api/posts.json';
+import commentsFromServer from '../../api/comments.json';
+import usersFromServer from '../../api/users.json';
+
+export const PostList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const postsWithCommentsAndUsers = postsFromServer
+      .map(post => {
+        const postComments = commentsFromServer.filter(
+          comment => comment.postId === post.id,
+        );
+        const postUser = usersFromServer.find(user => user.id === post.userId);
+
+        return {
+          ...post,
+          user: postUser,
+          comments: postComments,
+        };
+      })
+      .sort((a, b) => a.id - b.id);
+
+    setPosts(postsWithCommentsAndUsers);
+  }, []);
+
+  return (
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
