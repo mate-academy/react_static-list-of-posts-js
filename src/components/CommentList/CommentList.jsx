3adfd9e3156443bb21dde2e3dc5a17c1a1ad8fb4
Uusuff <90629840+Uusuff@users.noreply.github.com>
2025-03-06 19:33:44 +0100

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,20 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => {
+  return (
+    <div className="CommentList">
+      {comments.length > 0 ? (
+        comments.map(comment => (
+          <CommentInfo comment={comment} key={comment.id} />
+        ))
+      ) : (
+        <>
+          <hr />
+
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )}
+    </div>
+  );
+};
