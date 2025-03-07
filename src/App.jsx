e608fd87ed6fe321d0ff6ca75a68bb5605b0e314
Nadiia Bulmak <nadgaval@gmail.com>
2025-03-07 20:05:48 +0200

    add render solution

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList/PostList';
 
 function getUserById(userId) {
-  return usersFromServer.find(user => user.id === userId) || null;
+  return (
+    usersFromServer.find(user => user.id === userId) || {
+      id: 0,
+      name: 'Unknown User',
+      email: 'unknown@example.com',
+    }
+  );
 }
 
 function getComment(idOfPost) {
