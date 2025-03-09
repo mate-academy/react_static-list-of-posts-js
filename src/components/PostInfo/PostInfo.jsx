f39e24166bcdeb06664b55e40c7f2b1db234e1c9
Kyrylo Bondarenko <dejsk35@gmail.com>
2025-03-09 19:00:53 +0200

    solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,29 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  const { user } = post;
+  let classPostInfo = 'NoCommentsMessage';
+
+  if (post.comments.length !== 0) {
+    classPostInfo = 'CommentInfo__body';
+  }
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+        <UserInfo key={user.id} user={user} />
+      </div>
+
+      <p className="PostInfo__body"> {post.body} </p>
+
+      <hr />
+      {classPostInfo === 'NoCommentsMessage' ? (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      ) : (
+        <CommentList comments={post.comments} />
+      )}
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,13 @@
-export const UserInfo = () => <>Put the user here</>;
+export const UserInfo = ({ user }) => {
+  const userEmail = `mailto:${user.email}`;
+
+  return (
+    <p>
+      {user.name}
+
+      <a className="UserInfo" href={userEmail}>
+        {user.name}
+      </a>
+    </p>
+  );
+};
