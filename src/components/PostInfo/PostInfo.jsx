c4118968142d17f2d7f1dc5313265588b2250de5
Pasha Sich <97337742+Kapidastra@users.noreply.github.com>
2025-04-02 16:15:44 +0200

    Add Solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,34 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+
+import { CommentList } from '../CommentList';
+import { UserInfo } from '../UserInfo';
+
+function existComment(comments) {
+  if (!comments || comments.length === 0) {
+    return <b data-cy="NoCommentsMessage">No comments yet</b>;
+  }
+
+  return <CommentList comments={comments} />;
+}
+
+export const PostInfo = ({ post }) => {
+  const { user, comments } = post;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+        <p>
+          {' Posted by  '}
+          <UserInfo user={user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      {existComment(comments)}
+    </div>
+  );
+};
