ae1201b54e837c22ba8c2ef829d2a8c4bf9536d8
Ihor Kvasha <kvashaihor@gmail.com>
2025-05-05 23:49:46 -0700

    commit 1.0

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,42 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import './PostInfo.scss';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  if (post.comments.length !== 0) {
+    return (
+      <div className="PostInfo">
+        <div className="PostInfo__header">
+          <h3 className="PostInfo__title">{post.title}</h3>
+
+          <p>
+            {' Posted by '}
+            <UserInfo user={post.user} />
+          </p>
+        </div>
+
+        <p className="PostInfo__body">{post.body}</p>
+
+        <CommentList comments={post.comments} />
+      </div>
+    );
+  }
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by '}
+          <UserInfo user={post.user} />
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    </div>
+  );
+};
