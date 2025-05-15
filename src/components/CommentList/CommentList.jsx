7eb53fe37aa5a8f76419c654a9f124db86024a42
Yana Lunina <yanalunina96@gmail.com>
2025-05-15 23:41:24 +0300

    add static-list-of-posts-js

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,17 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  if (comments.length === 0) {
+    return <b data-cy="NoCommentsMessage">No comments yet</b>;
+  }
+
+  return (
+    <div className="CommentList">
+      <h4 className="CommentList__title">Comments:</h4>
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
