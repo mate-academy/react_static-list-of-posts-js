835355fd92cc9a7325f462200fefadd20f173ad4
Taras <taras.holotiuk@gmail.com>
2025-06-12 19:50:05 +0300

    write some tasks

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,12 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments, postId }) => (
+  <div className="CommentList">
+    {comments
+      .filter(comment => comment.postId === postId)
+      .map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+  </div>
+);
