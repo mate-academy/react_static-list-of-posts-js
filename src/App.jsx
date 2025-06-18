f06a4fccf383a1e88f16074bca2f88351307c354
Alexandre Almeida <alexandredd@duck.com>
2025-06-18 14:36:02 -0300

    Fix: variable name preparePosts to preparedPosts

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
 
-const preparePosts = postsFromServer.map(post => {
+const preparedPosts = postsFromServer.map(post => {
   const user = usersFromServer.find(u => u.id === post.userId);
   const comments = commentsFromServer.filter(c => c.postId === post.id);
 
@@ -15,6 +15,6 @@ const preparePosts = postsFromServer.map(post => {
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <PostList posts={preparePosts} />
+    <PostList posts={preparedPosts} />
   </section>
 );
