e686b5cbfe3e713255dc8b1591dad14bb0a55acb
Oleksii Myronenko <oleksiimrnko@gmail.com>
2025-06-22 23:02:13 +0300

    add test solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,19 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts, comments, users }) => (
+  <div className="PostList">
+    {posts.map(post => {
+      const user = users.find(u => u.id === post.userId);
+      const postComments = comments.filter(comment => comment.id === post.id);
+
+      return (
+        <PostInfo
+          post={post}
+          comments={postComments.length > 0 ? postComments : null}
+          users={user}
+          key={post.id}
+        />
+      );
+    })}
+  </div>
+);
