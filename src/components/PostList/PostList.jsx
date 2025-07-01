c6702961c7cdcc31f3994c1e783f25ecba4fc2c5
Vitalii Kredinets <kredinec.v@gmail.com>
2025-07-01 12:36:59 +0300

    solve task

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,13 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+import './PostList.scss';
+
+export const PostList = ({ posts, comments }) => (
+  <div className="Postlist">
+    {posts.length !== 0
+      ? posts.map(post => (
+          <PostInfo key={post.id} post={post} comments={comments} />
+          // eslint-disable-next-line prettier/prettier
+      ))
+      : null}
+  </div>
+);
