3344d59dffafaa368874943c14f6a971ea413ba4
Nádia <nadia.mfaria@hotmail.com>
2025-07-17 22:39:39 -0300

    make a react static list of posts

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,17 @@
-export const CommentList = () => <>Put the list here</>;
+import React from 'react';
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  if (!comments || comments.length === 0) {
+    return <b data-cy="NoCommentsMessage">No comments yet</b>;
+  }
+
+  return (
+    <div className="CommentList" data-cy="CommentList">
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
