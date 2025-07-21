02711ea145f62742b106bdb3e302b94d53eab1b6
Liudmyla Kovbasiuk <lchubatiuk96@gmail.com>
2025-07-21 14:16:08 +0200

    Solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,12 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ post }) =>
+  post.comments.length === 0 ? (
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  ) : (
+    <div className="CommentList">
+      {post.comments.map(comment => (
+        <CommentInfo comment={comment} />
+      ))}
+    </div>
+  );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,9 @@
-export const UserInfo = () => <>Put the user here</>;
+export const UserInfo = ({ post }) => (
+  <p>
+    {' Posted by  '}
+
+    <a className="UserInfo" href={`mailto:${post.user.email}`}>
+      {post.user.name}
+    </a>
+  </p>
+);
