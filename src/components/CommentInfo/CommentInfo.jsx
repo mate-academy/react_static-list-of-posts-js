103dc3d9e6ffb099da33f9a500dc3a73117d4cf3
michelam-pseudo <124099775+michelam-pseudo@users.noreply.github.com>
2025-07-27 13:59:24 -0300

    Correções em exceções estruturais - conteúdo de texto da tag âncora do componente UserInfo

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,17 @@
-export const CommentInfo = () => <>Put the comment here</>;
+export const CommentInfo = ({ comment }) => {
+  return (
+    <div className="CommentInfo">
+      <div className="CommentInfo__title">
+        <strong className="CommentInfo__name">{comment.name}</strong>
+
+        {' by '}
+
+        <a href={`mailto:${comment.email}`} className="CommentInfo__email">
+          {comment.email}
+        </a>
+      </div>
+
+      <div className="CommentInfo__body">{comment.body}</div>
+    </div>
+  );
+};
