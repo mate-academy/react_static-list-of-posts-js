74e3feaec8454831b4d1af103457b667ebcdfa10
dimon <80713na@gmail.com>
2025-09-02 17:58:51 +0300

    static-list

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,9 @@
-export const UserInfo = () => <>Put the user here</>;
+export const UserInfo = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
