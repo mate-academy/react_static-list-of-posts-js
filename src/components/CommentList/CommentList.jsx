4c0901de79fff642548cbe5b5be53389a7720355
Denys Herych <fojjof2@gmail.com>
2025-09-03 16:26:00 +0200

    solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -4,9 +4,10 @@ import { CommentInfo } from '../CommentInfo/CommentInfo';
 export const CommentList = ({ comments }) => (
   <>
     {' '}
-    <div className="CommentList" />
-    {comments.map(comment => (
-      <CommentInfo key={comment.id} comment={comment} />
-    ))}
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
   </>
 );
