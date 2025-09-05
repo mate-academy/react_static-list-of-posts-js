83cd2081fd00ff3454aa5369b829f498b0fcdfb0
haldaniko <haldaniko@gmail.com>
2025-09-05 17:38:18 +0300

    Solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,17 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}n</strong>
+
+      {' by '}
+
+      <a className="CommentInfo__email" href="mailto:Telly_Lynch@karl.co.uk">
+        {comment.email}
+      </a>
+    </div>
+
+    <div className="CommentInfo__body">{comment.body}</div>
+  </div>
+);
