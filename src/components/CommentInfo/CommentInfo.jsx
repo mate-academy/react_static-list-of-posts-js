52716e452db732acca77cc6ad8e1c4420b346391
haldaniko <haldaniko@gmail.com>
2025-09-05 19:58:03 +0300

    Solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -3,11 +3,11 @@ import './CommentInfo.scss';
 export const CommentInfo = ({ comment }) => (
   <div className="CommentInfo">
     <div className="CommentInfo__title">
-      <strong className="CommentInfo__name">{comment.name}n</strong>
+      <strong className="CommentInfo__name">{comment.name}</strong>
 
       {' by '}
 
-      <a className="CommentInfo__email" href="mailto:Telly_Lynch@karl.co.uk">
+      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
         {comment.email}
       </a>
     </div>
