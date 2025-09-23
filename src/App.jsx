77e2a9dde08ba99ac43f3767fb80a106789b4067
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-09-23 15:23:42 -0300

    dasdsa

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList/PostList';
-import { CommentList } from './components/CommentList/CommentList';
-const posts = postsFromServer.map(p => ({
-...p,
-user: usersFromServer.find(u => u.id === p.userId),
-comments: commentsFromServer.filter(c => c.postId === p.id),
-}));
 
+const posts = postsFromServer.map(p => ({
+  ...p,
+  user: usersFromServer.find(u => u.id === p.userId),
+  comments: commentsFromServer.filter(c => c.postId === p.id),
+}));
 
 export const App = () => (
   <div className="App">
