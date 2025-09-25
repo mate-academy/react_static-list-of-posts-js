9d0327fe767518395f56e8428dd7df9eaffdadc4
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-09-25 20:52:41 +0300

    post list

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,29 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+import './PostInfo.scss';
+
+export const PostInfo = ({ info }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{info.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <UserInfo user={info.user} />
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{info.body}</p>
+
+    {info.comments.length > 0 ? (
+      <CommentList comments={info.comments} />
+    ) : (
+      <>
+        <hr />
+
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      </>
+    )}
+  </div>
+);
