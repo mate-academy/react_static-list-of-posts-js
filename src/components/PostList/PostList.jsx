5e59ec57869b33bcf61d2df41a216028c192c29e
Matthew <kindtvorter@gmail.com>
2025-10-06 19:00:12 +0300

    Solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,5 +1,5 @@
 import './PostList.scss';
-import { PostInfo } from '../PostInfo';
+import { PostInfo } from '../PostInfo/PostInfo';
 
 export const PostList = ({ posts }) => (
   <div className="PostList">
