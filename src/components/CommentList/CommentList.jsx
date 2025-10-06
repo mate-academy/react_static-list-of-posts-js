50cbdf8490a09dc1bb4116deb3848729cab9dfa8
Matthew <kindtvorter@gmail.com>
2025-10-06 15:59:30 +0300

    Solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,5 +1,5 @@
 import './CommentList.scss';
-import { CommentInfo } from '../CommentInfo';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
 
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
