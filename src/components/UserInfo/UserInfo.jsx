bab3c3f21de67557e37afc8ba37333f8881a1844
Denys Anokhin <denys.anokhin.dev@gmail.com>
2025-10-10 17:29:57 +0300

    solve 3

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,8 @@
 import './UserInfo.scss';
 
-export const UserInfo = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user.email}`}>
-    {user.name}
-  </a>
-);
+export const UserInfo = ({ user }) =>
+  user && (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
