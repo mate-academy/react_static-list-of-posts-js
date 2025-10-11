f138571e52d7f631eeefe7b2e739c32b0de86107
Vadym-Kit <superovyq@gmai.com>
2025-10-11 20:50:03 +0300

    result

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,11 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+// eslint-disable-next-line import/no-cycle
+import { preparedPosts as posts } from '../../App';
+
+export const PostList = () => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo post={post} key={post.id} />
+    ))}
+  </div>
+);
