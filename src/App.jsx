d48c008080068c2d3c0f8d589c4555f3df6bef5b
Vladyslav Kostiuk <k4nezoor@gmail.com>
2025-10-14 14:40:59 +0200

    fixin saving

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,10 @@ export const posts = postsFromServer.map(post => ({
 }));
 
 export const App = () => (
+
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
     <PostList posts={posts} />
   </section>
+
 );
