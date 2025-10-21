ca1e072e980155e40d6ada764fb89a728298cacc
chumachenkoUA <kirito22122005@gmail.com>
2025-10-21 20:24:07 +0300

    fix: Handle unknown author case in PostInfo component

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -10,7 +10,11 @@ export const PostInfo = ({ post }) => (
 
       <p>
         {' Posted by  '}
-        <UserInfo user={post.user} />
+        {post.user ? (
+          <UserInfo user={post.user} />
+        ) : (
+          <span>Unknown author</span>
+        )}
       </p>
     </div>
 
