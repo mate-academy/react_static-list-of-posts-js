d26a4aea3a777e6f0523dd7240b45c64480c8e05
Roman <hpp1ovh@gmail.com>
2025-11-16 12:57:54 +0200

    complete task, add full render post list

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,15 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  if (comments.length === 0) {
+    return <b data-cy="NoCommentsMessage">No comments yet</b>;
+  }
+
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
