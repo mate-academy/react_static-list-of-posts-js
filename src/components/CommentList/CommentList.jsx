449bb05f70d838f6b2682d348bbc50b0956919a0
InhaKr <inkr3891@gmail.com>
2025-12-08 16:38:43 +0200

    first commit

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,18 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  // console.log('CommentList received:', comments);
+
+  if (!comments || comments.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="CommentList">
+      {comments.map(comment => {
+        // console.log('Rendering comment:', comment);
+        return <CommentInfo key={comment.id} comment={comment} />;
+      })}
+    </div>
+  );
+};
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,27 @@
-export const PostInfo = () => <>Put the post here</>;
+// import { CommentInfo } from '../CommentInfo/CommentInfo';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+// export const PostInfo = () => <>Put the post here</>;
+export const PostInfo = ({ post }) => {
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by '}
+          {post.user && <UserInfo user={post.user} />}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body} </p>
+      <hr />
+      {post.comments && post.comments.length > 0 ? (
+        <CommentList comments={post.comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
