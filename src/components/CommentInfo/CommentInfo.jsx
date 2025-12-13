06bc66722775b8e907054a401f2993040417e92d
Andriy <andrijs373@gmail.com>
2025-12-13 14:44:45 +0200

    add task solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,17 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import React from 'react';
+import comments from '../../api/comments.json';
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ postId }) => {
+  const commentsForPost = postId
+    ? comments.filter(c => c.postId === postId)
+    : comments;
+
+  return (
+    <div>
+      {commentsForPost.map(comment => (
+        <div key={comment.id}>{comment.body}</div>
+      ))}
+    </div>
+  );
+};
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,17 @@
-export const PostInfo = () => <>Put the post here</>;
+import React from 'react';
+import './PostInfo.scss';
+
+import CommentList from '../CommentList/CommentList';
+
+export const PostInfo = ({ body, comments }) => {
+  return (
+    <div className="PostInfo">
+      <p className="PostInfo_body">{body}</p>
+      {comments && comments.length !== 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
+    </div>
+  );
+};
