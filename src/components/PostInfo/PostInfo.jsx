520241fa2ceacc4ee45d558d2174a906eb6d258d
Nick <tortickk8475@gmail.com>
2026-01-03 18:02:15 +0200

    Refactor component structure for improved readability and consistency

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,22 +3,20 @@ import { CommentList } from '../CommentList';
 import './PostInfo.scss';
 
 export const PostInfo = ({ post }) => (
-  <>
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{post.title}</h3>
-        <UserInfo user={post.user} />
-      </div>
-
-      <p className="PostInfo__body">{post.body}</p>
-
-      <hr />
-
-      {post.comments.length ? (
-        <CommentList comments={post.comments} />
-      ) : (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      )}
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      <UserInfo user={post.user} />
     </div>
-  </>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
+    {post.comments.length ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,11 +1,9 @@
 import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ list }) => (
-  <>
-    <div className="PostList">
-      {list.map(post => (
-        <PostInfo key={post.id} post={post} />
-      ))}
-    </div>
-  </>
+  <div className="PostList">
+    {list.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </div>
 );
