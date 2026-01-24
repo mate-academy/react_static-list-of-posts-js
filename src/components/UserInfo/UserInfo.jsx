064b9902f98166f5e8ed26515a327b2d115ae224
Oleksandr Kozyriev <kozyrev.sania@gmail.com>
2026-01-24 22:14:14 +0200

    add examination at UserInfo

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,6 +1,10 @@
 import './UserInfo.scss';
 
 export const UserInfo = ({ user }) => {
+  if (!user) {
+    return null;
+  }
+
   return (
     <a href={`mailto:${user.email}`} className="UserInfo">
       {user.name}
