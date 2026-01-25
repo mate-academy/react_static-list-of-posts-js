761e9ec07510911ddc2265ae1c3c0530b7b2861e
ExtymAndriy <petrovskiyandriyko@gmail.com>
2026-01-25 11:49:59 +0200

    Solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,7 @@
-export const CommentList = () => <>Put the list here</>;
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </div>
+);
