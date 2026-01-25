8300f520ddc91fd1197fba1793984a8a2bcdccf6
Andrii Tsopych <andriythoupich01@gmail.com>
2026-01-25 02:01:56 +0200

    added task implementation

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,10 @@
-export const CommentList = () => <>Put the list here</>;
+// export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo comment={comment} key={comment.id} />
+    ))}
+  </div>
+);
