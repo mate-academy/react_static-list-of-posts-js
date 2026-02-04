6ece9716a8a085534e6c1187a8308bd0d4522925
Olga Wukelan <olga.wukelan@gmail.com>
2026-02-04 13:45:26 +0400

    create a files to react static list of posts

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,11 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ personalPost }) => {
+  return (
+    <div className="PostList">
+      {personalPost.map(post => (
+        <PostInfo post={post} key={post.id} />
+      ))}
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,11 @@
-export const UserInfo = () => <>Put the user here</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  // if (!user) return <span>Unknown User</span>;
+
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
