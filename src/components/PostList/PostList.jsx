2c540d0f167d05c52c69efeca954c3a145b849f5
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-02-08 21:31:08 +0200

    done React static list of posts (JS) task

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,11 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => {
+  return (
+    <>
+      {posts.map(post => (
+        <PostInfo key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
