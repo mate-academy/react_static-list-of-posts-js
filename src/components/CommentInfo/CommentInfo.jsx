9af22342ef9c311c087353b5fbb7a929d47b5d72
Gianluca Marchese <gianluca.marchese93@gmail.com>
2026-02-09 20:10:17 +0000

    Fix imports and add missing CommentInfo styles

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -5,10 +5,7 @@ export const CommentInfo = ({ comment }) => (
     <div className="CommentInfo__title">
       <strong className="CommentInfo__name">{comment.name}</strong>
       {' by '}
-      <a
-        className="CommentInfo__email"
-        href={`mailto:${comment.email}`}
-      >
+      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
         {comment.email}
       </a>
     </div>
