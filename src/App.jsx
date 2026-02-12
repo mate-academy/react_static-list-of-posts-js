f0514a02e5b1b68fa79b344228ca5e0f3e816196
ImPEPEGA <nicaly15117@gmail.com>
2026-02-12 13:19:21 -0500

    Solution 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
+
+import { PostList } from './components/PostList';
+
+function getById(data, id) {
+  return data.find(item => item.id === id);
+}
+
+export const posts = postsFromServer.map(post => ({
+  ...post,
+  user: getById(usersFromServer, post.userId),
+  comments: commentsFromServer.filter(comment => comment.postId === post.id),
+}));
+
+// export const comments = commentsFromServer.map(comment => ({
+//   ...comment,
+//   post: getById(postsFromServer, comment.postId),
+// }));
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <div className="PostList">
+    <PostList posts={posts} />
+
+    {/* <div className="PostList">
       <div className="PostInfo">
         <div className="PostInfo__header">
           <h3 className="PostInfo__title">qui est esse</h3>
@@ -99,6 +118,6 @@ export const App = () => (
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
   </section>
 );
