4f0123366a79a6ea17222ba67b1b45b93de6dd75
A-Melnykk <969696245n@gmail.com>
2026-02-12 13:50:06 +0200

    add task solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,18 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import React from 'react';
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => {
+  const { name, email, body } = comment;
+
+  return (
+    <article className="CommentInfo" data-cy="CommentInfo">
+      <div className="CommentInfo__header">
+        <h3 className="CommentInfo__name">{name}</h3>
+        <a href={`mailto:${email}`} className="CommentInfo__email">
+          {email}
+        </a>
+      </div>
+      <p className="CommentInfo__body">{body}</p>
+    </article>
+  );
+};
