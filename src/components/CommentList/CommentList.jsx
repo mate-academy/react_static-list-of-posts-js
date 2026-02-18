744401d5881674f92ec1e293b58d3fe0133c3176
Andriy <andriit35@gmail.com>
2026-02-18 14:58:15 +0200

    solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,8 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) =>
+  comments.length > 0 ? (
+    comments.map(comment => <CommentInfo key={comment.id} comment={comment} />)
+  ) : (
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,21 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const PostInfo = ({ post }) =>
+  post && (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          <UserInfo user={post.user} />
+        </p>
+
+        <hr />
+
+        <CommentList comments={post.comments} />
+      </div>
+    </div>
+  );
