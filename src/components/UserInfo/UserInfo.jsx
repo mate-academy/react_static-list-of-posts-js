088604c644cf8e74020743a95f6ff929e1aec3e1
Oleksandr Ratseburzhynskyi <ratseburzhynskyi@gmail.com>
2026-03-04 21:12:33 +0200

    fixed

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -4,8 +4,10 @@ export const UserInfo = ({ user }) => (
   <p>
     {' Posted by  '}
 
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
-    </a>
+    {user && (
+      <a className="UserInfo" href={`mailto:${user.email}`}>
+        {user.name}
+      </a>
+    )}
   </p>
 );
