0652862df6735bbb08f183153db3ababfdc2a749
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-03-05 21:12:05 +0200

    added solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -9,7 +9,7 @@ export const CommentList = ({ comments }) => {
   return (
     <div className="CommentList">
       {comments.map(comment => (
-        <CommentInfo comment={comment} />
+        <CommentInfo comment={comment} key={ comment.id } />
       ))}
     </div>
   );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -15,9 +15,7 @@ export const PostInfo = ({ post }) => (
     </div>
 
     <p className="PostInfo__body">
-      est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea
-      dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut
-      reiciendis qui aperiam non debitis possimus qui neque nisi nulla
+      {post.body}
     </p>
 
     <hr />
