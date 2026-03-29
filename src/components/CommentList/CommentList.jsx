d310a33187d937f5b725dd74b8791b156de11418
Taras Pidkuimukha <taraspidkuimukha@gmail.com>
2026-03-29 20:14:00 +0200

    feat: the list is done

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,12 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from "../CommentInfo/CommentInfo";
+import '../CommentList/CommentList.scss';
+
+export const CommentList = ({ comments }) => (
+
+  <div className="CommentList">
+    {comments.length === 0 && <p data-cy="NoCommentsMessage">No comments</p>}
+    {comments.map(comment =>
+      <CommentInfo key={comment.id} comment={comment} />
+    )}
+  </div>
+)
