8e1c9c94daab06768e777c95a087cdd9a135b161
Pavlo Polyvoda <pvlpeapod@gmail.com>
2026-03-30 12:11:49 +0300

    Add styles for component

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,39 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  const comments = post?.comments || [];
+
+  if (comments.length === 0) {
+    return (
+      <div className="PostInfo">
+        <div className="PostInfo__header">
+          <h3 className="PostInfo__title">{post.title}</h3>
+          <UserInfo user={post.user} />
+        </div>
+
+        <p className="PostInfo__body">{post.body}</p>
+
+        <div className="CommentList">
+          <strong data-cy="NoCommentsMessage" className="CommentInfo__name">
+            No comments yet
+          </strong>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+        <UserInfo user={post.user} />
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <CommentList comments={post.comments} />
+    </div>
+  );
+};
