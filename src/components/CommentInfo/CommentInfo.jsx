15f22c77a5a24e5464e644b07ea8e8ac30a60c1b
Oleh Prokopenko <olehprokopenko1@gmail.com>
2026-03-31 20:06:13 +0300

    add tusk solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,9 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => (
+  <div className="CommentInfo">
+    <h4 className="CommentInfo__name">{comment.name}</h4>
+    <p className="CommentInfo__email">{comment.email}</p>
+    <p className="CommentInfo__body">{comment.body}</p>
+  </div>
+);
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,16 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comment }) => {
+  if (!comment || comment.length === 0) {
+    return <div className="CommentList">No comments yet</div>;
+  }
+
+  return (
+    <div className="CommentList">
+      {comment.map(c => (
+        <CommentInfo key={c.id} comment={c} />
+      ))}
+    </div>
+  );
+};
