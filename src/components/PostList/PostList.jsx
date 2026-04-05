9740b73f8946385bd630f980a44d23e24ced9b3e
Nadiia <koropchenkon@gmail.com>
2026-04-05 21:10:45 +0300

    add task solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,10 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+import './PostList.scss';
+
+export const PostList = ({ posts = [] }) => (
+  <div className="PostList">
+    {posts.map(currentPost => (
+      <PostInfo post={currentPost} key={currentPost.id} />
+    ))}
+  </div>
+);
