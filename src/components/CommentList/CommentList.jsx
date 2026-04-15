2eab7aaa73d4b75bc9c0de8532b4cf8852f69194
ikseloi <gdalexgo@gmail.com>
2026-04-15 11:52:41 +0300

    add solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,19 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => {
+  if (!Array.isArray(comments) || comments.length === 0) {
+    return <b data-cy="NoCommentsMessage">No comments yet</b>;
+  }
+
+  return (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo
+          key={comment.id}
+          comment={comment}
+        />
+      ))}
+    </div>
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,22 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts, users, comments }) => {
+  return (
+    <div className="PostList">
+      {posts.map(post => {
+        const postWithData = {
+          ...post,
+          user: users[post.userId],
+          comments: comments[post.id] || [],
+        };
+
+        return (
+          <PostInfo
+            key={post.id}
+            post={postWithData}
+          />
+        );
+      })}
+    </div>
+  );
+};
