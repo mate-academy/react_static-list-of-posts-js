de3ed042f5aa3deb8337e29a5e6161ff406aebe7
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-04-22 11:40:46 -0300

    solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,18 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const PostInfo = ({ post, user, postComments }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      <p>
+        {' Posted by  '}
+        <UserInfo user={user} />
+      </p>
+    </div>
+    <p className="PostInfo__body">{post.body}</p>
+    <hr />
+    <CommentList postComments={postComments} />
+  </div>
+);
