c13a6aee17efd75ae5a07ed7784e2a7472261004
Kseniia Myronenko <kseniia.vasylenko11@gmail.com>
2026-04-23 14:57:36 +0300

    solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,12 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss'
+
+export const CommentList = ({ postId, comments }) => (
+  <div className="CommentList">
+  {comments
+  .map(comment => (
+    <div key={comment.id}>
+      <CommentInfo comment={comment} />
+    </div>
+  ))}
+  </div>
+);
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,11 @@
-export const PostList = () => <>Put the list here</>;
+export const PostList = ({ posts }) => {
+  return (
+    <div className="PostList">
+      {posts.map(post => (
+        <div className="PostInfo" key={post.id}>
+          <PostInfo post={post} />
+        </div>
+      ))}
+    </div>
+  );
+};
