c6d5b67696baa9b809a9d195578da5d1a101dff8
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-25 11:40:22 +0000

    feat: implement post components

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,15 @@
-export const UserInfo = () => <>Put the user here</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  if (!user) {
+    return <span className="UserInfo">Unknown user</span>;
+  }
+
+  const { email, name } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
