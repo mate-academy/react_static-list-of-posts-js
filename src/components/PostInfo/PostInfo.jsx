a362e74874217f418a87a1bf4cbd4765fc503e7d
Михайло <mykhailo@MacBook-Air-Mihajlo.local>
2026-04-25 15:27:39 +0300

    Solution 3

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -10,7 +10,7 @@ export const PostInfo = ({ post }) => (
       <p>
         {' Posted by  '}
 
-        <UserInfo user={post.user} key={post.userId} />
+        <UserInfo user={post.user} />
       </p>
     </div>
 
