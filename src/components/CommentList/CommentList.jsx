d9b0bf621a55ac81b4e3495226952dd8563ed895
Natalia Lunkevych <gadyka08@gmail.com>
2026-05-09 18:47:42 +0300

    done

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,17 @@
-export const CommentList = () => <>Put the list here</>;
+import React from 'react';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.length > 0 ? (
+      // Тут .map() для виведення списку коментарів (якщо вони є) ️
+      comments.map(comment => (
+        <CommentInfo key={comment.id} comment={comment} />
+      ))
+    ) : (
+      // Тут повідомлення, якщо коментарів немає
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
+);
