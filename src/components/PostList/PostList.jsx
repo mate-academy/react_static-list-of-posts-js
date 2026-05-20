6a6f2064426d663ac71755b087cfcee2ab08f719
Rodrigo <sistsayap>
2026-05-19 21:34:35 -0300

    fix: correct Navigation import3

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,56 +1,10 @@
-import postsFromServer from './api/posts.json';
-import commentsFromServer from './api/comments.json';
-import usersFromServer from './api/users.json';
-import { PostInfo } from './PostInfo';
+import { PostInfo } from '../PostInfo/PostInfo';
 
 export const PostList = ({ posts }) => {
   return (
     <div className="PostList">
       {posts.map(post => (
-        <div className="PostInfo" key={post.id}>
-          <div className="PostInfo__header">
-            <h3 className="PostInfo__title">{post.title}</h3>
-
-            <p>
-              {' Posted by '}
-
-              <a className="UserInfo" href={`mailto:${post.user.email}`}>
-                {post.user.name}
-              </a>
-            </p>
-          </div>
-
-          <p className="PostInfo__body">{post.body}</p>
-
-          <hr />
-
-          {!post.comments.length ? (
-            <b data-cy="NoCommentsMessage">No comments yet</b>
-          ) : (
-            <div className="CommentList">
-              {post.comments.map(comment => (
-                <div className="CommentInfo" key={comment.id}>
-                  <div className="CommentInfo__title">
-                    <strong className="CommentInfo__name">
-                      {comment.name}
-                    </strong>
-
-                    {' by '}
-
-                    <a
-                      className="CommentInfo__email"
-                      href={`mailto:${comment.email}`}
-                    >
-                      {comment.email}
-                    </a>
-                  </div>
-
-                  <div className="CommentInfo__body">{comment.body}</div>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+        <PostInfo key={post.id} post={post} />
       ))}
     </div>
   );
