aa740329f18aa10814e1560b9b13b186ee1cd885
Romulo Brasil <romulop.brasil25@gmail.com>
2026-05-22 23:23:48 -0300

    v1

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,9 @@
-export const CommentInfo = () => <>Put the comment here</>;
+export const CommentInfo = ({comment}) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__name">{comment.name}</div>
+    <a href={`mailto:${comment.email}`} className="CommentInfo__email">
+      {comment.email}
+    </a>
+    <p className="CommentInfo__body">{comment.body}</p>
+  </div>
+);
