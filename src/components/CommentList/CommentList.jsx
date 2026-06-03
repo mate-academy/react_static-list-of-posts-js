d915704161c8da361778bb15f7134667f8ea25da
Georgiy Veshchev <ghora2008@gmail.com>
2026-06-03 19:42:52 +0300

    solution 2.0

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -5,7 +5,9 @@ import './CommentList.scss';
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
     {comments.map(comment => (
-      <CommentInfo comment={comment} key={comment.id} />
+      <div key={comment.id}>
+        <CommentInfo comment={comment} />
+      </div>
     ))}
   </div>
 );
