d34aa29718812ba8b806678bf9574e9bbc88f7cb
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-06-08 14:00:20 +0300

    fix imports

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,5 +1,4 @@
-import { PostInfo } from '../PostInfo/PostInfo';
-import './PostList.scss';
+import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts }) => (
   <ul className="PostList">
