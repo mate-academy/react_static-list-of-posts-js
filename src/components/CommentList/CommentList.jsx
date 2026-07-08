953ae65dd8052bab8ea0309b9f30635f0f5465ca
Darren Rose <thorn086@gmail.com>
2026-07-08 11:36:12 -0500

    fix: updated bug logic

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -3,11 +3,11 @@ import './CommentList.scss';
 
 export const CommentList = ({ comments }) => {
   return comments.length > 0 ? (
-    <>
+    <div className="CommentList">
       {comments.map(comment => (
         <CommentInfo key={comment.id} comment={comment} />
       ))}
-    </>
+    </div>
   ) : (
     <b data-cy="NoCommentsMessage">No comments yet</b>
   );
