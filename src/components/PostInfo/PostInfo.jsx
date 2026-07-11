03ab46a12c116b6c81a7fbca169689934c957a56
Alex <11165950+mirlik@users.noreply.github.com>
2026-07-11 12:25:49 +0300

    solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,22 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+        <a className="UserInfo" href={`mailto:${post.author.email}`}>
+          {post.author.name}
+        </a>
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
+    <CommentList comments={post.comments} />
+  </div>
+);
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,9 @@
-export const UserInfo = () => <>Put the user here</>;
+export const UserInfo = () => (
+  <p>
+    {' Posted by  '}
+
+    <a className="UserInfo" href="mailto:Sincere@april.biz">
+      Leanne Graham
+    </a>
+  </p>
+);
