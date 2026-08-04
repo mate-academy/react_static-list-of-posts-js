4dfe369db0365783399a74c3e9839d9e33ab3049
LimeRider <cgakaviy@gmail.com>
2026-08-04 15:56:52 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import './App.scss';
 
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList/PostList';
 
-export const App = ({ posts }) => (
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+function getCommentsByPostId(postId) {
+  return commentsFromServer.filter(comment => comment.postId === postId);
+}
+
+const posts = postsFromServer.map(post => ({
+  ...post,
+  user: getUserById(post.userId),
+  comments: getCommentsByPostId(post.id),
+}));
+
+export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
     <PostList posts={posts} />
