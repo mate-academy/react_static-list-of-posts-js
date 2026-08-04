64053eb752db6643f0135d18bb78533ffcf68ede
LimeRider <cgakaviy@gmail.com>
2026-08-04 15:31:19 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import './App.scss';
 
-import postsFromServer from './api/posts.json';
-import commentsFromServer from './api/comments.json';
-import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList/PostList';
 
 export const App = ({ posts }) => (
