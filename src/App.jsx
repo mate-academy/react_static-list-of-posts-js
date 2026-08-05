e1691e361d7d56836feb2c68f6ae2a3b467287da
Sofiia Hryshko <sofiahryshko@gmail.com>
2026-08-05 16:49:24 +0300

    update task solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import './App.scss';
 import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
-import { PostList } from './components/PostList/PostList';
+import { PostList } from './components/PostList';
 
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId) || null;
 }
 
 function getCommentsById(postId) {
-  return (
-    commentsFromServer.filter(comment => comment.postId === postId) || null
-  );
+  return commentsFromServer.filter(comment => comment.postId === postId);
 }
 
 export const posts = postsFromServer.map(post => ({
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,4 +1,4 @@
-import { CommentList } from '../CommentList/CommentList';
+import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 
 export const PostInfo = ({ post }) => (
