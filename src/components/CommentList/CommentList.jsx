456628bc4aecdbc4c2b00755fe7b849b3350b9c9
diiyavol <nikitanesterenko88@gmail.com>
2026-08-14 21:32:41 +0300

    solution 1

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,10 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {/* <CommentInfo /> */}
+    {comments.map(comment => (
+      <CommentInfo comment={comment} key={comment.id} />
+    ))}
+  </div>
+);
