5cdeb114ef1642772531480799392faf4ce4f868
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-08-15 19:11:51 +0300

    complete task

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,15 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments = [] }) => {
+  if (comments.length > 0) {
+    return (
+      <div className="CommentList">
+        {comments.map(comment => (
+          <CommentInfo comment={comment} key={comment.id} />
+        ))}
+      </div>
+    );
+  }
+
+  return <b data-cy="NoCommentsMessage">No comments yet</b>;
+};
