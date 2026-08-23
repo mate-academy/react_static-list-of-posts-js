fa5b8ca415a6866e205c3ce876ef28c815ed376f
Anna Pryimak <sapfira03@gmail.com>
2026-08-23 14:09:59 +0300

    fixed syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,6 @@ export const posts = postsFromServer.map(post => ({
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
-    <PostList cosnt posts={posts} />
+    <PostList posts={posts} />
   </section>
 );
