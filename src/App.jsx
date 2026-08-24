d4641b5512c65ad82aefc8b248aaa4ece1ea4d68
ptrodion <rdnpetrenko@gmail.com>
2026-08-24 23:05:39 +0100

    react - task 6.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import { PostList } from './components/PostList/PostList';
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
+
+    <PostList />
 
     <div className="PostList">
       <div className="PostInfo">
@@ -71,30 +71,6 @@ export const App = () => (
               dolorum voluptas laboriosam quisquam ab totam beatae et aut
               aliquid optio assumenda voluptas velit itaque quidem voluptatem
               tempore cupiditate in itaque sit molestiae minus dolores magni
-            </div>
-          </div>
-
-          <div className="CommentInfo">
-            <div className="CommentInfo__title">
-              <strong className="CommentInfo__name">
-                odio adipisci rerum aut animi
-              </strong>
-
-              {' by '}
-
-              <a
-                className="CommentInfo__email"
-                href="mailto:Nikita@garfield.biz"
-              >
-                Nikita@garfield.biz
-              </a>
-            </div>
-
-            <div className="CommentInfo__body">
-              quia molestiae reprehenderit quasi aspernatur aut expedita
-              occaecati aliquam eveniet laudantium omnis quibusdam delectus
-              saepe quia accusamus maiores nam est cum et ducimus et vero
-              voluptates excepturi deleniti ratione
             </div>
           </div>
         </div>
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,19 @@
-export const PostList = () => <>Put the list here</>;
+import usersList from '../../api/users.json';
+import postsList from '../../api/posts.json';
+import commentsList from '../../api/comments.json';
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = () => {
+  return (
+    <div className="PostList">
+      {postsList.map(post => {
+        const user = usersList.find(u => u.id === post.userId);
+        const comments = commentsList.filter(c => c.postId === post.id);
+
+        const postWithDetails = { ...post, user, comments };
+
+        return <PostInfo key={post.id} post={postWithDetails} />;
+      })}
+    </div>
+  );
+};
