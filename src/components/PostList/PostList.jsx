e03de79d7b63fe20b3dc6f60813ccaf922bb9c98
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2026-08-26 11:38:32 +0300

    Completed

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,24 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts, comments, users }) => (
+  <div className="PostList">
+    {posts.map(post => {
+      const user = users.find(pers => pers.id === post.userId);
+
+      const postComments = comments.filter(
+        comment => comment.postId === post.id,
+      );
+
+      return (
+        <PostInfo
+          key={post.id}
+          post={{
+            ...post,
+            user,
+            comments: postComments,
+          }}
+        />
+      );
+    })}
+  </div>
+);
