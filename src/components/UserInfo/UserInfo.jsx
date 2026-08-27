474fa02b4180f689af581e3a3536625df77ad9e4
Olga Shcherbatiuk <nika32.n3@gmail.com>
2026-08-27 19:19:18 +0300

    fixed

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,7 @@
 import './UserInfo.scss';
 
 export const UserInfo = ({ user }) => {
-  const { name, email } = user || null;
+  const { name, email } = user || {};
 
   return (
     <a className="UserInfo" href={`mailto:${email}`}>
