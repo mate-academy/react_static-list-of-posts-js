ed0dc24b9d0b1ffa86f9e4a6b8990339d0a5ec13
Vlad CHuikov <koras4uikov@gmail.com>
2026-08-27 20:46:36 +0300

    forgot to lint

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,17 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts, users, comments }) => (
+  <div className="PostList">
+    {posts.map(post => {
+      const user = users.find(u => u.id === post.userId);
+
+      const postComments = comments.filter(
+        comment => comment.postId === post.id,
+      );
+
+      const postWithComments = { ...post, user, comments: postComments };
+
+      return <PostInfo post={postWithComments} key={post.id} />;
+    })}
+  </div>
+);
