09826603802be9fe1bff93c4a82941ebc84740e7
Chempik120 <135869484+Chempik120@users.noreply.github.com>
2026-08-29 12:39:14 +0300

    Solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -4,10 +4,7 @@ export const CommentInfo = ({ comment }) => (
   <div className="CommentInfo">
     <h4 className="CommentInfo__name">{comment.name}</h4>
     <p className="CommentInfo__body">{comment.body}</p>
-    <a 
-      href={`mailto:${comment.email}`} 
-      className="CommentInfo__email"
-    >
+    <a href={`mailto:${comment.email}`} className="CommentInfo__email">
       {comment.email}
     </a>
   </div>
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -6,9 +6,9 @@ export const PostInfo = ({ post }) => (
   <article className="PostInfo">
     <h2 className="PostInfo__title">{post.title}</h2>
     <p className="PostInfo__body">{post.body}</p>
-    
+
     {post.user && <UserInfo user={post.user} />}
-    
+
     <CommentList comments={post.comments} />
   </article>
 );
