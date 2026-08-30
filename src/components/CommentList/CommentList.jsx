314e0724dbd390107b521d97d2d93cd1290fdb9c
Royko7 <ura.royko@gmail.com>
2026-08-30 12:36:56 +0300

    add fix

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,6 +1,6 @@
 import { CommentInfo } from '../CommentInfo/CommentInfo';
 import './CommentList.scss';
-
+// Luke check again
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
     {comments.map(comment => (
