626906d5c03167fefb6d45ac426b2fcab5e8c790
Artroy666 <xandertroy666@gmail.com>
2026-08-31 15:54:06 +0300

    add task solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -9,7 +9,9 @@ export const CommentList = ({ comments }) => {
   return (
     <div className="CommentList">
       {comments.map(comment => (
-        <CommentInfo key={comment.id} comment={comment} />
+        <div key={comment.id}>
+          <CommentInfo comment={comment} />
+        </div>
       ))}
     </div>
   );
