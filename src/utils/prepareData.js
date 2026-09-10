3a7aa1570b315688af551290ffa689788d6d6184
Nataliia <nata.kovbasnyukk@gmail.com>
2026-09-10 11:54:17 +0200

    debug code

---
+++ src/utils/prepareData.js
@@ -0,0 +1,7 @@
+export function preparePosts(posts, users, comments) {
+  return posts.map(post => ({
+    ...post,
+    user: users.find(user => user.id === post.userId),
+    comments: comments.filter(comment => comment.postId === post.id),
+  }));
+}
