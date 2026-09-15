06f03cb817dd7c5308699e13c741064627084a23
Samuel <samuel.fsbs@gmail.com>
2026-09-15 09:55:54 -0300

    Adding the solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,16 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import React from 'react';
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => {
+  const { name, email, body } = comment;
+
+  return (
+    <div className="CommentInfo">
+      <h4 className="CommentInfo__name">{name}</h4>
+      <p className="CommentInfo__body">{body}</p>
+      <a href={`mailto:${email}`} className="CommentInfo__email">
+        {email}
+      </a>
+    </div>
+  );
+};
